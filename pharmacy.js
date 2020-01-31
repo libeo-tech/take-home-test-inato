e9209be685ea92ee53bc482535159b46dcfab72b
Jad ch <jad.chalita@gmail.com>
2020-01-31 21:40:15 +0100

    Refacto if now mainly done, all unit tests are passing

--- pharmacy.js
+++ pharmacy.js
@@ -34,8 +34,36 @@ const DRUGS_CONFIG = {
   // MAGIC PILL CASE
   "Magic Pill": (expiresIn, benefit) => ({
     expiresIn,
-    benefit
-  })
+    benefit // nothing happens for either expiresIn or benefit
+  }),
+
+  // FERVEX CASE
+  Fervex: (expiresIn, benefit) => {
+    // like Herbal Tea, increases in Benefit (+1) as its expiration date approaches.
+    if (expiresIn > 10)
+      return {
+        expiresIn: expiresIn - 1,
+        benefit: Math.min(benefit + 1, 50)
+      };
+    // Benefit increases by 2 when there are 10 days or less
+    if (expiresIn > 5)
+      return {
+        expiresIn: expiresIn - 1,
+        benefit: Math.min(benefit + 2, 50)
+      };
+    // Benefit increases by 3 when there are 5 days or less
+    if (expiresIn > 0)
+      return {
+        expiresIn: expiresIn - 1,
+        benefit: Math.min(benefit + 3, 50)
+      };
+    // Benefit drops to 0 after the expiration date
+    if (expiresIn <= 0)
+      return {
+        expiresIn: expiresIn - 1,
+        benefit: 0
+      };
+  }
 };
 
 export class Drug {
