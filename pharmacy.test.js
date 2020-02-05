953a2b567924c38a58679bd6895257667c79698d
Fanny Huang <>
2020-02-05 16:42:55 +0100

    Add a few elements for testing

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,24 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
+
+const drugs = [
+  new Drug("Doliprane", 20, 30),
+  new Drug("Herbal Tea", 10, 5),
+  new Drug("Fervex", 5, 40),
+  new Drug("Magic Pill", 15, 40),
+  new Drug("Dafalgan", 30, 20)
+];
+
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+    expect(new Pharmacy(drugs).updateBenefitValue()).toEqual(
+      [
+        new Drug("Doliprane", 19, 29),
+        new Drug("Herbal Tea", 9, 6),
+        new Drug("Fervex", 4, 43),
+        new Drug("Magic Pill", 15, 40),
+        new Drug("Dafalgan", 29, 18)
+      ]
     );
   });
 });
