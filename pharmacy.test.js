200414e0758216cb19958f605f6e266311e0b01b
bruno <bybrunobarros@gmail.com>
2020-02-09 12:25:19 +0100

    refactor: use test cases in a loop instead of several "it" functions

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,18 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
-  });
+  const testCases = {
+    "should decrease the benefit and expiresIn": {
+      drug: new Drug("test", 2, 3),
+      expected: new Drug("test", 1, 2)
+    }
+  };
+
+  for (const [key, testCase] of Object.entries(testCases)) {
+    it(key, () => {
+      expect(new Pharmacy([testCase.drug]).updateBenefitValue()).toStrictEqual([
+        testCase.expected
+      ]);
+    });
+  }
 });
