089095f42a6e92ee4e1a181a467844e9f18e7b97
Loïc Weinhard <loic.weinhard@neofacto.com>
2020-02-14 15:01:47 +0100

    Refactor Drug class and add benefit behavior handler

--- src/drug.js
+++ src/drug.js
@@ -1,7 +1,23 @@
+import { herbalTeaBenefitBehavior } from "./herbal_tea";
+
+const drugsBenefitBehaviors = {
+  "Herbal Tea": herbalTeaBenefitBehavior
+};
+
+function defaultBehavior() {
+  this.expiresIn -= 1;
+  this.benefit -= 1;
+}
+
+function assignBenefitBehaviorFromName(name) {
+  return drugsBenefitBehaviors[name];
+}
+
 export class Drug {
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
+    this.updateBenefitValue = assignBenefitBehaviorFromName(this.name) || defaultBehavior;
   }
 }
