9c90f61272127b22f59624dc92893b667e1eb858
Loïc Weinhard <loic.weinhard@neofacto.com>
2020-02-14 14:10:41 +0100

    Fix : move files + move Drug class from Pharmacy file to Drug file

---
+++ test/pharmacy.test.js
@@ -0,0 +1,10 @@
+import { Drug } from "../src/drug";
+import { Pharmacy } from "../src/pharmacy";
+
+describe("Pharmacy", () => {
+  it("should decrease the benefit and expiresIn", () => {
+    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 1, 2)]
+    );
+  });
+});
