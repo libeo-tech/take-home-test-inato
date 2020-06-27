6bf3719f3f75882b9e3264f1c50ffdc5dc13daf8
Maxime Amore <maxime.marco.amore@gmail.com>
2020-06-27 15:46:13 +0200

    linter, some fix on the ternaries operator and first tests

--- pharmacy.js
+++ pharmacy.js
@@ -1,57 +1,56 @@
 export class Drug {
   constructor(name, expiresIn, benefit) {
-    this.name = name
-    this.expiresIn = expiresIn
-    this.benefit = benefit
+    this.name = name;
+    this.expiresIn = expiresIn;
+    this.benefit = benefit;
   }
 }
 
 export class Pharmacy {
   constructor(drugs = []) {
-    this.drugs = drugs
+    this.drugs = drugs;
   }
   updateBenefitValue() {
-    this.drugs.forEach((drug) => {
-
+    this.drugs.forEach(drug => {
       // "Magic Pill" never expires nor decreases in Benefit.
-      if (drug.name === 'Magic Pill') return
+      if (drug.name === "Magic Pill") return;
 
-      drug.expiresIn = drug.expiresIn - 1
+      drug.expiresIn = drug.expiresIn - 1;
 
       // I use switch for a better reading experience
       // more than an optimized solution
       switch (drug.name) {
-        case 'Herbal Tea':
-          drug.benefit =
-            drug.benefit <= drug.expiresIn ? drug.benefit + 2 : drug.benefit + 1
-          break
-        case 'Dafalgan':
-          drug.benefit =
-            drug.benefit <= drug.expiresIn ? drug.benefit - 4 : drug.benefit - 2
-          break
-        case 'Fervex':
-          drug.benefit = updateFervexBenefit(drug)
-          break
+        case "Herbal Tea":
+          drug.benefit = drug.benefit =
+            drug.expiresIn <= 0 ? drug.benefit + 2 : drug.benefit + 1;
+          break;
+        case "Dafalgan":
+          drug.benefit = drug.benefit =
+            drug.expiresIn <= 0 ? drug.benefit - 4 : drug.benefit - 2;
+          break;
+        case "Fervex":
+          drug.benefit = updateFervexBenefit(drug);
+          break;
         default:
-          drug.benefit =
-            drug.benefit <= drug.expiresIn ? drug.benefit - 2 : drug.benefit - 1
-          break
+          drug.benefit = drug.benefit =
+            drug.expiresIn <= 0 ? drug.benefit - 2 : drug.benefit - 1;
+          break;
       }
 
       // The Benefit of an item is never more than 50.
-      if (drug.benefit > 50) drug.benefit = 50
+      if (drug.benefit > 50) drug.benefit = 50;
 
       // The Benefit of an item is never negative.
-      if (drug.benefit < 0) drug.benefit = 0
-    })
+      if (drug.benefit < 0) drug.benefit = 0;
+    });
 
-    return this.drugs
+    return this.drugs;
   }
 }
 
 function updateFervexBenefit(drug) {
-  if (drug.expiresIn <= 0) return 0
-  if (drug.expiresIn <= 5) return drug.benefit - 3
-  if (drug.expiresIn <= 10) return drug.benefit + 2
-  return drug.benefit + 2
+  if (drug.expiresIn <= 0) return 0;
+  if (drug.expiresIn <= 5) return drug.benefit - 3;
+  if (drug.expiresIn <= 10) return drug.benefit + 2;
+  return drug.benefit + 2;
 }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,15 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Drug, Pharmacy } from './pharmacy'
 
-describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
-  });
-});
+describe('Pharmacy', () => {
+  it('should decrease the benefit and expiresIn', () => {
+    expect(
+      new Pharmacy([new Drug('test', 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug('test', 1, 2)])
+  })
+
+  it('should decrease expiresIn and decrease twice the benefit', () => {
+    expect(
+      new Pharmacy([new Drug('test', 1, 2)]).updateBenefitValue()
+    ).toEqual([new Drug('test', 0, 0)])
+  })
+})
