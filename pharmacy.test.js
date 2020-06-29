9a7e33bac53a2b7a5193eaf24574d40e4c32860d
Robin VERDIER <verdier.robin@gmail.com>
2020-06-29 20:06:18 +0200

    Add tests for any situation of any type of drugs

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,69 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+  it("handles Herbal Tea", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 10, 5)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 9, 6)]
+    );
+  });
+  it("handles expired Herbal Tea", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", -2, 5)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -3, 7)]
+    );
+  });
+  it("handles Fervex", () => {
+    expect(new Pharmacy([new Drug("Fervex", 12, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 11, 40)]
+    );
+  });
+  it("handles 10 days Fervex", () => {
+    expect(new Pharmacy([new Drug("Fervex", 10, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 9, 42)]
+    );
+  });
+  it("handles 5 days Fervex", () => {
+    expect(new Pharmacy([new Drug("Fervex", 5, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 4, 43)]
+    );
+  });
+  it("handles expired Fervex", () => {
+    expect(new Pharmacy([new Drug("Fervex", -2, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", -3, 0)]
+    );
+  });
+  it("handles Magic Pill", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 15, 40)]
+    );
+  });
+  it("handles random drugs", () => {
+    expect(new Pharmacy([new Drug("Doliprane", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Doliprane", 1, 2)]
+    );
+  });
+  it("handles expired random drugs", () => {
+    expect(new Pharmacy([new Drug("Doliprane", -2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Doliprane", -3, 1)]
+    );
+  });
+  it("handles drugs during 30 days", () => {
+    const drugs = [
+      new Drug("Doliprane", 20, 30),
+      new Drug("Herbal Tea", 10, 5),
+      new Drug("Fervex", 5, 40),
+      new Drug("Magic Pill", 15, 40)
+    ];
+    let trial = new Pharmacy(drugs);
+    for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+      trial.updateBenefitValue();
+    }
+    expect(trial.drugs).toEqual(
+      [
+        new Drug("Doliprane", -10, 0),
+        new Drug("Herbal Tea", -20, 50),
+        new Drug("Fervex", -25, 0),
+        new Drug("Magic Pill", 15, 40)
+      ]
     );
   });
 });
