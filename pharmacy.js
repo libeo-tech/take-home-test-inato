efe5cf19a1513e809bb48fbcbd5d8a32c4f444cf
Nicolas Borrelly <niborrel@student.42.fr>
2020-07-31 05:38:08 +0200

    PHARMACY: Refactored drugs benefit updating, added Dafalgan handling

--- pharmacy.js
+++ pharmacy.js
@@ -10,56 +10,71 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
+
+  updateDolipraneBenefitValue(drug) {
+    drug.benefit = drug.expiresIn === 0 ? drug.benefit -= 2 : drug.benefit -= 1;
+    // -1 default, twice as fast once expiration -2
+  }
+
+  updateHerbalTeaBenefitValue(drug) {
+    drug.benefit = drug.expiresIn === 0 ? drug.benefit += 2 : drug.benefit += 1;
+    // +1 default, twice as fast on expiration +2
+  }
+
+  updateFervexBenefitValue(drug) {
+    if (drug.expiresIn === 0) {
+      drug.benefit = 0;
+
+      return;
     }
+
+    if (drug.expiresIn > 10) {
+      drug.benefit = drug.benefit += 1;
+    } else if (drug.expiresIn > 5) {
+      drug.benefit = drug.benefit += 2;
+    } else if (drug.expiresIn <= 5) {
+      drug.benefit = drug.benefit += 3;
+    }
+    // +1 default, +2 if 10- days, +3 if 5- days, 0 after expiration
+  }
+
+  updateDafalganBenefitValue(drug) {
+    drug.benefit = drug.expiresIn === 0 ? drug.benefit -= 4 : drug.benefit -= 2;
+    // -2 default, twice as fast on expiration -4
+  }
+
+
+  updateBenefitValue() {
+    this.drugs.forEach((drug) => {
+      if (drug.benefit === 0 || drug.benefit === 50 || drug.name === "Magic Pill") {
+        return;
+      }
+
+      switch (drug.name) {
+        case 'Doliprane':
+          this.updateDolipraneBenefitValue(drug);
+          break;
+        case 'Herbal Tea':
+          this.updateHerbalTeaBenefitValue(drug);
+          break;
+        case 'Fervex':
+          this.updateFervexBenefitValue(drug);
+          break;
+        case 'Dafalgan':
+          this.updateDafalganBenefitValue(drug);
+          break;
+      }
+
+      if (drug.benefit < 0) {
+        drug.benefit = 0;
+      } else if (drug.benefit > 50) {
+        drug.benefit = 50;
+      }
+
+      if (drug.expiresIn != 0) {
+        drug.expiresIn = drug.expiresIn -= 1;
+      }
+    })
 
     return this.drugs;
   }
