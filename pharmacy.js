eb9aed753b0182766c9958041bf7ae0d7f34adfc
François Guillot <francois.guillot@epsor.fr>
2020-09-01 23:24:54 +0200

    feat: Refactor pharmacy 'updateBenefitValue' function

--- pharmacy.js
+++ pharmacy.js
@@ -1,9 +1,184 @@
-export class Drug {
-  constructor(name, expiresIn, benefit) {
-    this.name = name;
-    this.expiresIn = expiresIn;
-    this.benefit = benefit;
+import { UPDATE_BENEFIT_STRATEGIES } from "./updateBenefitStrategies";
+
+const BENEFIT_LOWER_BOUND = 0;
+const BENEFIT_HIGHER_BOUND = 50;
+
+/**
+ * Fixes drug benefit to be within acceptable bounds
+ *
+ * @param {number} drugBenefit Drug benefit
+ * @returns {number} Fixed drug benefit
+ */
+function fixDrugBenefitWithinBounds(drugBenefit) {
+  const fixedBenefitOnLowerBound = Math.max(drugBenefit, BENEFIT_LOWER_BOUND);
+  const fixedBenefitOnHigherBound = Math.min(
+    fixedBenefitOnLowerBound,
+    BENEFIT_HIGHER_BOUND
+  );
+
+  return fixedBenefitOnHigherBound;
+}
+
+/**
+ * Updates drug based on default strategy
+ *
+ * @param {object} drug Drug to update
+ * @returns {object} Updated drug
+ */
+function updateBenefitDefaultStrategy(drug) {
+  const hasExpired = drug.expiresIn === 0;
+  const valueToSubtract = hasExpired ? 2 : 1;
+  const benefit = drug.benefit - valueToSubtract;
+  const expiresIn = Math.max(0, drug.expiresIn - 1);
+
+  const updatedDrug = {
+    ...drug,
+    expiresIn,
+    benefit: fixDrugBenefitWithinBounds(benefit)
+  };
+
+  return updatedDrug;
+}
+
+/**
+ * Updates drug based on 'increases-until-expiration-date-then-increases-twice-as-fast' strategy
+ *
+ * @param {object} drug Drug to update
+ * @returns {object} Updated drug
+ */
+function updateBenefitIncreasesUntilExpirationDateThenIncreasesTwiceAsFastStrategy(
+  drug
+) {
+  const hasExpired = drug.expiresIn === 0;
+  const valueToAdd = hasExpired ? 2 : 1;
+  const benefit = drug.benefit + valueToAdd;
+  const expiresIn = Math.max(0, drug.expiresIn - 1);
+
+  const updatedDrug = {
+    ...drug,
+    expiresIn,
+    benefit: fixDrugBenefitWithinBounds(benefit)
+  };
+
+  return updatedDrug;
+}
+
+/**
+ * Returns the value to add to drug's benefit for 'Fizzbuzz' strategy
+ *
+ * @param {number} currentBenefit Drug's current benefit
+ * @param {number} expiresIn Drug's number of days until expiration
+ * @returns {number} Value to add to drug's benefit
+ */
+function calculateValueToAddForFizzbuzzStrategy(currentBenefit, expiresIn) {
+  const hasExpired = expiresIn === 0;
+
+  if (hasExpired) {
+    return -currentBenefit;
   }
+
+  if (expiresIn <= 5) {
+    return 3;
+  }
+
+  if (expiresIn <= 10) {
+    return 2;
+  }
+
+  return 1;
+}
+
+/**
+ * Updates drug based on 'increases-until-expiration-date-like-fizzbuzz' strategy
+ *
+ * @param {object} drug Drug to update
+ * @returns {object} Updated drug
+ */
+function updateBenefitIncreasesUntilExpirationDateLikeFizzbuzzStrategy(drug) {
+  const valueToAdd = calculateValueToAddForFizzbuzzStrategy(
+    drug.benefit,
+    drug.expiresIn
+  );
+  const benefit = drug.benefit + valueToAdd;
+  const expiresIn = Math.max(0, drug.expiresIn - 1);
+
+  const updatedDrug = {
+    ...drug,
+    expiresIn,
+    benefit: fixDrugBenefitWithinBounds(benefit)
+  };
+
+  return updatedDrug;
+}
+
+/**
+ * Updates drug based on 'decreases-twice-as-fast' strategy
+ *
+ * @param {object} drug Drug to update
+ * @returns {object} Updated drug
+ */
+function updateBenefitDecreasesTwiceAsFastStrategy(drug) {
+  const hasExpired = drug.expiresIn === 0;
+  const valueToSubtract = hasExpired ? 4 : 2;
+  const benefit = drug.benefit - valueToSubtract;
+  const expiresIn = Math.max(0, drug.expiresIn - 1);
+
+  const updatedDrug = {
+    ...drug,
+    expiresIn,
+    benefit: fixDrugBenefitWithinBounds(benefit)
+  };
+
+  return updatedDrug;
+}
+
+/**
+ * Updates drug based on 'benefit-does-not-change' strategy
+ *
+ * @param {object} drug Drug to update
+ * @returns {object} Updated drug
+ */
+function updateBenefitBenefitDoesNotChangeStrategy(drug) {
+  const updatedDrug = {
+    ...drug,
+    benefit: fixDrugBenefitWithinBounds(drug.benefit)
+  };
+
+  return updatedDrug;
+}
+
+// Map of applicable update benefit strategies
+const updateBenefitStrategies = {
+  [UPDATE_BENEFIT_STRATEGIES.DEFAULT]: updateBenefitDefaultStrategy,
+  [UPDATE_BENEFIT_STRATEGIES.INCREASES_UNTIL_EXPIRATION_DATE_THEN_INCREASES_TWICE_AS_FAST]: updateBenefitIncreasesUntilExpirationDateThenIncreasesTwiceAsFastStrategy,
+  [UPDATE_BENEFIT_STRATEGIES.INCREASES_UNTIL_EXPIRATION_DATE_LIKE_FIZZBUZZ]: updateBenefitIncreasesUntilExpirationDateLikeFizzbuzzStrategy,
+  [UPDATE_BENEFIT_STRATEGIES.BENEFIT_DOES_NOT_CHANGE]: updateBenefitBenefitDoesNotChangeStrategy,
+  [UPDATE_BENEFIT_STRATEGIES.DECREASES_TWICE_AS_FAST]: updateBenefitDecreasesTwiceAsFastStrategy
+};
+
+/**
+ * Updates drug based on applicable update benefit strategy
+ *
+ * @param {object} drug Drug to update
+ * @returns {object} Updated drug
+ */
+function updateBenefit(drug) {
+  if (!drug.updateBenefitStrategy) {
+    throw new Error(`Drug '${drug.name}' has no update benefit strategy`);
+  }
+
+  const updateBenefitStrategy =
+    updateBenefitStrategies[drug.updateBenefitStrategy];
+
+  if (!updateBenefitStrategy) {
+    throw new Error(
+      `There is no update benefit strategy named '${drug.updateBenefitStrategy}'`
+    );
+  }
+
+  const updatedDrug = updateBenefitStrategy(drug);
+
+  return updatedDrug;
 }
 
 export class Pharmacy {
@@ -11,56 +186,15 @@ export class Pharmacy {
     this.drugs = drugs;
   }
   updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
-    }
+    this.drugs = this.drugs.map(updateBenefit);
 
-    return this.drugs;
+    // This is just so the result fits 'output.txt'
+    const formattedDrugs = this.drugs.map(drug => ({
+      name: drug.name,
+      expiresIn: drug.expiresIn,
+      benefit: drug.benefit
+    }));
+
+    return formattedDrugs;
   }
 }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,173 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { WELL_KNOWN_DRUG_NAMES } from "./wellKnownDrugNames";
+import { Drug } from "./drug";
+import { Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("updateBenefitValue()", () => {
+    describe("with a default drug", () => {
+      it.each([
+        [25, 24, 30, 29],
+        [25, 24, 55, 50],
+        [25, 24, 0, 0],
+        [0, 0, 12, 10],
+        [0, 0, 1, 0]
+      ])(
+        "should turn 'expiresIn' from '%i' to '%i' and 'benefit' from '%i' to '%i'",
+        (fromExpiresIn, toExpiresIn, fromBenefit, toBenefit) => {
+          // Arrange
+          const drug = new Drug("Foobar", fromExpiresIn, fromBenefit);
+          const pharmacy = new Pharmacy([drug]);
+
+          // Act
+          const result = pharmacy.updateBenefitValue();
+
+          // Assert
+          expect(result).toEqual([
+            {
+              name: "Foobar",
+              benefit: toBenefit,
+              expiresIn: toExpiresIn
+            }
+          ]);
+        }
+      );
+    });
+
+    describe("with a drug whose benefit increases until expiration date then increases twice as fast", () => {
+      it.each([
+        [25, 24, 30, 31],
+        [25, 24, 55, 50],
+        [25, 24, 0, 1],
+        [0, 0, 12, 14],
+        [0, 0, 49, 50]
+      ])(
+        "should turn 'expiresIn' from '%i' to '%i' and 'benefit' from '%i' to '%i'",
+        (fromExpiresIn, toExpiresIn, fromBenefit, toBenefit) => {
+          // Arrange
+          const drug = new Drug(
+            WELL_KNOWN_DRUG_NAMES.HERBAL_TEA,
+            fromExpiresIn,
+            fromBenefit
+          );
+          const pharmacy = new Pharmacy([drug]);
+
+          // Act
+          const result = pharmacy.updateBenefitValue();
+
+          // Assert
+          expect(result).toEqual([
+            {
+              name: WELL_KNOWN_DRUG_NAMES.HERBAL_TEA,
+              benefit: toBenefit,
+              expiresIn: toExpiresIn
+            }
+          ]);
+        }
+      );
+    });
+
+    describe("with a drug whose benefit increases until expiration date like fizzbuzz", () => {
+      it.each([
+        [25, 24, 30, 31],
+        [25, 24, 55, 50],
+        [10, 9, 55, 50],
+        [5, 4, 55, 50],
+        [25, 24, 0, 1],
+        [10, 9, 0, 2],
+        [5, 4, 0, 3],
+        [1, 0, 12, 15],
+        [0, 0, 50, 0]
+      ])(
+        "should turn 'expiresIn' from '%i' to '%i' and 'benefit' from '%i' to '%i'",
+        (fromExpiresIn, toExpiresIn, fromBenefit, toBenefit) => {
+          // Arrange
+          const drug = new Drug(
+            WELL_KNOWN_DRUG_NAMES.FERVEX,
+            fromExpiresIn,
+            fromBenefit
+          );
+          const pharmacy = new Pharmacy([drug]);
+
+          // Act
+          const result = pharmacy.updateBenefitValue();
+
+          // Assert
+          expect(result).toEqual([
+            {
+              name: WELL_KNOWN_DRUG_NAMES.FERVEX,
+              benefit: toBenefit,
+              expiresIn: toExpiresIn
+            }
+          ]);
+        }
+      );
+    });
+
+    describe("with a drug which has no benefit change", () => {
+      it.each([
+        [25, 25, 30, 30],
+        [25, 25, 55, 50],
+        [25, 25, 0, 0],
+        [1, 1, 12, 12],
+        [0, 0, 1, 1],
+        [0, 0, -1, 0]
+      ])(
+        "should turn 'expiresIn' from '%i' to '%i' and 'benefit' from '%i' to '%i'",
+        (fromExpiresIn, toExpiresIn, fromBenefit, toBenefit) => {
+          // Arrange
+          const drug = new Drug(
+            WELL_KNOWN_DRUG_NAMES.MAGIC_PILL,
+            fromExpiresIn,
+            fromBenefit
+          );
+          const pharmacy = new Pharmacy([drug]);
+
+          // Act
+          const result = pharmacy.updateBenefitValue();
+
+          // Assert
+          expect(result).toEqual([
+            {
+              name: WELL_KNOWN_DRUG_NAMES.MAGIC_PILL,
+              benefit: toBenefit,
+              expiresIn: toExpiresIn
+            }
+          ]);
+        }
+      );
+    });
+
+    describe("with a drug whose benefit decreases twice as fast", () => {
+      it.each([
+        [25, 24, 30, 28],
+        [25, 24, 55, 50],
+        [25, 24, 0, 0],
+        [0, 0, 12, 8],
+        [0, 0, 1, 0]
+      ])(
+        "should turn 'expiresIn' from '%i' to '%i' and 'benefit' from '%i' to '%i'",
+        (fromExpiresIn, toExpiresIn, fromBenefit, toBenefit) => {
+          // Arrange
+          const drug = new Drug(
+            WELL_KNOWN_DRUG_NAMES.DAFALGAN,
+            fromExpiresIn,
+            fromBenefit
+          );
+          const pharmacy = new Pharmacy([drug]);
+
+          // Act
+          const result = pharmacy.updateBenefitValue();
+
+          // Assert
+          expect(result).toEqual([
+            {
+              name: WELL_KNOWN_DRUG_NAMES.DAFALGAN,
+              benefit: toBenefit,
+              expiresIn: toExpiresIn
+            }
+          ]);
+        }
+      );
+    });
   });
 });
