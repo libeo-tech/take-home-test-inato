1f9b6afd2192d01736034b8cf6353ad935db674a
Valentino-Houessou <valentino.houessou@gmail.com>
2020-10-05 20:51:44 +0200

    Move Magic pill Benefit Updater file

--- src/MagicPillBenefitUpdater.js
+++
@@ -1,7 +0,0 @@
-import { BenefitUpdater } from "./benefitUpdater/BenefitUpdater";
-
-export class MagicPillBenefitUpdater extends BenefitUpdater {
-  updateBenefitValue(benefit, expiresIn) {
-    return "this is the MAgic Pill Benefit updater";
-  }
-}
