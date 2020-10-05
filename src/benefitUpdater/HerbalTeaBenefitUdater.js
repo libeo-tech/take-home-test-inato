5739a217d72168f63b896a4e57842e497825bcbb
Valentino-Houessou <valentino.houessou@gmail.com>
2020-10-05 20:39:40 +0200

    Implement HerbalTeaBenefitUpdateValue

--- src/benefitUpdater/HerbalTeaBenefitUdater.js
+++ src/benefitUpdater/HerbalTeaBenefitUdater.js
@@ -1,7 +1,12 @@
 import { BenefitUpdater } from "./BenefitUpdater";
+import { MAX_BENEFIT_VALUE } from "../Constants";
 
 export class HerbalTeaBenefitUpdate extends BenefitUpdater {
   updateBenefitValue(benefit, expiresIn) {
-    return "herbal tea benefit updater";
+    if (benefit < MAX_BENEFIT_VALUE) benefit++;
+    expiresIn--;
+    if (expiresIn < 0 && benefit < MAX_BENEFIT_VALUE) benefit++;
+
+    return { benefit, expiresIn };
   }
 }
