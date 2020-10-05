dd4a398c4a741c89073eabe35997815ec8484f47
Valentino-Houessou <valentino.houessou@gmail.com>
2020-10-06 01:36:51 +0200

    Create Dafalgan Benefit Updater

---
+++ src/benefitUpdater/DafalganBenefitUpdater.js
@@ -0,0 +1,7 @@
+import { BenefitUpdater } from './BenefitUpdater';
+
+export class DafalganBenefitUpdater extends BenefitUpdater {
+    updateBenefitValue(benefit, expiresIn) {
+      return "this one update Dafalgan Benefit";
+    }
+  }
