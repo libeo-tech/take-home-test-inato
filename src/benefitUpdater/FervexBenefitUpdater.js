b162d852a7bfe672a6e99c530232d8da3e66273d
Valentino-Houessou <valentino.houessou@gmail.com>
2020-10-05 22:36:19 +0200

    implement Fervex update Benefit Value

--- src/benefitUpdater/FervexBenefitUpdater.js
+++ src/benefitUpdater/FervexBenefitUpdater.js
@@ -1,7 +1,14 @@
 import { BenefitUpdater } from "./BenefitUpdater";
+import { MAX_BENEFIT_VALUE } from "../Constants";
 
 export class FervexBenefitUpdater extends BenefitUpdater {
   updateBenefitValue(benefit, expiresIn) {
-    return "This is fervex Benefit updater";
+    if (benefit < MAX_BENEFIT_VALUE) benefit++;
+    if(expiresIn <= 10 && benefit < MAX_BENEFIT_VALUE) benefit++;
+    if(expiresIn <= 5 && benefit < MAX_BENEFIT_VALUE) benefit++;
+    expiresIn--;
+    if(expiresIn < 0) benefit = 0;
+
+    return { benefit, expiresIn };
   }
 }
