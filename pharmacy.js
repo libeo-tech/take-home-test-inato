d7ea53c6691a44d6a231443194888b8ba8061536
Mdauphin42 <mdauphin@student.42.fr>
2020-11-27 11:04:46 +0100

    Make code easier to read

--- pharmacy.js
+++ pharmacy.js
@@ -12,58 +12,52 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (this.drugs[i].benefit > 50) {
-        this.drugs[i].benefit = 50;
-      }
-      switch (this.drugs[i].name) {
+      const benefit = this.drugs[i].benefit;
+      const expiresIn = this.drugs[i].expiresIn;
+      const name = this.drugs[i].name;
+
+      benefit = benefit > 50 ? 50 : benefit;
+
+      switch (name) {
         case "Herbal Tea":
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit += this.drugs[i].expiresIn > 0 ? 1 : 2;
+          if (benefit < 50) {
+            benefit += expiresIn > 0 ? 1 : 2;
           }
-          this.drugs[i].expiresIn -= 1;
-          this.drugs[i].benefit =
-            this.drugs[i].benefit > 50 ? 50 : this.drugs[i].benefit;
+          expiresIn -= 1;
+          benefit = benefit > 50 ? 50 : benefit;
           break;
         case "Magic Pill":
           break;
         case "Fervex":
-          if (this.drugs[i].expiresIn > 0) {
-            if (this.drugs[i].benefit < 50) {
-              if (this.drugs[i].expiresIn > 10) {
-                this.drugs[i].benefit += 1;
-              } else if (
-                this.drugs[i].expiresIn <= 10 &&
-                this.drugs[i].expiresIn > 5
-              ) {
-                this.drugs[i].benefit += 2;
-              } else {
-                this.drugs[i].benefit += 3;
-              }
-              this.drugs[i].benefit =
-                this.drugs[i].benefit > 50 ? 50 : this.drugs[i].benefit;
-            }
+          if (expiresIn > 10) {
+            benefit += 1;
+          } else if (expiresIn <= 10 && expiresIn > 5) {
+            benefit += 2;
+          } else if (expiresIn > 0 && expiresIn <= 5) {
+            benefit += 3;
           } else {
-            this.drugs[i].benefit = 0;
+            benefit = 0;
           }
-          this.drugs[i].expiresIn -= 1;
+          benefit = benefit > 50 ? 50 : benefit;
+          expiresIn -= 1;
           break;
         case "Dafalgan":
-          if (this.drugs[i].benefit > 0) {
-            this.drugs[i].benefit -= this.drugs[i].expiresIn > 0 ? 2 : 4;
-            this.drugs[i].benefit =
-              this.drugs[i].benefit < 0 ? 0 : this.drugs[i].benefit;
+          if (benefit > 0) {
+            benefit -= expiresIn > 0 ? 2 : 4;
+            benefit = benefit < 0 ? 0 : benefit;
           }
-          this.drugs[i].expiresIn -= 1;
+          expiresIn -= 1;
           break;
         default:
-          if (0 < this.drugs[i].benefit < 50 && this.drugs[i].benefit > 0) {
-            this.drugs[i].benefit -= this.drugs[i].expiresIn > 0 ? 1 : 2;
-            this.drugs[i].benefit =
-              this.drugs[i].benefit < 0 ? 0 : this.drugs[i].benefit;
+          if (benefit > 0) {
+            benefit -= expiresIn > 0 ? 1 : 2;
+            benefit = benefit < 0 ? 0 : benefit;
           }
-          this.drugs[i].expiresIn -= 1;
+          expiresIn -= 1;
           break;
       }
+      this.drugs[i].benefit = benefit;
+      this.drugs[i].expiresIn = expiresIn;
     }
     return this.drugs;
   }
