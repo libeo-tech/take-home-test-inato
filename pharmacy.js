8d212aa1ba56cfb6232c267b9db5b4ebad561906
Zeydi <oussema.zeidi@ieee.org>
2020-12-06 18:01:34 +0100

    Updated Drug class

--- pharmacy.js
+++ pharmacy.js
@@ -4,6 +4,26 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+
+  // Define benefits limit value
+
+  static get MAX_BENEFITS() {
+    return 50;
+  }
+  static get MIN_BENEFITS() {
+    return 0;
+  }
+
+  // Benefits update functions
+
+  increaseBenefit = (value) => {
+    this.benefit += value;
+    if (this.benefit > Drug.MAX_BENEFITS) this.benefit = Drug.MAX_BENEFITS;
+  };
+  decreaseBenefit = (value) => {
+    this.benefit -= value;
+    if (this.benefit < Drug.MIN_BENEFITS) this.benefit = Drug.MIN_BENEFITS;
+  };
 }
 
 export class Pharmacy {
@@ -13,18 +33,18 @@ export class Pharmacy {
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
+        this.drugs[i].name != 'Herbal Tea' &&
+        this.drugs[i].name != 'Fervex'
       ) {
         if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
+          if (this.drugs[i].name != 'Magic Pill') {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
         }
       } else {
         if (this.drugs[i].benefit < 50) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
+          if (this.drugs[i].name == 'Fervex') {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
@@ -38,14 +58,14 @@ export class Pharmacy {
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
+      if (this.drugs[i].name != 'Magic Pill') {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
+        if (this.drugs[i].name != 'Herbal Tea') {
+          if (this.drugs[i].name != 'Fervex') {
             if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
+              if (this.drugs[i].name != 'Magic Pill') {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
             }
