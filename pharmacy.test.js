031a27b75c9e3cd407224ba03e091880fac0e900
Lancelot <lbreon@gmail.com>
2020-12-08 14:48:16 +0100

    Add check for Herbal Tea and make some change in test structure

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -8,19 +8,13 @@ describe("Pharmacy", () => {
       ).toEqual([new Drug("test", 1, 2)]);
     });
 
-    it("benefit can not be negative", () => {
+    it("should not decrease benefit under 0", () => {
       expect(
         new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()
       ).toEqual([new Drug("test", 1, 0)]);
     });
 
-    it("benefit can not be more than 50", () => {
-      expect(
-        new Pharmacy([new Drug("Fervex", 2, 50)]).updateBenefitValue()
-      ).toEqual([new Drug("Fervex", 1, 50)]);
-    });
-
-    it("benefit decrease twice as fast once expriration date is passed", () => {
+    it("should decrease benefit twice as fast once expriration date is passed", () => {
       expect(
         new Pharmacy([new Drug("test", -1, 42)]).updateBenefitValue()
       ).toEqual([new Drug("test", -2, 40)]);
@@ -32,6 +26,26 @@ describe("Pharmacy", () => {
       expect(
         new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()
       ).toEqual([new Drug("Magic Pill", 15, 40)]);
+    });
+  });
+
+  describe("when the given drug is Herbal Tea", () => {
+    it("should increase the benefit and decrese expireIn", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 15, 40)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", 14, 41)]);
+    });
+
+    it("should not increase the benefit past 50", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 15, 50)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", 14, 50)]);
+    });
+
+    it("should increase benefit twice as fast once expriration date is passed", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", -1, 40)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", -2, 42)]);
     });
   });
 });
