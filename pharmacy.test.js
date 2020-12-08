7f0b9ae3bea3feb23e941d05692f3199a2b971ff
Lancelot <lbreon@gmail.com>
2020-12-08 14:15:09 +0100

    Add a section to test

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,22 +1,24 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
-  });
+  describe("when any drug is given", () => {
+    it("should decrease the benefit and expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
 
-  it("benefit can not be negative", () => {
-    expect(new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 0)]
-    );
-  });
+    it("benefit can not be negative", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 0)]);
+    });
 
-  it("benefit can not be more than 50", () => {
-    expect(
-      new Pharmacy([new Drug("Fervex", 2, 50)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 1, 50)]);
+    it("benefit can not be more than 50", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 2, 50)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 1, 50)]);
+    });
   });
 
   describe("when the given drug is Magic Pill", () => {
