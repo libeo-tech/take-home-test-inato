9fa5e7b2b642c0bfadde9d30749aedba7526d6a6
Lancelot <lbreon@gmail.com>
2020-12-08 13:57:32 +0100

    Add basic check for benefit

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,12 +6,24 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
   });
-  
+
+  it("benefit can not be negative", () => {
+    expect(new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 1, 0)]
+    );
+  });
+
+  it("benefit can not be more than 50", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 2, 50)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 1, 50)]);
+  });
+
   describe("when the given drug is Magic Pill", () => {
     it("should not decrease the benefit and expireIn", () => {
-      expect(new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()).toEqual(
-        [new Drug("Magic Pill", 15, 40)]
-      );
-    })
-  })
+      expect(
+        new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()
+      ).toEqual([new Drug("Magic Pill", 15, 40)]);
+    });
+  });
 });
