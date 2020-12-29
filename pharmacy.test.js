e57223f9bce9f21297b54b81532057cce5c52f8a
GustavolLo <goodluke111931@gmail.com>
2020-12-29 17:58:42 +0100

    Adding snapshot test for pharmacy example.

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -16,12 +16,18 @@ describe("Pharmacy", () => {
       new Pharmacy([new Drug("Dafalgan", 2, 1)]).updateBenefitValue()
     ).toEqual([new Drug("Dafalgan", 1, 0)]);
   });
-  it("should not have drugs with more than 50 benefit or lower than 0", () => {
-    expect(
-      new Pharmacy([new Drug("Drug", 10, 51)]).updateBenefitValue()
-    ).toEqual([new Drug("Drug", 9, 50)]);
-    expect(
-      new Pharmacy([new Drug("Drug", 10, -2)]).updateBenefitValue()
-    ).toEqual([new Drug("Drug", 9, 0)]);
+  it("should match snapshot", () => {
+    const drugs = [
+      new Drug("Doliprane", 20, 30),
+      new Drug("Herbal Tea", 10, 5),
+      new Drug("Fervex", 5, 40),
+      new Drug("Magic Pill", 15, 40)
+    ];
+    const trial = new Pharmacy(drugs);
+    const log = [];
+    for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+      log.push(JSON.stringify(trial.updateBenefitValue()));
+    }
+    expect(log).toMatchSnapshot();
   });
 });
