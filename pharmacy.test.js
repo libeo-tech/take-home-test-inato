adfcab473baf89d75047484e01363a23a248a56d
Amine OUCHIHA <ouchihaamine@gmail.com>
2021-01-19 00:49:43 +0100

    tech(remove-legacy-code): swith unit test to target migrated code and remove legacy

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,29 +1,29 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { DrugFactory } from "./pharmacy";
 
 /**
  *
  */
 describe("standard drugs cases", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+    expect(DrugFactory.getInstance("test", 2, 3).updateBenefitValue()).toEqual(
+      DrugFactory.getInstance("test", 1, 2)
     );
   });
 
   it("should decrease the benefit with 2 unit and decrease expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", -1, 1)]
+    expect(DrugFactory.getInstance("test", 0, 3).updateBenefitValue()).toEqual(
+      DrugFactory.getInstance("test", -1, 1)
     );
   });
 
   it("should stop decrease at benefit 0 and decrease expiresIn", () => {
-    expect(
-      new Pharmacy([new Drug("test", -1, 1)]).updateBenefitValue()
-    ).toEqual([new Drug("test", -2, 0)]);
+    expect(DrugFactory.getInstance("test", -1, 1).updateBenefitValue()).toEqual(
+      DrugFactory.getInstance("test", -2, 0)
+    );
 
-    expect(
-      new Pharmacy([new Drug("test", -3, 0)]).updateBenefitValue()
-    ).toEqual([new Drug("test", -4, 0)]);
+    expect(DrugFactory.getInstance("test", -3, 0).updateBenefitValue()).toEqual(
+      DrugFactory.getInstance("test", -4, 0)
+    );
   });
 });
 
@@ -33,24 +33,24 @@ describe("standard drugs cases", () => {
 describe("Herbal Tea drugs cases", () => {
   it("should increase the benefit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 9, 6)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", 8, 7)]);
+      DrugFactory.getInstance("Herbal Tea", 9, 6).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", 8, 7));
   });
 
   it("should increase the benefit with 2 units and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 0, 15)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -1, 17)]);
+      DrugFactory.getInstance("Herbal Tea", 0, 15).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", -1, 17));
   });
 
   it("should stop incresing benefit to 50 and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -1, 50)]);
+      DrugFactory.getInstance("Herbal Tea", 0, 49).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", -1, 50));
 
     expect(
-      new Pharmacy([new Drug("Herbal Tea", -1, 50)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -2, 50)]);
+      DrugFactory.getInstance("Herbal Tea", -1, 50).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", -2, 50));
   });
 });
 
@@ -60,24 +60,24 @@ describe("Herbal Tea drugs cases", () => {
 describe("Herbal Tea drugs cases", () => {
   it("should increase the benefit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 9, 6)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", 8, 7)]);
+      DrugFactory.getInstance("Herbal Tea", 9, 6).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", 8, 7));
   });
 
   it("should increase the benefit with 2 units and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 0, 15)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -1, 17)]);
+      DrugFactory.getInstance("Herbal Tea", 0, 15).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", -1, 17));
   });
 
   it("should stop incresing benefit to 50 and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -1, 50)]);
+      DrugFactory.getInstance("Herbal Tea", 0, 49).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", -1, 50));
 
     expect(
-      new Pharmacy([new Drug("Herbal Tea", -1, 50)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -2, 50)]);
+      DrugFactory.getInstance("Herbal Tea", -1, 50).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Herbal Tea", -2, 50));
   });
 });
 
@@ -87,32 +87,32 @@ describe("Herbal Tea drugs cases", () => {
 describe("Herbal Tea drugs cases", () => {
   it("should increase the benefit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 12, 6)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 11, 7)]);
+      DrugFactory.getInstance("Fervex", 12, 6).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Fervex", 11, 7));
   });
 
   it("should increase the benefit with 2 unit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 10, 20)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 9, 22)]);
+      DrugFactory.getInstance("Fervex", 10, 20).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Fervex", 9, 22));
   });
 
   it("should increase the benefit with 3 unit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 5, 20)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 4, 23)]);
+      DrugFactory.getInstance("Fervex", 5, 20).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Fervex", 4, 23));
   });
 
   it("should increase the benefit with 1 to not exceed 50 unit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 4, 49)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 3, 50)]);
+      DrugFactory.getInstance("Fervex", 4, 49).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Fervex", 3, 50));
   });
 
   it("should put benefit to 0 to not exceed 50 unit and decrease expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 0, 49)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", -1, 0)]);
+      DrugFactory.getInstance("Fervex", 0, 49).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Fervex", -1, 0));
   });
 });
 
@@ -122,7 +122,7 @@ describe("Herbal Tea drugs cases", () => {
 describe("Magic Pill drugs cases", () => {
   it("should fix Magic Pill params", () => {
     expect(
-      new Pharmacy([new Drug("Magic Pill", 1, 49)]).updateBenefitValue()
-    ).toEqual([new Drug("Magic Pill", 1, 49)]);
+      DrugFactory.getInstance("Magic Pill", 1, 49).updateBenefitValue()
+    ).toEqual(DrugFactory.getInstance("Magic Pill", 1, 49));
   });
 });
