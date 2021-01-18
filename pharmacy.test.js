6fa07816161a218e5245357e1f15ef4e3305c433
Mohammed Sami EL KHAIRA <sami.elkhaira@gmail.com>
2021-01-18 21:24:44 +0100

    Add herbal tea test cases

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -9,8 +9,20 @@ describe("Pharmacy updateBenefitValue", () => {
   });
 
   it("should do nothing for magic pill", () => {
-    expect(new Pharmacy([new Drug("Magic Pill", 44, 30)]).updateBenefitValue()).toEqual(
-      [new Drug("Magic Pill", 44, 30)]
+    expect(new Pharmacy([new Drug("Magic Pill", 30, 44)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 30, 44)]
+    );
+  });
+
+  it("should increase the benefit and decrease expiresIn for normal herbal tea", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 30, 44)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 29, 45)]
+    );
+  });
+
+  it("should increase the benefit twice faster and decrease expiresIn for expired herbal tea", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 30, 44)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 29, 46)]
     );
   });
 });
