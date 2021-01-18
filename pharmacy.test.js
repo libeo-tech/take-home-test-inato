bc53e8dc0af0b60297a674aa58455fcbc17d8408
Mohammed Sami EL KHAIRA <sami.elkhaira@gmail.com>
2021-01-18 21:18:59 +0100

    Add magic pill test case

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,10 +1,16 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
-describe("Pharmacy", () => {
+describe("Pharmacy updateBenefitValue", () => {
 
   it("should decrease the benefit and expiresIn for normal drugs", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
+    );
+  });
+
+  it("should do nothing for magic pill", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 44, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 44, 30)]
     );
   });
 });
