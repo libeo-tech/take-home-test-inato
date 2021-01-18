3b04d72064fc53dadd28b05a6b752ffed834c5e4
Amine OUCHIHA <ouchihaamine@gmail.com>
2021-01-18 22:24:55 +0100

    feat(logic-migration): migrate pharamacy logic, using Inheritance

--- pharmacy.js
+++ pharmacy.js
@@ -1,53 +1,115 @@
-const  HERBAL_TEA = "Herbal Tea";
-const  FERVEX = "Fervex";
-const  MAGIC_PILL = "Magic Pill";
+const HERBAL_TEA = "Herbal Tea";
+const FERVEX = "Fervex";
+const MAGIC_PILL = "Magic Pill";
 
 /**
  * @description build Drug instance using factory based on name
  */
 export class DrugFactory {
-  constructor() {
-   
-  }
-  
-  static getInstance(name, expiresIn, benefit){
-    switch(name){
-      case FERVEX: return new Fervex(expiresIn, benefit);
-      case MAGIC_PILL: return new MagicPill(expiresIn, benefit);
-      case HERBAL_TEA: return new HerbalTea(expiresIn, benefit);
-      default: return new Drug(name, expiresIn, benefit);
+  constructor() {}
+
+  static getInstance(name, expiresIn, benefit) {
+    switch (name) {
+      case FERVEX:
+        return new Fervex(expiresIn, benefit);
+      case MAGIC_PILL:
+        return new MagicPill(expiresIn, benefit);
+      case HERBAL_TEA:
+        return new HerbalTea(expiresIn, benefit);
+      default:
+        return new Drug(name, expiresIn, benefit);
     }
-    
   }
 }
 
 export class Drug {
   constructor(name, expiresIn, benefit) {
+    if (!name && !expiresIn && !benefit) {
+      throw new Error(
+        "Must provide all 'name' , 'expiresIn' , 'benefit' arguments"
+      );
+    }
+
+    if (typeof expiresIn !== "number" || typeof benefit !== "number") {
+      throw new Error("args 'expiresIn' and 'benefit' must be integer");
+    }
+
     this.name = name;
-    this.expiresIn = expiresIn;
-    this.benefit = benefit;
+    this.expiresIn = Math.floor(expiresIn);
+    this.benefit = Math.floor(benefit);
+  }
+
+  updateBenefitValue() {
+    if (this.benefit > 0) {
+      this.benefit--;
+    }
+
+    if (this.benefit > 0 && this.expiresIn <= 0) {
+      this.benefit--;
+    }
+
+    this.expiresIn--;
+    return this;
   }
 }
 
-export class Fervex extends Drug{
-  constructor( expiresIn, benefit) {
+export class Fervex extends Drug {
+  constructor(expiresIn, benefit) {
     super(FERVEX, expiresIn, benefit);
+  }
+
+  updateBenefitValue() {
+    if (this.expiresIn <= 0) {
+      this.benefit = 0;
+      this.expiresIn--;
+      return this;
+    }
+
+    if (this.benefit < 50) {
+      this.benefit++;
+    }
+
+    if (this.benefit < 50 && this.expiresIn < 11) {
+      this.benefit++;
+    }
+
+    if (this.benefit < 50 && this.expiresIn < 6) {
+      this.benefit++;
+    }
+
+    this.expiresIn--;
+    return this;
   }
 }
 
 export class MagicPill extends Drug {
-  constructor( expiresIn, benefit) {
+  constructor(expiresIn, benefit) {
     super(MAGIC_PILL, expiresIn, benefit);
+  }
+
+  updateBenefitValue() {
+    return this;
   }
 }
 
 export class HerbalTea extends Drug {
-  constructor( expiresIn, benefit) {
+  constructor(expiresIn, benefit) {
     super(HERBAL_TEA, expiresIn, benefit);
   }
+
+  updateBenefitValue() {
+    if (this.benefit < 50) {
+      this.benefit++;
+    }
+
+    if (this.benefit < 50 && this.expiresIn <= 0) {
+      this.benefit++;
+    }
+
+    this.expiresIn--;
+    return this;
+  }
 }
-
-
 
 export class Pharmacy {
   constructor(drugs = []) {
