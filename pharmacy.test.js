f58d507b40c4cf30a1b7e945fc313ac3fe7bf46b
Robin 'Soraaz' Partouche <robinpartouche@hotmail.fr>
2021-01-19 11:14:04 +0100

    Adding support of Dafalgan drug

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,12 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
   });
+
+  it("Dafalgan benefit decrease twice as fast than normal drug", () => {
+    const pharmacy = new Pharmacy([new Drug("Dafalgan", 2, 10)]);
+
+    expect(pharmacy.updateBenefitValue()).toEqual([new Drug("Dafalgan", 1, 9)]);
+    expect(pharmacy.updateBenefitValue()).toEqual([new Drug("Dafalgan", 0, 8)]);
+    expect(pharmacy.updateBenefitValue()).toEqual([new Drug("Dafalgan", -1, 4)]);
+  });
 });
