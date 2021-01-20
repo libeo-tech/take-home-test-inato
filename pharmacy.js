ab9b3b02821f822a9b9bad6738b1560f436e7606
unknown <omar.elaoufi@gmail.com>
2021-01-21 00:18:13 +0100

    ++ and -- instead of longer expressions

--- pharmacy.js
+++ pharmacy.js
@@ -13,35 +13,35 @@ export class Pharmacy {
       ) {
         if (this.drugs[i].benefit > 0) {
           if (this.drugs[i].name != drugNames.MAGIC_PILL) {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            this.drugs[i].benefit--;
           }
         }
       } else {
         if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          this.drugs[i].benefit++;
           if (this.drugs[i].name == drugNames.FERVEX) {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                this.drugs[i].benefit++;
               }
             }
             if (this.drugs[i].expiresIn < 6) {
               if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                this.drugs[i].benefit++;
               }
             }
           }
         }
       }
       if (this.drugs[i].name != drugNames.MAGIC_PILL) {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+        this.drugs[i].expiresIn--;
       }
       if (this.drugs[i].expiresIn < 0) {
         if (this.drugs[i].name != drugNames.HERBAL_TEA) {
           if (this.drugs[i].name != drugNames.FERVEX) {
             if (this.drugs[i].benefit > 0) {
               if (this.drugs[i].name != drugNames.MAGIC_PILL) {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
+                this.drugs[i].benefit--;
               }
             }
           } else {
@@ -50,7 +50,7 @@ export class Pharmacy {
           }
         } else {
           if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+            this.drugs[i].benefit++;
           }
         }
       }
