94bcf0bb28ea336af8dfe44d94cd7149698f006c
unknown <omar.elaoufi@gmail.com>
2021-01-21 00:28:15 +0100

    wrote general drug methods

--- drug.js
+++ drug.js
@@ -4,4 +4,25 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+
+  updateBenefitValue() {
+    this.decreaseExpiresIn();
+    if (this.expiresIn < 0) return this.decreaseBenefitValue(2);
+    return this.decreaseBenefitValue(1);
+  }
+
+  decreaseExpiresIn() {
+    this.expiresIn--;
+    return this;
+  }
+
+  increaseBenefitValue(delta = 1) {
+    this.benefit = this.benefit === 50 ? 50 : this.benefit + delta;
+    return this;
+  }
+
+  decreaseBenefitValue(delta = 1) {
+    this.benefit = this.benefit === 0 ? 0 : this.benefit - delta;
+    return this;
+  }
 }
