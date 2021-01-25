71ba7bdbf39ea2742515df8371e58b54bc7e70cc
Julien Prugne <julien@webeleon.dev>
2021-01-25 15:34:36 +0100

    implement: Dafalgan

---
+++ src/drugs/Dafalgan/Dafalgan.test.ts
@@ -0,0 +1,28 @@
+import {Dafalgan} from "./Daflagan";
+
+describe('Dafalgan', () => {
+    it('set the correct default values', () => {
+        const dafalgan = new Dafalgan();
+        expect(dafalgan).toMatchObject({
+            name: 'Dafalgan',
+            expiresIn: 10,
+            benefit: 10,
+        });
+    });
+
+    it('degrades in Benefit twice as fast as normal drugs.', () => {
+        const dafalgan = new Dafalgan();
+        dafalgan.updateBenefit()
+        expect(dafalgan).toMatchObject({
+            expiresIn: 9,
+            benefit: 8,
+        });
+
+        dafalgan.expiresIn = 0;
+        dafalgan.updateBenefit();
+        expect(dafalgan).toMatchObject({
+            expiresIn: -1,
+            benefit: 4,
+        });
+    });
+});
--- src/drugs/Drug.ts
+++ src/drugs/Drug.ts
@@ -1,3 +1,6 @@
+const BEFORE_EXPIRATION_DECAY = 1;
+const AFTER_EXPIRATION_DECAY = 2;
+
 export class Drug {
   name: string;
   expiresIn: any;
@@ -12,9 +15,9 @@ export class Drug {
   updateBenefit(): void {
     this.expiresIn--;
     if (this.expiresIn < 0) {
-      this.decreaseBenefit(2);
+      this.decreaseBenefit(AFTER_EXPIRATION_DECAY);
     } else {
-      this.decreaseBenefit(1);
+      this.decreaseBenefit(BEFORE_EXPIRATION_DECAY);
     }
   }
 
