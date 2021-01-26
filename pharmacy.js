11a3b166ae3a8a9193e741f6c156b1e39331e174
Amine Sadry <amine.sadry@gmail.com>
2021-01-26 18:35:51 +0100

    refactor: use constants instead of hard coded drugs names:
    - Export an object DRUGS containing all drugs names
    - Use strict equality to avoid type coercion

--- pharmacy.js
+++ pharmacy.js
@@ -6,21 +6,25 @@ export class Drug {
   }
 }
 
-const DAFALGAN = "Dafalgan";
+export const DRUGS = {
+  DAFALGAN: "Dafalgan",
+  FERVEX: "Fervex",
+  HERBAL_TEA: "Herbal Tea",
+  MAGIC_PILL: "Magic Pill",
+};
+
+const { DAFALGAN, FERVEX, HERBAL_TEA, MAGIC_PILL } = DRUGS;
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
+      if (this.drugs[i].name !== HERBAL_TEA && this.drugs[i].name !== FERVEX) {
         if (this.drugs[i].benefit > 0) {
           if (
-            this.drugs[i].name != "Magic Pill" &&
-            this.drugs[i].name != DAFALGAN
+            this.drugs[i].name !== MAGIC_PILL &&
+            this.drugs[i].name !== DAFALGAN
           ) {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
@@ -32,7 +36,7 @@ export class Pharmacy {
       } else {
         if (this.drugs[i].benefit < 50) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
+          if (this.drugs[i].name === FERVEX) {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
@@ -46,16 +50,16 @@ export class Pharmacy {
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
+      if (this.drugs[i].name !== MAGIC_PILL) {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
+        if (this.drugs[i].name !== HERBAL_TEA) {
+          if (this.drugs[i].name !== FERVEX) {
             if (this.drugs[i].benefit > 0) {
               if (
-                this.drugs[i].name != "Magic Pill" &&
-                this.drugs[i].name != DAFALGAN
+                this.drugs[i].name !== MAGIC_PILL &&
+                this.drugs[i].name !== DAFALGAN
               ) {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,4 +1,4 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Drug, Pharmacy, DRUGS } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
@@ -20,8 +20,8 @@ describe("Pharmacy", () => {
      * */
     it("Should never get a benefit more than 50 (max == 50)", () => {
       expect(
-        new Pharmacy([new Drug("Herbal Tea", -1, 50)]).updateBenefitValue()
-      ).toEqual([new Drug("Herbal Tea", -2, 50)]);
+        new Pharmacy([new Drug(DRUGS.HERBAL_TEA, -1, 50)]).updateBenefitValue()
+      ).toEqual([new Drug(DRUGS.HERBAL_TEA, -2, 50)]);
     });
 
     describe("When expiration has passed", () => {
@@ -36,8 +36,8 @@ describe("Pharmacy", () => {
   describe("When it is Herbal Tea", () => {
     it("Should increase benefit twice as fast after the expiration", () => {
       expect(
-        new Pharmacy([new Drug("Herbal Tea", -1, 20)]).updateBenefitValue()
-      ).toEqual([new Drug("Herbal Tea", -2, 22)]);
+        new Pharmacy([new Drug(DRUGS.HERBAL_TEA, -1, 20)]).updateBenefitValue()
+      ).toEqual([new Drug(DRUGS.HERBAL_TEA, -2, 22)]);
     });
   });
 
@@ -45,24 +45,24 @@ describe("Pharmacy", () => {
     describe("When expiration is < 10 days but > 5 days", () => {
       it("Should increase benefit by 2 ", () => {
         expect(
-          new Pharmacy([new Drug("Fervex", 7, 12)]).updateBenefitValue()
-        ).toEqual([new Drug("Fervex", 6, 14)]);
+          new Pharmacy([new Drug(DRUGS.FERVEX, 7, 12)]).updateBenefitValue()
+        ).toEqual([new Drug(DRUGS.FERVEX, 6, 14)]);
       });
     });
 
     describe("When expiration is < 5 days", () => {
       it("Should increase benefit by 3", () => {
         expect(
-          new Pharmacy([new Drug("Fervex", 3, 12)]).updateBenefitValue()
-        ).toEqual([new Drug("Fervex", 2, 15)]);
+          new Pharmacy([new Drug(DRUGS.FERVEX, 3, 12)]).updateBenefitValue()
+        ).toEqual([new Drug(DRUGS.FERVEX, 2, 15)]);
       });
     });
 
     describe("When expiration has passed", () => {
       it("Should drop benefit to 0", () => {
         expect(
-          new Pharmacy([new Drug("Fervex", -1, 12)]).updateBenefitValue()
-        ).toEqual([new Drug("Fervex", -2, 0)]);
+          new Pharmacy([new Drug(DRUGS.FERVEX, -1, 12)]).updateBenefitValue()
+        ).toEqual([new Drug(DRUGS.FERVEX, -2, 0)]);
       });
     });
   });
@@ -70,8 +70,8 @@ describe("Pharmacy", () => {
   describe("When it is Magic Pill", () => {
     it("Should never change expiration or benefit ", () => {
       expect(
-        new Pharmacy([new Drug("Magic Pill", 7, 12)]).updateBenefitValue()
-      ).toEqual([new Drug("Magic Pill", 7, 12)]);
+        new Pharmacy([new Drug(DRUGS.MAGIC_PILL, 7, 12)]).updateBenefitValue()
+      ).toEqual([new Drug(DRUGS.MAGIC_PILL, 7, 12)]);
     });
   });
 
@@ -79,12 +79,12 @@ describe("Pharmacy", () => {
     describe("When expiration has passed", () => {
       it("Should degrade benefit twice as fast as normal drugs", () => {
         expect(
-          new Pharmacy([new Drug("Dafalgan", 5, 12)]).updateBenefitValue()
-        ).toEqual([new Drug("Dafalgan", 4, 10)]);
+          new Pharmacy([new Drug(DRUGS.DAFALGAN, 5, 12)]).updateBenefitValue()
+        ).toEqual([new Drug(DRUGS.DAFALGAN, 4, 10)]);
 
         expect(
-          new Pharmacy([new Drug("Dafalgan", -1, 12)]).updateBenefitValue()
-        ).toEqual([new Drug("Dafalgan", -2, 8)]);
+          new Pharmacy([new Drug(DRUGS.DAFALGAN, -1, 12)]).updateBenefitValue()
+        ).toEqual([new Drug(DRUGS.DAFALGAN, -2, 8)]);
       });
     });
   });
