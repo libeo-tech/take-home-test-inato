ac72912f2bc5382a3b07530e53ca9992f2c6357c
Jalil Arfaoui <jalil@arfaoui.net>
2021-01-26 22:14:36 +0100

    refactor: use constants for drugs names

--- pharmacy.js
+++ pharmacy.js
@@ -1,3 +1,7 @@
+const HERBAL_TEA = "Herbal Tea";
+const FERVEX = "Fervex";
+const MAGIC_PILL = "Magic Pill";
+
 export class Drug {
   constructor(name, expiresIn, benefit) {
     this.name = name;
@@ -12,19 +16,16 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
+      if (this.drugs[i].name != HERBAL_TEA && this.drugs[i].name != FERVEX) {
         if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
+          if (this.drugs[i].name != MAGIC_PILL) {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
         }
       } else {
         if (this.drugs[i].benefit < 50) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
+          if (this.drugs[i].name == FERVEX) {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
@@ -38,14 +39,14 @@ export class Pharmacy {
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
+      if (this.drugs[i].name != MAGIC_PILL) {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
+        if (this.drugs[i].name != HERBAL_TEA) {
+          if (this.drugs[i].name != FERVEX) {
             if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
+              if (this.drugs[i].name != MAGIC_PILL) {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
             }
