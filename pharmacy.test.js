54bdc8f9cf64dade1fa08b456c12a1d2a601e834
Amine Sadry <amine.sadry@gmail.com>
2021-01-26 08:59:27 +0100

    wip: list all business rules ar tests description to be implemented
    At this point I have a running env with a good understanding of the system.
    To do :
    - Implement tests
    - Refactor existing code
    - Implement Dafalgon
    
    duration: Tue from 8am to 9am

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,54 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
   });
+
+  describe("When it is normal drug", () => {
+    it("Should never get a negative benefit (min == 0)", () => {
+      throw Error("TO BE IMPLEMENTED");
+    });
+
+    it("Should never get a benefit more than 50 (max == 50)", () => {
+      throw Error("TO BE IMPLEMENTED");
+    });
+
+    describe("When expiration has passed", () => {
+      it("Should degrade benefit twice as fast", () => {
+        throw Error("TO BE IMPLEMENTED");
+      });
+    });
+  });
+
+  describe("When it is Herbal Tea", () => {
+    it("Should increase benefit twice as fast after the expiration", () => {
+      throw Error("TO BE IMPLEMENTED");
+    });
+  });
+
+  describe("When it is Fervex", () => {
+    describe("When expiration is < 10 days but > 5 days", () => {
+      it("Should increase benefit by 2 ", () => {
+        throw Error("TO BE IMPLEMENTED");
+      });
+    });
+
+    describe("When expiration is < 5 days", () => {
+      it("Should increase benefit by 3", () => {
+        throw Error("TO BE IMPLEMENTED");
+      });
+    });
+
+    describe("When expiration has passed", () => {
+      it("Should drop benefit to 0", () => {
+        throw Error("TO BE IMPLEMENTED");
+      });
+    });
+  });
+
+  describe("When it is Dafalgan", () => {
+    describe("When expiration has passed", () => {
+      it("Should degrade benefit twice as fast as normal drugs", () => {
+        throw Error("TO BE IMPLEMENTED");
+      });
+    });
+  });
 });
