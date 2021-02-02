01851edfa3204bf043c3e1a6eeeb694bb159c412
Kajan Sivaramalingam <716149+KajanSiva@users.noreply.github.com>
2021-02-02 01:52:30 +0100

    chore: Add comments to Drug.updateBenefitValue method

--- pharmacy.js
+++ pharmacy.js
@@ -7,6 +7,7 @@ export class Drug {
 
   updateBenefitValue() {
     if (this.name != "Herbal Tea" && this.name != "Fervex") {
+      // benefit decrease for normal drugs
       if (this.benefit > 0) {
         if (this.name != "Magic Pill") {
           this.benefit = this.benefit - 1;
@@ -14,7 +15,9 @@ export class Drug {
       }
     } else {
       if (this.benefit < 50) {
+        // benefit increase for "Herbal Tea" and "Fervex"
         this.benefit = this.benefit + 1;
+        // benefit increase special cases for "Fervex"
         if (this.name == "Fervex") {
           if (this.expiresIn < 11) {
             if (this.benefit < 50) {
@@ -29,21 +32,25 @@ export class Drug {
         }
       }
     }
+    // expiresIn update
     if (this.name != "Magic Pill") {
       this.expiresIn = this.expiresIn - 1;
     }
     if (this.expiresIn < 0) {
       if (this.name != "Herbal Tea") {
         if (this.name != "Fervex") {
+          // benefit decrease for normal drugs after expiration
           if (this.benefit > 0) {
             if (this.name != "Magic Pill") {
               this.benefit = this.benefit - 1;
             }
           }
         } else {
+          // benefit set to 0 for "Fervex" after expiration
           this.benefit = this.benefit - this.benefit;
         }
       } else {
+        // benefit increase for "Herbal Tea" after expiration
         if (this.benefit < 50) {
           this.benefit = this.benefit + 1;
         }
