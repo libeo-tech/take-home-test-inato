7d2515b4f5a5cf913f2d8a2466d68d4189991733
Kajan Sivaramalingam <716149+KajanSiva@users.noreply.github.com>
2021-02-02 01:59:52 +0100

    refactor: Extract Magic Pill's logic in Drug.updateBenefitValue

--- pharmacy.js
+++ pharmacy.js
@@ -6,55 +6,55 @@ export class Drug {
   }
 
   updateBenefitValue() {
-    if (this.name != "Herbal Tea" && this.name != "Fervex") {
-      // benefit decrease for normal drugs
-      if (this.benefit > 0) {
-        if (this.name != "Magic Pill") {
-          this.benefit = this.benefit - 1;
-        }
-      }
-    } else {
-      if (this.benefit < 50) {
-        // benefit increase for "Herbal Tea" and "Fervex"
-        this.benefit = this.benefit + 1;
-        // benefit increase special cases for "Fervex"
-        if (this.name == "Fervex") {
-          if (this.expiresIn < 11) {
-            if (this.benefit < 50) {
-              this.benefit = this.benefit + 1;
-            }
-          }
-          if (this.expiresIn < 6) {
-            if (this.benefit < 50) {
-              this.benefit = this.benefit + 1;
-            }
-          }
-        }
-      }
-    }
-    // expiresIn update
-    if (this.name != "Magic Pill") {
-      this.expiresIn = this.expiresIn - 1;
-    }
-    if (this.expiresIn < 0) {
-      if (this.name != "Herbal Tea") {
-        if (this.name != "Fervex") {
-          // benefit decrease for normal drugs after expiration
+    switch (this.name) {
+      case "Magic Pill":
+        break;
+      default:
+        if (this.name != "Herbal Tea" && this.name != "Fervex") {
+          // benefit decrease for normal drugs
           if (this.benefit > 0) {
-            if (this.name != "Magic Pill") {
-              this.benefit = this.benefit - 1;
-            }
+            this.benefit = this.benefit - 1;
           }
         } else {
-          // benefit set to 0 for "Fervex" after expiration
-          this.benefit = this.benefit - this.benefit;
+          if (this.benefit < 50) {
+            // benefit increase for "Herbal Tea" and "Fervex"
+            this.benefit = this.benefit + 1;
+            // benefit increase special cases for "Fervex"
+            if (this.name == "Fervex") {
+              if (this.expiresIn < 11) {
+                if (this.benefit < 50) {
+                  this.benefit = this.benefit + 1;
+                }
+              }
+              if (this.expiresIn < 6) {
+                if (this.benefit < 50) {
+                  this.benefit = this.benefit + 1;
+                }
+              }
+            }
+          }
         }
-      } else {
-        // benefit increase for "Herbal Tea" after expiration
-        if (this.benefit < 50) {
-          this.benefit = this.benefit + 1;
+        // expiresIn update
+        this.expiresIn = this.expiresIn - 1;
+
+        if (this.expiresIn < 0) {
+          if (this.name != "Herbal Tea") {
+            if (this.name != "Fervex") {
+              // benefit decrease for normal drugs after expiration
+              if (this.benefit > 0) {
+                this.benefit = this.benefit - 1;
+              }
+            } else {
+              // benefit set to 0 for "Fervex" after expiration
+              this.benefit = this.benefit - this.benefit;
+            }
+          } else {
+            // benefit increase for "Herbal Tea" after expiration
+            if (this.benefit < 50) {
+              this.benefit = this.benefit + 1;
+            }
+          }
         }
-      }
     }
   }
 }
