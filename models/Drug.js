1c77d7f215196edd7ddb9a0b0c87b44cf15ae4eb
Matthieu Coelho <mcoelho@smatechnologies.com>
2021-02-19 16:05:48 +0100

    Add constraint on Drug to have benefit < 51 and >=0

--- models/Drug.js
+++ models/Drug.js
@@ -4,4 +4,21 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+  get benefit() {
+    return this._benefit;
+  }
+  set benefit(benefit) {
+    if (benefit >= 0) {
+      this._benefit = benefit > 50 ? 50 : benefit;
+    } else {
+      this._benefit = 0;
+    }
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      expiresIn: this.expiresIn,
+      benefit: this.benefit
+    };
+  }
 }
