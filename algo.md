5845c6df0935acaab940c8b89f9384ffa857e877
Beh <dev@behrouze.com>
2021-02-19 09:30:46 +0100

    Unit tests for existing rules

---
+++ algo.md
@@ -0,0 +1,31 @@
+# Pharmacy
+
+## Drugs
+
+Drugs have 3 characteristics : _new Drug (name, expiresIn, benefit)_
+
+**expiresIn** : number of days we have until the item expires
+
+**benefit** : how powerful the drug is
+
+## Algo
+
+### General
+
+- At the end of each day our system lowers both values for every drug
+- Once the expiration date has passed, Benefit degrades twice as fast.
+- The Benefit of an item is never more than 50
+- The Benefit of an item is never negative
+
+### Specifics
+
+- **"Herbal Tea"** actually increases in Benefit the older it gets. Benefit increases twice as fast after the expiration date.
+- **"Magic Pill"** never expires nor decreases in Benefit
+- **"Fervex"**, like Herbal Tea, increases in Benefit as its expiration date approaches
+  - Benefit increases by 2 when there are 10 days or less
+  - Benefit increasesby 3 when there are 5 days or less but
+  - Benefit drops to 0 after the expiration date
+
+### New feature
+
+We have recently signed a supplier of **"Dafalgan"** which degrades in Benefit twice as fast as normal drugs.
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,65 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Drug, Pharmacy } from './pharmacy';
 
-describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+describe('Pharmacy general rules', () => {
+  it('should decrease the benefit and expiresIn', () => {
+    expect(new Pharmacy([new Drug('test', 2, 3)]).updateBenefitValue()).toEqual([
+      new Drug('test', 1, 2)
+    ]);
+  });
+
+  it('shoudl decrease twice as fast when expiresIn < 0', () => {
+    expect(new Pharmacy([new Drug('test', -1, 3)]).updateBenefitValue()).toEqual([
+      new Drug('test', -2, 1)
+    ]);
+  });
+
+  it('should never have a Drug with negative value', () => {
+    expect(new Pharmacy([new Drug('test', 2, 0)]).updateBenefitValue()).toEqual([
+      new Drug('test', 1, 0)
+    ]);
+  });
+
+  it('should never have a Benefit > 50', () => {
+    expect(new Pharmacy([new Drug('Herbal Tea', 2, 50)]).updateBenefitValue()).toEqual([
+      new Drug('Herbal Tea', 1, 50)
+    ]);
+  });
+});
+
+describe('Pharmacy of specifif products', () => {
+  it('"Magic Pill" should never expires nor decreases in Benefit', () => {
+    expect(new Pharmacy([new Drug('Magic Pill', 2, 3)]).updateBenefitValue()).toEqual([
+      new Drug('Magic Pill', 2, 3)
+    ]);
+  });
+
+  it('"Herbal Tea" should increase the Benefit by 1 the older it gets', () => {
+    expect(new Pharmacy([new Drug('Herbal Tea', 2, 3)]).updateBenefitValue()).toEqual([
+      new Drug('Herbal Tea', 1, 4)
+    ]);
+  });
+
+  it('"Fervex" should increase the Benefit by 1 the older it gets: expire > 10 days', () => {
+    expect(new Pharmacy([new Drug('Fervex', 11, 3)]).updateBenefitValue()).toEqual([
+      new Drug('Fervex', 10, 4)
+    ]);
+  });
+
+  it('"Fervex" should increase the Benefit by 2 the older it gets: 5 < expire <= 10 days', () => {
+    expect(new Pharmacy([new Drug('Fervex', 10, 3)]).updateBenefitValue()).toEqual([
+      new Drug('Fervex', 9, 5)
+    ]);
+  });
+
+  it('"Fervex" should increase the Benefit by 3 the older it gets: expire <= 5 days', () => {
+    expect(new Pharmacy([new Drug('Fervex', 5, 3)]).updateBenefitValue()).toEqual([
+      new Drug('Fervex', 4, 6)
+    ]);
+  });
+
+  it('"Fervex" should loose all Benefit if it expires', () => {
+    expect(new Pharmacy([new Drug('Fervex', 0, 5)]).updateBenefitValue()).toEqual([
+      new Drug('Fervex', -1, 0)
+    ]);
   });
 });
