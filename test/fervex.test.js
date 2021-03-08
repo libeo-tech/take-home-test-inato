0e148e4c913bda1c48e5ae7ab533a57807f4f3d0
Sabri B <sabride@hotmail.fr>
2021-03-08 20:51:25 +0100

    TEST: Update Fervex tests

--- test/fervex.test.js
+++ test/fervex.test.js
@@ -1,33 +1,34 @@
-import { Drug, Pharmacy } from "../pharmacy";
+import { Fervex } from "../Drugs/Fervex";
+import { Pharmacy } from "../pharmacy";
 
 describe("Fervex not expired", () => {
   it("should increase by 1 the benefit and decrease by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 12, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 11, 11)]);
+      new Pharmacy([new Fervex("Fervex", 12, 10)]).updateBenefitValue()
+    ).toEqual([new Fervex("Fervex", 11, 11)]);
   });
 });
 
 describe("Fervex not expired but expiresIn between 10 and 6", () => {
   it("should increase by 2 the benefit and decrease by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 9, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 8, 12)]);
+      new Pharmacy([new Fervex("Fervex", 9, 10)]).updateBenefitValue()
+    ).toEqual([new Fervex("Fervex", 8, 12)]);
   });
 });
 
 describe("Fervex not expired but expiresIn between 5 and 1", () => {
   it("should increase by 3 the benefit and decrease by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", 4, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", 3, 13)]);
+      new Pharmacy([new Fervex("Fervex", 4, 10)]).updateBenefitValue()
+    ).toEqual([new Fervex("Fervex", 3, 13)]);
   });
 });
 
 describe("Fervex expired", () => {
   it("should drop to 0 the benefit and by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Fervex", -1, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Fervex", -2, 0)]);
+      new Pharmacy([new Fervex("Fervex", -1, 10)]).updateBenefitValue()
+    ).toEqual([new Fervex("Fervex", -2, 0)]);
   });
 });
