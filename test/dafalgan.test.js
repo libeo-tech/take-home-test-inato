4b93d2ff39173c79be37149b9aeb8441207604dd
Sabri B <sabride@hotmail.fr>
2021-03-08 20:01:09 +0100

    TEST: Update Dafalgan tests

--- test/dafalgan.test.js
+++ test/dafalgan.test.js
@@ -1,41 +1,42 @@
-import { Drug, Pharmacy } from "../pharmacy";
+import { Dafalgan } from "../Drugs/Dafalgan";
+import { Pharmacy } from "../pharmacy";
 
 describe("Dafalgan not expired", () => {
   it("should decrease by 2 the benefit and by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Dafalgan", 10, 1)]).updateBenefitValue()
-    ).toEqual([new Drug("Dafalgan", 9, 0)]);
+      new Pharmacy([new Dafalgan("Dafalgan", 10, 1)]).updateBenefitValue()
+    ).toEqual([new Dafalgan("Dafalgan", 9, 0)]);
   });
 });
 
 describe("Dafalgan expire in one day", () => {
   it("should decrease by 1 the benefit and by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Dafalgan", 2, 1)]).updateBenefitValue()
-    ).toEqual([new Drug("Dafalgan", 1, 0)]);
+      new Pharmacy([new Dafalgan("Dafalgan", 2, 1)]).updateBenefitValue()
+    ).toEqual([new Dafalgan("Dafalgan", 1, 0)]);
   });
 });
 
 describe("Dafalgan benefit already to zero", () => {
   it("should decrease by the expiresIn but not the benefit", () => {
     expect(
-      new Pharmacy([new Drug("Dafalgan", -1, 0)]).updateBenefitValue()
-    ).toEqual([new Drug("Dafalgan", -2, 0)]);
+      new Pharmacy([new Dafalgan("Dafalgan", -1, 0)]).updateBenefitValue()
+    ).toEqual([new Dafalgan("Dafalgan", -2, 0)]);
   });
 });
 
 describe("Dafalgan is expired and the benefit > 4", () => {
   it("should decrease by 4 the benefit and by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Dafalgan", -2, 4)]).updateBenefitValue()
-    ).toEqual([new Drug("Dafalgan", -3, 0)]);
+      new Pharmacy([new Dafalgan("Dafalgan", -2, 4)]).updateBenefitValue()
+    ).toEqual([new Dafalgan("Dafalgan", -3, 0)]);
   });
 });
 
 describe("Dafalgan is expired and the benefit < 4", () => {
   it("should drop to 0 the benefit and by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Dafalgan", -2, 2)]).updateBenefitValue()
-    ).toEqual([new Drug("Dafalgan", -3, 0)]);
+      new Pharmacy([new Dafalgan("Dafalgan", -2, 2)]).updateBenefitValue()
+    ).toEqual([new Dafalgan("Dafalgan", -3, 0)]);
   });
 });
