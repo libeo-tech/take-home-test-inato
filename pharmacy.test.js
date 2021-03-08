7c130155857d6d4999a91a17f2e0813d23ebdadf
Sabri B <sabride@hotmail.fr>
2021-03-08 16:13:58 +0100

    TEST: check benefit value never negative and only decrease by one when equal to 1

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -11,7 +11,23 @@ describe("Pharmacy", () => {
 describe("Dafalgan not expired", () => {
   it("should decrease by 2 the benefit and by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Dafalgan", 10, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Dafalgan", 9, 8)]);
+      new Pharmacy([new Drug("Dafalgan", 10, 1)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 9, 0)]);
+  });
+});
+
+describe("Dafalgan expire in one day", () => {
+  it("should decrease by 1 the benefit and by 1 the expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 2, 1)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 1, 0)]);
+  });
+});
+
+describe("Dafalgan benefit already to zero", () => {
+  it("should decrease by the expiresIn but not the benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", -1, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", -2, 0)]);
   });
 });
