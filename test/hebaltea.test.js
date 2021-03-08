fc80897a325208c8a60f583183a154d877b4e200
Sabri B <sabride@hotmail.fr>
2021-03-08 20:33:00 +0100

    TEST: Update HerbalTea tests

--- test/hebaltea.test.js
+++ test/hebaltea.test.js
@@ -1,17 +1,18 @@
-import { Drug, Pharmacy } from "../pharmacy";
+import { HerbalTea } from "../Drugs/HerbalTea";
+import { Pharmacy } from "../pharmacy";
 
 describe("Herbal Tea not expired", () => {
   it("should increase by 1 the benefit and decrease by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", 10, 4)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", 9, 5)]);
+      new Pharmacy([new HerbalTea("Herbal Tea", 10, 4)]).updateBenefitValue()
+    ).toEqual([new HerbalTea("Herbal Tea", 9, 5)]);
   });
 });
 
 describe("Herbal Tea expired", () => {
   it("should increase by 2 the benefit and decrease by 1 the expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Herbal Tea", -2, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Herbal Tea", -3, 12)]);
+      new Pharmacy([new HerbalTea("Herbal Tea", -2, 10)]).updateBenefitValue()
+    ).toEqual([new HerbalTea("Herbal Tea", -3, 12)]);
   });
 });
