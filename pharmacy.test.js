834d22bd8ca6b52896ef1abf142fa7b4b741a56f
Sabri B <sabride@hotmail.fr>
2021-03-08 15:44:06 +0100

    TEST: Add a first test for Dafalgan which should fail for now before building the feature

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -2,8 +2,16 @@ import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+    expect(
+      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", 1, 2)]);
+  });
+});
+
+describe("Dafalgan not expired", () => {
+  it("should decrease by 2 the benefit and by 1 the expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 10, 10)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 9, 8)]);
   });
 });
