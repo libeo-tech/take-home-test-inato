4b946618da6dc9ce2c26c95e0f2f2121e2ae5f75
Sabri B <sabride@hotmail.fr>
2021-03-08 22:46:07 +0100

    FIX: fix issue in HerbalTea updateBenefit method

--- Drugs/HerbalTea.js
+++ Drugs/HerbalTea.js
@@ -10,9 +10,8 @@ export class HerbalTea extends Drug {
   }
 
   updateBenefit() {
-    this.benefit < 50
+    this.benefit < 49
       ? (this.benefit += this.getBenefitFactor())
-      : (this.benefit = 0);
-    if (this.benefit > 50) this.benefit = 50;
+      : (this.benefit = 50);
   }
 }
