1d5c963cd79ec4fe1ace9fc0be666235b268c898
Adrien El Zein <adrien.elzein@zelros.com>
2021-03-08 07:45:47 +0100

    chore(drugs): add comments to new classes

--- src/drug/imperishable.js
+++ src/drug/imperishable.js
@@ -1,5 +1,8 @@
 import { Drug } from "./base";
 
 export class ImperishableDrug extends Drug {
+  /**
+   * Imperishable drugs do not age
+   */
   updateBenefit() {}
 }
--- src/drug/progressive.js
+++ src/drug/progressive.js
@@ -1,6 +1,10 @@
 import { Drug } from "./base";
 
 export class ProgressiveDrug extends Drug {
+  /**
+   * Progressive drugs get progressively better with age until they give no effect
+   */
+
   get _benefitVariation() {
     if (this.expiresIn <= 0) return -this.benefit;
     if (this.expiresIn <= 5) return 3;
