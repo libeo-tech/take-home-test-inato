8a1b65473717c8b60494a594519a14e3f8768ea6
Sabri B <sabride@hotmail.fr>
2021-03-08 20:17:03 +0100

    TEST: Update Magic Pills tests

--- test/magicpill.test.js
+++ test/magicpill.test.js
@@ -1,9 +1,10 @@
-import { Drug, Pharmacy } from "../pharmacy";
+import { MagicPill } from "../Drugs/MagicPill";
+import { Pharmacy } from "../pharmacy";
 
 describe("Magic Pill not expired", () => {
   it("should have the same benefit and expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("Magic Pill", 10, 10)]).updateBenefitValue()
-    ).toEqual([new Drug("Magic Pill", 10, 10)]);
+      new Pharmacy([new MagicPill("Magic Pill", 10, 10)]).updateBenefitValue()
+    ).toEqual([new MagicPill("Magic Pill", 10, 10)]);
   });
 });
