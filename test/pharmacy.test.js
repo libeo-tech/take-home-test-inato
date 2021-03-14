72d72a5fb7bd9c0d06dd01b5ba7d3ebab3b169a9
BOUSSANT-ROUX Luc <luc.boussant-roux.ext@oscaro.com>
2021-03-14 12:20:30 +0100

    test: add tests to check benefit behaviour

--- test/pharmacy.test.js
+++ test/pharmacy.test.js
@@ -6,4 +6,42 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
   });
+
+  describe("Benefit behaviour check", () => {
+    it("should decrease the benefit by one when not yet expired", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
+
+    it("should decrease the benefit twice when expired", () => {
+      expect(
+        new Pharmacy([new Drug("test", -1, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", -2, 1)]);
+    });
+
+    it("should not decrease the benefit under 0 when expired", () => {
+      expect(
+        new Pharmacy([new Drug("test", -1, 1)]).updateBenefitValue()
+      ).toEqual([new Drug("test", -2, 0)]);
+    });
+
+    it("should not decrease the benefit under 0 when not expired", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 0)]);
+    });
+
+    it("should not increase the benefit over 50 when expired", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", -1, 50)]);
+    });
+
+    it("should not increase the benefit over 50 when not expired", () => {
+      expect(
+        new Pharmacy([new Drug("Herbal Tea", 1, 50)]).updateBenefitValue()
+      ).toEqual([new Drug("Herbal Tea", 0, 50)]);
+    });
+  });
 });
