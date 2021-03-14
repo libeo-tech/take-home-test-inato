94f929f7929420e2b1e23d49a3e80a29e8c504eb
BOUSSANT-ROUX Luc <luc.boussant-roux.ext@oscaro.com>
2021-03-14 14:10:03 +0100

    refactor: add description for first test

--- test/pharmacy.test.ts
+++ test/pharmacy.test.ts
@@ -2,10 +2,12 @@ import { Pharmacy } from "../src/pharmacy";
 import { Drug } from "../src/drug";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("ExpiresIn behaviour check", () => {
+    it("should decrease expiresIn field", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
   });
 
   describe("Benefit behaviour check", () => {
