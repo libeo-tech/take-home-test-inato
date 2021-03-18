892eb9fbcf41ebed9fad633ad1d7410550808b8f
Kamel <aissaoui.k.mel@gmail.com>
2021-03-18 14:50:19 +0100

    improve test

--- pharmacy.js
+++ pharmacy.js
@@ -10,7 +10,7 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
     this.specialDrugsName = {
-      "Doliprane": this.dolipraneExpireFaster,
+      "Dafalgan": this.dafalganExpireFaster,
       "Herbal Tea": this.herbalTeaGetOlder,
       "Fervex": this.fervexGetOlder,
       "Magic Pill": this.magicalPillDoMagic,
@@ -18,43 +18,75 @@ export class Pharmacy {
     }
   }
 
+  /**
+   * 
+   * @param {*} drug 
+   * @returns 
+   */
   herbalTeaGetOlder(drug) {
+    drug = this.generic(drug);
     drug.benefit = drug.benefit + 2;
     if (drug.expiresIn < 0)
       drug.benefit = drug.benefit + 1;
-    return this.generic(drug);
+    return drug;
   }
 
+  /**
+   * 
+   * @param {*} drug 
+   * @returns 
+   */
   magicalPillDoMagic(drug) {
     //do nothing :D
     return drug;
   }
 
+  /**
+   * 
+   * @param {*} drug 
+   * @returns 
+   */
   fervexGetOlder(drug) {
+    drug = this.generic(drug);
     if (drug.expiresIn <= 10 && drug.expiresIn > 5)
       drug.benefit = drug.benefit + 3;
     if (drug.expiresIn <= 5)
       drug.benefit = drug.benefit + 4;
     if (drug.expiresIn < 0)
       drug.benefit = 0;
-    return this.generic(drug);;
+    return drug;
   }
 
+  /**
+   * 
+   * @param {*} drug 
+   * @returns 
+   */
   genericGetOlder(drug) {
     drug.benefit = drug.benefit - 1;
     drug.expiresIn = drug.expiresIn - 1;
+    if (drug.expiresIn < 0)
+      drug.benefit = drug.benefit - 1;
+    return drug;
+  }
+
+  /**
+   * 
+   * @param {*} drug 
+   * @returns 
+   */
+  dafalganExpireFaster(drug) {
+    drug = this.generic(drug);
+    drug.benefit = drug.benefit - 1;
     if (drug.expiresIn < 0)
       drug.expiresIn = drug.benefit - 1;
     return drug;
   }
 
-  // dolipraneExpireFaster(drug) {
-  //   drug.benefit = drug.benefit - 1;
-  //   if (drug.expiresIn < 0)
-  //     drug.expiresIn = drug.benefit - 1;
-  //   return this.generic(drug);
-  // }
-
+  /**
+   * 
+   * @returns 
+   */
   updateBenefitValue() {
     this.drugs = this.drugs.map((drug) => {
       if (this.specialDrugsName.hasOwnProperty(drug.name)) {
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,20 +6,39 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
 
-    // expect(new Pharmacy([new Drug("Doliprane", 20, 30)]).updateBenefitValue()).toEqual(
-    //   [new Drug("Doliprane", 19, 29)]
-    // );
-
-    expect(new Pharmacy([new Drug("Herbal Tea", 10, 5)]).updateBenefitValue()).toEqual(
-      [new Drug("Herbal Tea", 9, 6)]
+    expect(new Pharmacy([new Drug("Doliprane", 20, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Doliprane", 19, 29)]
     );
+  });
 
-    expect(new Pharmacy([new Drug("Fervex", 5, 40)]).updateBenefitValue()).toEqual(
-      [new Drug("Fervex", 4, 43)]
-    );
-
+  it("should not decrease", () => {
     expect(new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()).toEqual(
       [new Drug("Magic Pill", 15, 40)]
     );
   });
+
+  it("should increase benefit", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 10, 5)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 9, 6)]
+    );
+  });
+
+  it("should increase benefit", () => {
+    expect(new Pharmacy([new Drug("Fervex", 5, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 4, 43)]
+    );
+  });
+
+  it("benefit should be 0", () => {
+    expect(new Pharmacy([new Drug("Fervex", 0, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", -1, 0)]
+    );
+  });
+
+  it("should decrease twice as fast", () => {
+    expect(new Pharmacy([new Drug("Dafalgan", 20, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Dafalgan", 19, 28)]
+    );
+  });
+
 });
