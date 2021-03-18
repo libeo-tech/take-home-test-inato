cf303399b285a00bbbd050f700ad4a2f7bffbc80
Kamel <aissaoui.k.mel@gmail.com>
2021-03-18 13:55:30 +0100

    update test to lock feature before improve

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -5,5 +5,21 @@ describe("Pharmacy", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
+
+    // expect(new Pharmacy([new Drug("Doliprane", 20, 30)]).updateBenefitValue()).toEqual(
+    //   [new Drug("Doliprane", 19, 29)]
+    // );
+
+    expect(new Pharmacy([new Drug("Herbal Tea", 10, 5)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 9, 6)]
+    );
+
+    expect(new Pharmacy([new Drug("Fervex", 5, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 4, 43)]
+    );
+
+    expect(new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 15, 40)]
+    );
   });
 });
