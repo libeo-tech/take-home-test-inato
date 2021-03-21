7516cc79a038d4359d874f62daaeb29684d9d6cc
Alexandre Wotawa <awotawa@gmail.com>
2021-03-21 11:54:05 +0100

    added some comment for explaining choices made and deleted old commented code

--- pharmacy.js
+++ pharmacy.js
@@ -3,11 +3,10 @@ export class Pharmacy {
     this.drugs = drugs;
   }
   updateBenefitValue() {
-    //Do a switch statement, default being the regular
-    const dailyBenefitIncrease = 1;
-    for (var i = 0; i < this.drugs.length; i++) { //goes through all the drugs list
-      this.drugs[i].expiresIn--; // if it doesn't exist, the expiry date doesn't change
-      switch (this.drugs[i].name) {
+    const dailyBenefitIncrease = 1; // easier to change globally in the future
+    for (var i = 0; i < this.drugs.length; i++) {
+      this.drugs[i].expiresIn--;
+      switch (this.drugs[i].name) { // easiest way to enable adding/changing code for a drug in the future
         case "Herbal Tea":
           if (this.drugs[i].expiresIn >= 0) {
             this.drugs[i].benefit += dailyBenefitIncrease;
@@ -55,56 +54,7 @@ export class Pharmacy {
             this.drugs[i].benefit = 0;
           }
       }
-      // if (
-      //   this.drugs[i].name != "Herbal Tea" &&
-      //   this.drugs[i].name != "Fervex"
-      // ) {
-      //   // eslint-disable-next-line prettier/prettier
-      //   if (this.drugs[i].benefit > 0) { //Magic pill never expires nor decreases in Benefit
-      //     if (this.drugs[i].name != "Magic Pill") {
-      //       this.drugs[i].benefit = this.drugs[i].benefit - 1; // name a const decreaseDaily for benefit loss so that you can change this value easier
-      //     }
-      //   }
-      // } else {
-      //   if (this.drugs[i].benefit < 50) {
-      //     this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //     if (this.drugs[i].name == "Fervex") {
-      //       if (this.drugs[i].expiresIn < 11) {
-      //         if (this.drugs[i].benefit < 50) {
-      //           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //         }
-      //       }
-      //       if (this.drugs[i].expiresIn < 6) {
-      //         if (this.drugs[i].benefit < 50) {
-      //           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-      // if (this.drugs[i].name != "Magic Pill") {
-      //   this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      // }
-      // if (this.drugs[i].expiresIn < 0) {
-      //   if (this.drugs[i].name != "Herbal Tea") {
-      //     if (this.drugs[i].name != "Fervex") {
-      //       if (this.drugs[i].benefit > 0) {
-      //         if (this.drugs[i].name != "Magic Pill") {
-      //           this.drugs[i].benefit = this.drugs[i].benefit - 1; //should degrade twice as fast (-2 * decreaseDaily instead of -1 * decreaseDaily)
-      //         }
-      //       }
-      //     } else {
-      //       this.drugs[i].benefit =
-      //         this.drugs[i].benefit - this.drugs[i].benefit;
-      //     }
-      //   } else {
-      //     if (this.drugs[i].benefit < 50) {
-      //       this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //     }
-      //   }
-      // }
     }
-
     return this.drugs;
   }
 }
