4de53274f56006870a24942d7b60a80d14dd1250
chaloupm <maxime.chaloupe@grenoble-inp.org>
2021-03-25 17:35:15 +0100

    First version with sub-classing and method overriding

--- pharmacy.js
+++ pharmacy.js
@@ -4,63 +4,93 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+
+  updateBenefitValue() {
+      if (this.expiresIn < 0) {
+          this.benefit -= 2;
+      }
+      else {
+          this.benefit -= 1;
+      }
+  }
+
+  updateExpiration() {
+      this.expiresIn -= 1;
+  }
+}
+
+export class HerbalDrug extends Drug {
+    // constructor(name, expiresIn, benefit) {
+    //     super(name, expiresIn, benefit);
+    // }
+
+    updateBenefitValue() {
+        if (this.expiresIn < 0) {
+            this.benefit += 2;
+        }
+        else {
+            this.benefit += 1;
+        }
+    }
+}
+
+export class NoExpirationDrug extends Drug {
+    constructor(name, expiresIn, benefit) {
+        super(name, expiresIn, benefit);
+    }
+
+    updateBenefitValue() {
+        // do nothing
+    }
+
+    updateExpiration() {
+        // do nothing
+    }
+}
+
+export class FervexDrug extends Drug {
+    constructor(name, expiresIn, benefit) {
+        super(name, expiresIn, benefit);
+    }
+
+    updateBenefitValue() {
+        if (this.expiresIn < 0) {
+            this.benefit = 0;
+        }
+        else if (this.expiresIn <= 5) {
+            this.benefit += 3;
+        }
+        else if (this.expiresIn <= 10) {
+            this.benefit += 2;
+        }
+        else {
+            this.benefit += 1;
+        }
+    }
+
+}
+
+export class DafalganDrug extends Drug {
+    updateBenefitValue() {
+        if (this.expiresIn < 0) {
+            this.benefit -= 4;
+        }
+        else {
+            this.benefit -= 2;
+        }
+    }
 }
 
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
-    }
 
-    return this.drugs;
+  updateBenefitValue() {
+      this.drugs.forEach((drug) => {
+          drug.updateBenefitValue();
+          drug.updateExpiration();
+      });
+      return this.drugs;
   }
 }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,104 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Drug, DafalganDrug, HerbalDrug, NoExpirationDrug, FervexDrug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
+  it("should decrease the benefit and expiresIn by one", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
   });
 });
+
+describe("Pharmacy", () => {
+  it("should decrease the benefit by two and expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("test", -1, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -2, 1)]
+    );
+  });
+});
+
+// Herbal drugs
+describe("Pharmacy", () => {
+  it("should do benefit+1 and expiresIn-1", () => {
+    expect(new Pharmacy([new HerbalDrug("test", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 1, 4)]
+    );
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should do benefit+2 and expiresIn-1", () => {
+    expect(new Pharmacy([new HerbalDrug("test", -1, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -2, 8)]
+    );
+  });
+});
+
+// Drugs without expiration date
+describe("Pharmacy", () => {
+  it("should not modify benefit and expiresIn", () => {
+    expect(new Pharmacy([new NoExpirationDrug("test", -1, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -1, 6)]
+    );
+  });
+});
+
+// Drugs like Fervex
+describe("Pharmacy", () => {
+  it("should do benefit+1 and expiresIn-1", () => {
+    expect(new Pharmacy([new FervexDrug("test", 12, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 11, 7)]
+    );
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should do benefit+2 and expiresIn-1", () => {
+    expect(new Pharmacy([new FervexDrug("test", 8, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 7, 8)]
+    );
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should do benefit+2 and expiresIn-1", () => {
+    expect(new Pharmacy([new FervexDrug("test", 3, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 2, 9)]
+    );
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should do benefit+2 and expiresIn-1", () => {
+    expect(new Pharmacy([new FervexDrug("test", -1, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -2, 0)]
+    );
+  });
+});
+
+
+// Daphalgan type drug
+let daphalganDegradation = function() {
+    if (this.expiresIn < 0) {
+        this.benefit -= 4;
+    }
+    else {
+        this.benefit -= 2;
+    }
+}
+
+describe("Pharmacy", () => {
+  it("should do benefit+2 and expiresIn-1", () => {
+    expect(new Pharmacy([new DafalganDrug("Dafalgan", -1, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("Dafalgan", -2, 2)]
+    );
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should do benefit+2 and expiresIn-1", () => {
+    expect(new Pharmacy([new DafalganDrug("Dafalgan", 1, 6)]).updateBenefitValue()).toEqual(
+      [new Drug("Dafalgan", 0, 4)]
+    );
+  });
+});
+
