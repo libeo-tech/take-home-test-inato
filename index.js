80b3f9f714fd784305d048dbea4c2a4b1278271e
Tonton <yannick.bonnefond@gmail.com>
2021-03-26 17:28:34 +0100

    refactor: move trial run in own function

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { runTrial } from "./trial";
 
-import fs from "fs";
-
-const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
-];
-const trial = new Pharmacy(drugs);
-
-const log = [];
-
-for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(trial.updateBenefitValue()));
-}
-
-/* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+runTrial("output.txt", err => {
+  /* eslint-disable no-console */
   if (err) {
     console.log("error");
   } else {
     console.log("success");
   }
+  /* eslint-enable no-console */
 });
-/* eslint-enable no-console */
---
+++ trial.test.js
@@ -0,0 +1,28 @@
+import { readFileSync, unlinkSync, existsSync } from "fs";
+
+import { runTrial } from "./trial";
+
+describe("trial", () => {
+  const OUTPUT_TEST = `${__dirname}/output.test.txt`;
+  const OUTPUT_PROD = `${__dirname}/output.txt`;
+
+  beforeEach(clean);
+  afterAll(clean);
+
+  it("should produce the same output file", done => {
+    runTrial(OUTPUT_TEST, err => {
+      expect(readFile(OUTPUT_TEST)).toBe(readFile(OUTPUT_PROD));
+      done(err);
+    });
+  });
+
+  function readFile(file) {
+    return String(readFileSync(file));
+  }
+
+  function clean() {
+    if (existsSync(OUTPUT_TEST)) {
+      unlinkSync(OUTPUT_TEST);
+    }
+  }
+});
