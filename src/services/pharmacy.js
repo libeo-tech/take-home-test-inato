cf2d47b6a161c0cd8a221e003fcec2024316e02d
Roland Basset-Chercot <rolandbasset@hotmail.fr>
2021-03-31 08:56:15 +0200

    separate benefits and expiresIn

--- src/services/pharmacy.js
+++ src/services/pharmacy.js
@@ -10,6 +10,36 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+  updateExpiresIn() {
+    for (var i = 0; i < this.drugs.length; i++) {
+      if (this.drugs[i].name === "Magic Pill") {
+        // magic pill never expire
+        continue;
+      }
+      this.drugs[i].expiresIn -= 1;
+    }
+    return this.drugs;
+  }
+  updateBenefitValueNew() {
+    for (var i = 0; i < this.drugs.length; i++) {
+      // benefit cant be negative
+      if (this.drugs[i].benefit === 0) {
+        continue;
+      }
+      if (this.drugs[i].name === "Magic Pill") {
+        // magic pill never decrease benefit
+        continue;
+      }
+      // benefit decrease twice fast if expired
+      if (this.drugs[i].expiresIn < 0) {
+        this.drugs[i].benefit -= 2;
+        continue;
+      } else {
+        this.drugs[i].benefit -= 1;
+      }
+    }
+    return this.drugs;
+  }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       if (
--- tests/pharmacy.test.js
+++ tests/pharmacy.test.js
@@ -1,9 +1,45 @@
 import { Drug, Pharmacy } from "../src/services/pharmacy";
 
 describe("Pharmacy", () => {
+  // BASIC RULES
   it("should decrease the benefit and expiresIn", () => {
     expect(
-      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue(),
+      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
     ).toEqual([new Drug("test", 1, 2)]);
+  });
+  it("should decrease the expiresIn", () => {
+    expect(new Pharmacy([new Drug("test", 2, 0)]).updateExpiresIn()).toEqual([
+      new Drug("test", 1, 0)
+    ]);
+  });
+  // expiresIn can be negative
+  it("should decrease the expiresIn to negative number", () => {
+    expect(new Pharmacy([new Drug("test", 0, 0)]).updateExpiresIn()).toEqual([
+      new Drug("test", -1, 0)
+    ]);
+  });
+
+  it("should decrease the benefit", () => {
+    expect(
+      new Pharmacy([new Drug("test", 2, 1)]).updateBenefitValueNew()
+    ).toEqual([new Drug("test", 2, 0)]);
+  });
+  // benefit never negative
+  it("should let benefit to zero", () => {
+    expect(
+      new Pharmacy([new Drug("test", 0, 0)]).updateBenefitValueNew()
+    ).toEqual([new Drug("test", 0, 0)]);
+  });
+  // expiration passed -> benefit degrades twice as fast
+  it("should decrease benefit twice fast if expired", () => {
+    expect(
+      new Pharmacy([new Drug("test", -1, 2)]).updateBenefitValueNew()
+    ).toEqual([new Drug("test", -1, 0)]);
+  });
+  // SPECIFIC RULES
+  it("should let MAGIC PILL benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 1, 1)]).updateBenefitValueNew()
+    ).toEqual([new Drug("Magic Pill", 1, 1)]);
   });
 });
