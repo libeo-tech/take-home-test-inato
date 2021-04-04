328bc1532f6755623d4f0afbcd97d38897b92b5c
lizixuan <lizixuan4530@gmail.com>
2021-04-04 18:19:11 +0200

    add test cases & complete original updateBenefitValue

--- pharmacy.js
+++ pharmacy.js
@@ -12,50 +12,75 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
+      console.log(this.drugs[i].name);
       if (
         this.drugs[i].name != "Herbal Tea" &&
         this.drugs[i].name != "Fervex"
+        //if the drug is not HerbalTea nor Fervex
       ) {
         if (this.drugs[i].benefit > 0) {
+          //benefit>0
           if (this.drugs[i].name != "Magic Pill") {
+            //if the drug is not Magic Pill
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            //benefit drop 1 point => Doliprane => nomal case
+          }
+          if (this.drugs[i].name == "Dafalgan") {
+            this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            //console.log(this.drugs[i].benefit);
+            //Dafalgan => -1 again, so totally -2
           }
         }
       } else {
         if (this.drugs[i].benefit < 50) {
+          //if the drug is HerbalTea or Fervex, and the benefit < 50
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          //benefit + 1
           if (this.drugs[i].name == "Fervex") {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                //the drug is Fervex and expire in 10 days, benefit + 1 again, so toally +2
               }
             }
             if (this.drugs[i].expiresIn < 6) {
               if (this.drugs[i].benefit < 50) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                //Fervex expire in 5 days, benefit + 1 again, so toally +3
               }
             }
           }
         }
       }
+      //Beside Magic Pill, all other drugs expire date - 1
       if (this.drugs[i].name != "Magic Pill") {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
+      //if Drugs get expired
       if (this.drugs[i].expiresIn < 0) {
         if (this.drugs[i].name != "Herbal Tea") {
           if (this.drugs[i].name != "Fervex") {
             if (this.drugs[i].benefit > 0) {
               if (this.drugs[i].name != "Magic Pill") {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
+                //when normal drug Doliprane expire => benefit -1, so totally -2
+              }
+              if (this.drugs[i].name == "Dafalgan") {
+                this.drugs[i].benefit = this.drugs[i].benefit - 1;
+                //console.log(this.drugs[i].benefit);
+                //Dafalgan => -1 again, so totally -4
+                //2 times faster than normal drugs
               }
             }
           } else {
             this.drugs[i].benefit =
               this.drugs[i].benefit - this.drugs[i].benefit;
+            //when Fervex expire => benefit is always 0
           }
         } else {
           if (this.drugs[i].benefit < 50) {
             this.drugs[i].benefit = this.drugs[i].benefit + 1;
+            //when Herbal Tea expire && benefit less than 50 => benefit +1 again, so totally + 2
           }
         }
       }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,59 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  it("Normal Drugs should decrease the benefit and expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drugs", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Normal Drugs", 1, 2)]);
+  });
+  it("Expired Normal Drugs should decrease the expiresIn and the benefit 2 times", () => {
+    expect(
+      new Pharmacy([new Drug("Normal Drugs", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Normal Drugs", -1, 1)]);
+  });
+  it("Herbal Tea should decrease the expiresIn and increase the benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+  });
+  it("Expired Herbal Tea should decrease the expiresIn and increase the benefit 2 times", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -1, 5)]);
+  });
+  it("Fervex should decrease the expiresIn and increase the benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 12, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 11, 4)]);
+  });
+  it("Less than 10 days to expired Fervex should decrease the expiresIn and increase the benefit 2 times", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 10, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 9, 5)]);
+  });
+  it("Less than 5 days to expired Fervex should decrease the expiresIn and increase the benefit 3 times", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 5, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 4, 6)]);
+  });
+  it("Expired Fervex should decrease the expiresIn and the benefit is 0", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 0, 10)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", -1, 0)]);
+  });
+  it("Magic Pill never expires nor decreases in Benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 9, 10)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 9, 10)]);
+  });
+  it("Dafalgan should decrease the benefit 2 times and expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 1, 1)]);
+  });
+  it("Expired Dafalgan should decrease expiresIn and the benefit 2 times fater than Expired normal drugs", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 0, 5)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", -1, 1)]);
   });
 });
