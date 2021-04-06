60597f9f69d184e0dae47dddb5d0d21a176174c9
Yaya Abdoul Ly <yaya-abdoul.ly@etu.utc.fr>
2021-04-06 03:11:13 +0200

    Refactor code and add unit test

--- pharmacy.js
+++ pharmacy.js
@@ -10,54 +10,70 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
+
+  updateBenefitValueOfFervex(i) {
+    if (this.drugs[i].expiresIn < 0) {
+      this.drugs[i].benefit =
+        this.drugs[i].benefit - this.drugs[i].benefit;
+    } else {
+      if (this.drugs[i].benefit < 50) {
+        this.drugs[i].benefit = this.drugs[i].benefit + 1;
+        if (this.drugs[i].expiresIn < 11 && this.drugs[i].benefit < 50) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
+        }
+        if (this.drugs[i].expiresIn < 6 && this.drugs[i].benefit < 50) {
+          this.drugs[i].benefit = this.drugs[i].benefit + 1;
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
+    }
+  }
+
+  updateDefaultValue(i) {
+    if (this.drugs[i].benefit > 0) {
+      if (this.drugs[i].expiresIn >= 0) {
+        this.drugs[i].benefit = this.drugs[i].benefit - 1;
+      } else {
+        this.drugs[i].benefit = this.drugs[i].benefit - 2;
+        // make it null if benefit negatif after -2
+        if (this.drugs[i].benefit < 0) {
+          this.drugs[i].benefit = 0;
         }
+
+      }
+    }
+  }
+
+  updateHerbalTea(i) {
+    if (this.drugs[i].benefit < 50) {
+      this.drugs[i].benefit = this.drugs[i].benefit + 1;
+      if (this.drugs[i].expiresIn < 0 && this.drugs[i].benefit < 50) {
+        this.drugs[i].benefit = this.drugs[i].benefit + 1;
+      }
+    }
+  }
+
+  updateBenefitValue() {
+    for (let i = 0; i < this.drugs.length; i++) {
+
+      switch (this.drugs[i].name) {
+        case "Herbal Tea":
+          this.updateHerbalTea(i);
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+
+          break;
+
+        case "Fervex":
+          this.updateBenefitValueOfFervex(i);
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+          break;
+
+        case "Magic Pill":
+          break;
+
+        default:
+          this.updateDefaultValue(i);
+          this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+          break;
       }
     }
 
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,77 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import {
+  Drug,
+  Pharmacy
+} from "./pharmacy";
 
+//test for default
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
+    );
+  });
+});
+
+// test for herbal tea
+describe("Pharmacy-herbal-tea", () => {
+  it("should incremente the benefit and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 1, 4)]
+    );
+  });
+});
+
+describe("Pharmacy-herbal-tea", () => {
+  it("should incremente the benefit 2 faster and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", -1, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -2, 5)]
+    );
+  });
+});
+
+// Magic pill
+
+describe("Pharmacy-herbal-tea", () => {
+  it("benefit and expiresIn should stay the same", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 12, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 12, 40)]
+    );
+  });
+});
+
+// Fervex
+//less than 5
+describe("Pharmacy-herbal-tea", () => {
+  it("should incremente the benefit by 3 and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Fervex", 5, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 4, 43)]
+    );
+  });
+});
+
+// expireIn betwen 6 and 10
+describe("Pharmacy-herbal-tea", () => {
+  it("should incremente the benefit by 2 and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Fervex", 6, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 5, 42)]
+    );
+  });
+});
+
+//expireIn > 10
+describe("Pharmacy-herbal-tea", () => {
+  it("should incremente the benefit 2 and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Fervex", 11, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 10, 41)]
+    );
+  });
+});
+
+//expireIN negatif
+describe("Pharmacy-herbal-tea", () => {
+  it("should incremente the benefit 2 and decrease expiresIn", () => {
+    expect(new Pharmacy([new Drug("Fervex", -6, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", -7, 0)]
     );
   });
 });
