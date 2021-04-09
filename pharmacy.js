39d66471e4395a8128bc4423f2591ffa86ddafe8
Adrien Etienne <a.etienne92@gmail.com>
2021-04-09 09:48:03 +0200

    Pharmacy create distinct drug objects

--- pharmacy.js
+++ pharmacy.js
@@ -6,9 +6,55 @@ export class Drug {
   }
 }
 
+export class HerbalTeaDrug extends Drug {
+  constructor(name, expiresIn, benefit) {
+    super(name, expiresIn, benefit);
+  }
+}
+
+export class MagicPillDrug extends Drug {
+  constructor(name, expiresIn, benefit) {
+    super(name, expiresIn, benefit);
+  }
+}
+
+export class FervexDrug extends Drug {
+  constructor(name, expiresIn, benefit) {
+    super(name, expiresIn, benefit);
+  }
+}
+
+export class DafalganDrug extends Drug {
+  constructor(name, expiresIn, benefit) {
+    super(name, expiresIn, benefit);
+  }
+}
+
 export class Pharmacy {
   constructor(drugs = []) {
-    this.drugs = drugs;
+    this.drugs = [];
+
+    drugs.forEach(drug => {
+      if (drug.name === "Herbal Tea") {
+        this.drugs.push(
+          new HerbalTeaDrug(drug.name, drug.expiresIn, drug.benefit)
+        );
+      } else if (drug.name === "Magic Pill") {
+        this.drugs.push(
+          new MagicPillDrug(drug.name, drug.expiresIn, drug.benefit)
+        );
+      } else if (drug.name === "Fervex") {
+        this.drugs.push(
+          new FervexDrug(drug.name, drug.expiresIn, drug.benefit)
+        );
+      } else if (drug.name === "Dafalgan") {
+        this.drugs.push(
+          new DafalganDrug(drug.name, drug.expiresIn, drug.benefit)
+        );
+      } else {
+        this.drugs.push(drug);
+      }
+    });
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,46 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import {
+  Drug,
+  Pharmacy,
+  HerbalTeaDrug,
+  MagicPillDrug,
+  FervexDrug,
+  DafalganDrug
+} from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+    expect(
+      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", 1, 2)]);
+  });
+
+  describe("Drugs cast -- A little bit of polymorphism", () => {
+    it("should be an Herbal Tea", () => {
+      var drug = new Pharmacy([new Drug("Herbal Tea", 10, 5)]).drugs[0];
+      expect(drug).toBeInstanceOf(HerbalTeaDrug);
+    });
+    it("should be a Magic Pill", () => {
+      var drug = new Pharmacy([new Drug("Magic Pill", 15, 40)]).drugs[0];
+      expect(drug).toBeInstanceOf(MagicPillDrug);
+    });
+    it("should be a Fervex", () => {
+      var drug = new Pharmacy([new Drug("Fervex", 15, 40)]).drugs[0];
+      expect(drug).toBeInstanceOf(FervexDrug);
+    });
+    it("should be a Dafalgan", () => {
+      var drug = new Pharmacy([new Drug("Dafalgan", 15, 40)]).drugs[0];
+      expect(drug).toBeInstanceOf(DafalganDrug);
+    });
+    it("should just be a Drug", () => {
+      var drug = new Pharmacy([new Drug("Random Drug", 15, 40)]).drugs[0];
+      expect(drug).toBeInstanceOf(Drug);
+
+      expect(drug).not.toBeInstanceOf(HerbalTeaDrug);
+      expect(drug).not.toBeInstanceOf(MagicPillDrug);
+      expect(drug).not.toBeInstanceOf(FervexDrug);
+      expect(drug).not.toBeInstanceOf(DafalganDrug);
+    });
+  });
+});
   });
 });
