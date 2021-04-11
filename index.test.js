51afe400b38407379b0243a09fc724e0b573dc64
Ebooth <pauldejean01@gmail.com>
2021-04-11 14:06:44 +0530

    Added snapshot test to not break existing feature

---
+++ index.test.js
@@ -0,0 +1,20 @@
+import { Drug, Pharmacy } from "./pharmacy";
+
+
+it('should not change the output file while refactoring', () => {
+    const drugs = [
+        new Drug("Doliprane", 20, 30),
+        new Drug("Herbal Tea", 10, 5),
+        new Drug("Fervex", 5, 40),
+        new Drug("Magic Pill", 15, 40)
+    ];
+    const trial = new Pharmacy(drugs);
+
+    const log = [];
+
+    for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+        log.push(JSON.stringify(trial.updateBenefitValue()));
+    }
+
+    expect(JSON.stringify(log)).toMatchSnapshot()
+})
