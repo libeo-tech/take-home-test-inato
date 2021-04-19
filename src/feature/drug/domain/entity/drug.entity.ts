e8b2ec80c67b355ec0a4542e899446f74144c6a5
Duy Nghia Tran <tdnghia98@gmail.com>
2021-04-19 23:31:36 +0200

    feat(drug/benefit): benefit of an item should never be negative

--- src/feature/drug/domain/entity/drug.entity.ts
+++ src/feature/drug/domain/entity/drug.entity.ts
@@ -21,14 +21,20 @@ export class Drug {
   get benefit() {
     const daysSinceCreation = this.timeSinceCreation() / (1000 * 24 * 3600);
     let benefit;
+
+    // If the expiration date has passed
     if (this.expiresIn < 0) {
+      // Only degrade 1 point before expiration
       benefit = this._benefit - this._expiresIn;
+      // Degrade twice as fast after expiration date
       benefit -= 2 * (daysSinceCreation - this._expiresIn);
     } else {
       benefit = this._benefit - daysSinceCreation;
     }
 
-    return benefit;
+    // Benefit should never be 0
+    if (benefit >= 0) return benefit;
+    return 0;
   }
 
   private timeSinceCreation() {
