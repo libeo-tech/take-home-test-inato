19ff61f825c9522eff0ade83cfae4c8d349bea7a
Duy Nghia Tran <tdnghia98@gmail.com>
2021-04-20 20:13:21 +0200

    fix(fervex): fix benefit calculation of fervex

--- src/feature/drug/domain/entity/fervex.entity.ts
+++ src/feature/drug/domain/entity/fervex.entity.ts
@@ -11,7 +11,7 @@ export class Fervex extends Drug {
     while (daysElapsed <= daysSinceCreation) {
       const expiration = this._expiresIn - daysElapsed;
       if (expiration > 10) {
-        benefitLoss++;
+        benefitLoss--;
       } else if (expiration <= 10 && expiration > 5) {
         benefitLoss -= 2;
       } else if (expiration <= 5 && expiration >= 0) {
