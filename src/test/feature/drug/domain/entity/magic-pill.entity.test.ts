7923b30c3fd06236bb560e843c6a490eaa348570
Duy Nghia Tran <tdnghia98@gmail.com>
2021-04-20 09:17:11 +0200

    test(magic pill): create test on magic pill expiration and benefit

---
+++ src/test/feature/drug/domain/entity/magic-pill.entity.test.ts
@@ -0,0 +1,31 @@
+import { MagicPill } from "feature/drug";
+import { creationTime, dayInMs } from "./constants";
+
+describe("Magic Pill Entity", () => {
+  it("should have an expiresIn value which denotes the number of days we have until the item expires", () => {
+    const drug = new MagicPill("MagicPill", 10, 10);
+    expect(drug.expiresIn).toBe(10);
+  });
+
+  it("should have a benefit value which denotes how powerful the drug is", () => {
+    const drug = new MagicPill("MagicPill", 10, 10);
+    expect(drug.benefit).toBe(10);
+  });
+
+  it("should never lower the values of benefit and expiresIn at the end of each day", () => {
+    jest.spyOn(global.Date, "now").mockImplementation(() => creationTime);
+    const drug = new MagicPill("MagicPill", 10, 10);
+
+    jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => creationTime + dayInMs);
+    expect(drug.expiresIn).toBe(10);
+    expect(drug.benefit).toBe(10);
+  });
+  it("should never exceed 50 in benefit ", () => {
+    const veryBeneficialDrug = new MagicPill("MagicPill", 10, 100);
+    expect(veryBeneficialDrug.benefit).toBe(50);
+    const limitBeneficialDrug = new MagicPill("MagicPill", 10, 50);
+    expect(limitBeneficialDrug.benefit).toBe(50);
+  });
+});
