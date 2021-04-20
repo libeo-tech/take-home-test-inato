9560576ce8c8304a3d5a61c23e623bfb1e9310bf
Duy Nghia Tran <tdnghia98@gmail.com>
2021-04-20 20:12:54 +0200

    fix(drug/fervex): fix benefit test

--- src/test/feature/drug/domain/entity/fervex.drug.entity.test.ts
+++ src/test/feature/drug/domain/entity/fervex.drug.entity.test.ts
@@ -11,13 +11,13 @@ describe("Fervex", () => {
     const drug = new Fervex("Fervex", 10, 10);
     expect(drug.benefit).toBeCloseTo(10);
   });
-  it("should lower the values of benefit and expiresIn at the end of each day", () => {
+  it("should increase the values of benefit and decrease the value of expiresIn at the end of each day", () => {
     mockDateNow(creationTime);
     const drug = new Fervex("Fervex", 20, 20);
 
     mockDateNow(creationTime + dayInMs);
     expect(drug.expiresIn).toBe(19);
-    expect(drug.benefit).toBe(19);
+    expect(drug.benefit).toBe(21);
   });
   it("should increases in benefit by 2 when there are 10 days or less in expiration", () => {
     mockDateNow(creationTime);
@@ -25,11 +25,11 @@ describe("Fervex", () => {
 
     mockDateNow(creationTime + 10 * dayInMs);
     expect(drug.expiresIn).toBe(10);
-    expect(drug.benefit).toBe(13);
+    expect(drug.benefit).toBe(31);
 
     mockDateNow(creationTime + 11 * dayInMs);
     expect(drug.expiresIn).toBe(9);
-    expect(drug.benefit).toBe(15);
+    expect(drug.benefit).toBe(33);
   });
   it("should increases in benefit by 3 when there are 5 days or less in expiration", () => {
     mockDateNow(creationTime);
@@ -37,11 +37,11 @@ describe("Fervex", () => {
 
     mockDateNow(creationTime + 15 * dayInMs);
     expect(drug.expiresIn).toBe(5);
-    expect(drug.benefit).toBe(24);
+    expect(drug.benefit).toBe(42);
 
     mockDateNow(creationTime + 16 * dayInMs);
     expect(drug.expiresIn).toBe(4);
-    expect(drug.benefit).toBe(27);
+    expect(drug.benefit).toBe(45);
   });
   it("should drop benefit to 0 after expiration date", () => {
     mockDateNow(creationTime);
@@ -49,7 +49,7 @@ describe("Fervex", () => {
 
     mockDateNow(creationTime + 20 * dayInMs);
     expect(drug.expiresIn).toBe(0);
-    expect(drug.benefit).toBe(39);
+    expect(drug.benefit).toBe(50);
 
     mockDateNow(creationTime + 21 * dayInMs);
     expect(drug.expiresIn).toBe(-1);
