482843130b2a7a9cadedce701d2b1a10238b5ec3
Duy Nghia Tran <tdnghia98@gmail.com>
2021-04-20 08:52:45 +0200

    test(herbaltea): add herbal tea test on benefit

---
+++ src/test/feature/drug/domain/entity/herbal-tea.drug.entity.test.ts
@@ -0,0 +1,37 @@
+import { HerbalTea } from "feature/drug";
+import { creationTime, dayInMs } from "./constants";
+
+describe("Herbal Tea", () => {
+  it("should lower the values of benefit and expiresIn at the end of each day", () => {
+    jest.spyOn(global.Date, "now").mockImplementation(() => creationTime);
+    const drug = new HerbalTea("HerbalTea", 10, 10);
+
+    jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => creationTime + dayInMs);
+    expect(drug.expiresIn).toBe(9);
+    expect(drug.benefit).toBe(9);
+  });
+  it("should increases in benefit twice as fast after the expiration date", () => {
+    jest.spyOn(global.Date, "now").mockImplementation(() => creationTime);
+    const drug = new HerbalTea("HerbalTea", 10, 20);
+
+    jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => creationTime + 10 * dayInMs);
+    expect(drug.expiresIn).toBe(0);
+    expect(drug.benefit).toBe(10);
+
+    jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => creationTime + 11 * dayInMs);
+    expect(drug.expiresIn).toBe(-1);
+    expect(drug.benefit).toBe(12);
+
+    jest
+      .spyOn(global.Date, "now")
+      .mockImplementation(() => creationTime + 12 * dayInMs);
+    expect(drug.expiresIn).toBe(-2);
+    expect(drug.benefit).toBe(14);
+  });
+});
