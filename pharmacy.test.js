2d38f72ac4aa7e8ceac025a2ad20fd8c076b2e64
Tom <tomecrepont@gmail.com>
2021-04-21 14:09:08 +0200

    Added the dafalgan test

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -67,6 +67,13 @@ describe("Pharmacy", () => {
           [new Drug("Fervex", -1, 0)]
         );
       });
+      describe("Dafalgan", () => {
+        it("Dafalgan degrades in Benefit twice as fast as normal drugs.", () => {
+          expect(new Pharmacy([new Drug("Dafalgan", 5, 5)]).updateBenefitValue()).toEqual(
+            [new Drug("Dafalgan", 4, 3)]
+          );
+        });
+      });
     });
   });
 });
