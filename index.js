bcb19872b069c67ae2d35a30c42d9ac3a1c26e15
Tom <tomecrepont@gmail.com>
2021-04-21 16:38:55 +0200

    Added the dafalgan in the index Test

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const drugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug("Magic Pill", 15, 40),
+  new Drug("Dafalgan", 26, 20)
 ];
 const trial = new Pharmacy(drugs);
 
@@ -21,8 +22,7 @@ fs.writeFile("output.txt", log, err => {
   if (err) {
     console.log("error");
   } else {
-    console.log("success
-    ");
+    console.log("success");
   }
 });
 /* eslint-enable no-console */
