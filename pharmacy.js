b9d92daa693be3eeb7b9f7f5ffac8b88d3bd9f91
Tom <tomecrepont@gmail.com>
2021-04-21 17:19:03 +0200

    Added some pseudocode

--- pharmacy.js
+++ pharmacy.js
@@ -15,6 +15,7 @@ export class Drug {
   }
 
   defaultBenefitVariation() {
+    // Once the expiration date has passed, Benefit degrades twice as fast.
     return this.expiresIn > 0 ? 1 : 2
   }
 }
@@ -29,12 +30,15 @@ export class Pharmacy {
     this.drugs.forEach(drug => {
       switch (drug.name) {
         case "Herbal Tea":
+          // "Herbal Tea" actually increases in Benefit the older it gets. Benefit increases twice as fast after the expiration date.
           drug.increaseBenefitBy(drug.defaultBenefitVariation())
           break;
         case "Magic Pill":
+          // "Magic Pill" never expires nor decreases in Benefit.
           drug.expiresIn += 1
           break;
         case "Fervex":
+          // "Fervex", like Herbal Tea, increases in Benefit as its expiration date approaches. Benefit increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Benefit drops to 0 after the expiration date.
           if (drug.expiresIn <= 0) {
             drug.benefit = 0
           } else if (drug.expiresIn > 10) {
@@ -46,6 +50,7 @@ export class Pharmacy {
           }
           break;
         case "Dafalgan":
+          // "Dafalgan" degrades in Benefit twice as fast as normal drugs.
           drug.decreaseBenefitBy(drug.defaultBenefitVariation()*2)
           break;      
         default:
