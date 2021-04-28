b58dd63f82614f35fb228a2b465c7587a20a10d2
Moaad Fattali <f.moaad@gmail.com>
2021-04-28 23:03:43 +0200

    replace non regression test with reference to actual legacy code for reassurance

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,10 +1,31 @@
 import { Drug, Pharmacy } from "./pharmacy";
-import { generateOutput } from "./generateOuput.js";
-import { legacyOutput } from "./output.js";
+import { Pharmacy as LegacyPharmacy } from "./legacyPharmacy";
 
 describe("Pharmacy", () => {
   it("should not change behaviour", () => {
-    expect(generateOutput()).toEqual(legacyOutput);
+    const legacyOuput = [];
+    const output = [];
+
+    const drugs = [
+      new Drug("Doliprane", 20, 3120),
+      new Drug("Doliprane", 20, 312210),
+      new Drug("Herbal Tea", 10, 5),
+      new Drug("Herbal Tea", 101, 5),
+      new Drug("Fervex", 15, 40),
+      new Drug("Fervex", 0, 40),
+      new Drug("Fervex", 890, 40),
+      new Drug("Magic Pill", 15, 40),
+      new Drug("Magic Pill", 15, 1),
+    ];
+    const trial = new Pharmacy(drugs);
+    const legacyTrial = new LegacyPharmacy(drugs);
+
+    for (let elapsedDays = 0; elapsedDays < 300; elapsedDays++) {
+      legacyOuput.push(legacyTrial.updateBenefitValue());
+      output.push(trial.updateBenefitValue());
+    }
+
+    expect(output).toEqual(legacyOuput);
   });
 
   it("should decrease the benefit and expiresIn", () => {
