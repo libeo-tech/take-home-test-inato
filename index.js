ac22c6526f1f522cc82d68fd5e4015477bcaf8af
Moaad Fattali <f.moaad@gmail.com>
2021-04-28 20:16:45 +0200

    change output generation to generate valid JSON

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ const drugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug("Magic Pill", 15, 40),
 ];
 const trial = new Pharmacy(drugs);
 
-const log = [];
-
-for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(trial.updateBenefitValue()));
-}
+export const generateOutput = () => {
+  let log = {};
+  for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+    log[elapsedDays] = trial.updateBenefitValue();
+  }
+  return log;
+};
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+fs.writeFile("output.txt", JSON.stringify(generateOutput()), (err) => {
   if (err) {
     console.log("error");
   } else {
