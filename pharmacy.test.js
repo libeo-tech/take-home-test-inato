958be509ffaaa04600f5e0e2d802c40666f6587c
Moaad Fattali <f.moaad@gmail.com>
2021-04-28 20:36:01 +0200

    add non regression unit test for faster and more reliable feeback

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,6 +1,12 @@
 import { Drug, Pharmacy } from "./pharmacy";
+import { generateOutput } from "./generateOuput.js";
+import { legacyOutput } from "./output.js";
 
 describe("Pharmacy", () => {
+  it("should not change behaviour", () => {
+    expect(generateOutput()).toEqual(legacyOutput);
+  });
+
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
