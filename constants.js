81fe3b8ff80765f043c8394ac46aa555f9518ddc
Gaspardloriot <gaspardloriot@gmail.com>
2021-04-30 11:23:11 +0200

    feat: refact pharmacy by adding switch statements

---
+++ constants.js
@@ -0,0 +1,6 @@
+const HERBAL_TEA = "Herbal Tea";
+const FERVEX = "Fervex";
+const MAGIC_PILL = "Magic Pill";
+const DOLIPRANE = "Doliprane";
+
+export { HERBAL_TEA, FERVEX, MAGIC_PILL, DOLIPRANE };
--- pharmacy.js
+++ pharmacy.js
@@ -1,66 +1,83 @@
+import { FERVEX, MAGIC_PILL, HERBAL_TEA } from "./constants";
+
+/**
+ * Class representing a Drug
+ */
 export class Drug {
-  constructor(name, expiresIn, benefit) {
+  constructor (name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
 }
 
+/**
+ * Class representing a Pharmacy containing drugs
+ */
 export class Pharmacy {
-  constructor(drugs = []) {
+  constructor (drugs = []) {
     this.drugs = drugs;
   }
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
+  /**
+   * @description update benefit for a single drug
+   * @param {string} name
+   * @param {number} benefit
+   * @param {number} expiresIn
+   * @returns {number}
+   */
+  getUpdatedDrugBenefit (name, benefit, expiresIn) {
+    let benefitChange;
+    switch (name) {
+      case FERVEX:
+        benefitChange = this.getFervexBenefitChange(expiresIn);
+        break;
+      case MAGIC_PILL:
+        benefitChange = 0;
+        break;
+      case HERBAL_TEA:
+        benefitChange = expiresIn > 0 ? 1 : 2;
+        break;
+      default:
+        benefitChange = expiresIn > 0 ? -1 : -2;
     }
 
+    return benefit + benefitChange;
+  }
+
+  /**
+   * @description update benefits for all drugs
+   * @returns {array}
+   */
+  updateBenefitValue () {
+    this.drugs.forEach(drug => {
+      const updatedBenefit = this.getUpdatedDrugBenefit(
+        drug.name,
+        drug.benefit,
+        drug.expiresIn
+      );
+      if (updatedBenefit >= 0 && updatedBenefit <= 50) {
+        drug.benefit = updatedBenefit;
+      }
+      drug.expiresIn--;
+    });
     return this.drugs;
+  }
+
+  /**
+   * @description returns the change to a fervex's benefit
+   * @param {number} expiresIn
+   * @returns {number}
+   */
+  getFervexBenefitChange (expiresIn) {
+    switch (true) {
+      case expiresIn > 5 && expiresIn <= 10:
+        return 2;
+      case expiresIn <= 5 && expiresIn >= 0:
+        return 3;
+      case expiresIn < 0:
+        return 0;
+      default:
+        return 1;
+    }
   }
 }
