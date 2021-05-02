9ae4664269bb084c8db2886cb76ae7860de90ad7
Julien Delaigues <julien.delaigues@icloud.com>
2021-05-03 01:31:32 +0200

    Adding a getter/setter for the benefit value
    - Adding a getter/setter for the benefit value
    - Removing benefit conditional guards

--- pharmacy.js
+++ pharmacy.js
@@ -2,30 +2,36 @@ export class Drug {
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
-    this.benefit = benefit;
+    this._benefit = benefit;
+  }
+
+  get benefit() {
+    return this._benefit;
+  }
+
+  set benefit(value) {
+    if (value < 0) {
+      this._benefit = 0;
+    } else if (value > 50) {
+      this._benefit = 50;
+    } else {
+      this._benefit = value;
+    }
   }
 
   updateDrugProperties() {
     if (this.name != "Herbal Tea" && this.name != "Fervex") {
-      if (this.benefit > 0) {
-        if (this.name != "Magic Pill") {
-          this.benefit = this.benefit - 1;
-        }
+      if (this.name != "Magic Pill") {
+        this.benefit = this.benefit - 1;
       }
     } else {
-      if (this.benefit < 50) {
-        this.benefit = this.benefit + 1;
-        if (this.name == "Fervex") {
-          if (this.expiresIn < 11) {
-            if (this.benefit < 50) {
-              this.benefit = this.benefit + 1;
-            }
-          }
-          if (this.expiresIn < 6) {
-            if (this.benefit < 50) {
-              this.benefit = this.benefit + 1;
-            }
-          }
+      this.benefit = this.benefit + 1;
+      if (this.name == "Fervex") {
+        if (this.expiresIn < 11) {
+          this.benefit = this.benefit + 1;
+        }
+        if (this.expiresIn < 6) {
+          this.benefit = this.benefit + 1;
         }
       }
     }
@@ -35,18 +41,14 @@ export class Drug {
     if (this.expiresIn < 0) {
       if (this.name != "Herbal Tea") {
         if (this.name != "Fervex") {
-          if (this.benefit > 0) {
-            if (this.name != "Magic Pill") {
-              this.benefit = this.benefit - 1;
-            }
+          if (this.name != "Magic Pill") {
+            this.benefit = this.benefit - 1;
           }
         } else {
           this.benefit = this.benefit - this.benefit;
         }
       } else {
-        if (this.benefit < 50) {
-          this.benefit = this.benefit + 1;
-        }
+        this.benefit = this.benefit + 1;
       }
     }
   }
@@ -56,6 +58,7 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+
   updateBenefitValue() {
     this.drugs.forEach(drug => drug.updateDrugProperties());
     return this.drugs;
