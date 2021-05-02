7ad32a045d2ebc51976acb9852db17bd5f0eb50b
Julien Delaigues <julien.delaigues@icloud.com>
2021-05-03 01:25:58 +0200

    Unit test code disambiguation

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -2,8 +2,11 @@ import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+    const drugs = [new Drug("test", 2, 3)];
+    const pharmacy = new Pharmacy(drugs);
+
+    const expectedDrugs = [new Drug("test", 1, 2)];
+
+    expect(pharmacy.updateBenefitValue()).toEqual(expectedDrugs);
   });
 });
