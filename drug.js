20f5dea17842996b91a56a5e1189b595e9b0a1c3
ngako <laurent.ngako@gmail.com>
2021-05-02 14:16:52 +0200

    create one class who extend drug class to represent each drug

---
+++ drug.js
@@ -0,0 +1,87 @@
+export default class Drug {
+  constructor(name, expiresIn, benefit) {
+    this.name = name;
+    this.expiresIn = expiresIn;
+    this.benefit = benefit;
+  }
+  decreaseBenefit() {
+    if (this.benefit >= 1) {
+      this.benefit -= 1;
+    }
+  }
+  increaseBenefit() {
+    this.benefit += 1;
+  }
+  updateExpiresIn() {
+    if (this.benefit < 50) {
+      this.expiresIn -= 1;
+    }
+  }
+  updateDataAfterOneDay() {
+    this.updateExpiresIn();
+    this.decreaseBenefit();
+  }
+}
+
+class Doliprane extends Drug {
+  constructor(expiresIn, benefit) {
+    super("Doliprane", expiresIn, benefit);
+  }
+}
+
+class HerbalTea extends Drug {
+  constructor(expiresIn, benefit) {
+    super("Herbal Tea", expiresIn, benefit);
+  }
+  decreaseBenefit() {}
+  increaseBenefit() {
+    if (this.expiresIn < 0) {
+      this.benefit += 2;
+    } else {
+      this.benefit += 1;
+    }
+  }
+  updateDataAfterOneDay() {
+    this.updateExpiresIn();
+    this.increaseBenefit();
+  }
+}
+
+class Fervex extends Drug {
+  constructor(expiresIn, benefit) {
+    super("Fervex", expiresIn, benefit);
+  }
+  decreaseBenefit() {}
+  increaseBenefit() {
+    if (this.expiresIn < 11 && this.expiresIn >= 6) {
+      this.benefit += 2;
+    } else if (this.expiresIn < 6 && this.expiresIn >= 0) {
+      this.benefit += 3;
+    } else {
+      this.benefit = 0;
+    }
+  }
+  updateDataAfterOneDay() {
+    this.updateExpiresIn();
+    this.increaseBenefit();
+  }
+}
+
+class MagicPill extends Drug {
+  constructor(expiresIn, benefit) {
+    super("Magic Pill", expiresIn, benefit);
+  }
+  decreaseBenefit() {}
+  updateExpiresIn() {}
+}
+
+class Dafalgan extends Drug {
+  constructor(expiresIn, benefit) {
+    super("Dafalgan", expiresIn, benefit);
+  }
+  decreaseBenefit() {
+    this.benefit -= 2;
+  }
+}
+
+export { Doliprane, HerbalTea, Fervex, MagicPill, Dafalgan };
