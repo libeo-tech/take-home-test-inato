61ceb84a548e25fcccf03958511fd3bf38e6e4ed
Damien Joris <damien.joris@promyze.com>
2021-05-17 19:25:36 +0200

    Add test for the entire Doliprane suite

--- pharmacy.js
+++ pharmacy.js
@@ -10,43 +10,52 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
+      const drugName = this.drugs[i].name;
+
+      if (drugName == "Doliprane") {
         if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
+          this.drugs[i].benefit = this.drugs[i].benefit - 1;
+        }
+
+        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+
+        if (this.drugs[i].expiresIn < 0 && this.drugs[i].benefit > 0) {
+          this.drugs[i].benefit = this.drugs[i].benefit - 1;
+        }
+
+        continue;
+      }
+
+      const allButHerbalTeaOrFervex =
+        drugName != "Herbal Tea" && drugName != "Fervex";
+
+      if (allButHerbalTeaOrFervex) {
+        if (this.isBenefitsAtLeastMoreThan0(i)) {
+          const allButMagicPills = drugName != "Magic Pill";
+          if (allButMagicPills) {
+            this.substractOneFromBenefits(i);
           }
         }
       } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
+        if (this.isBenefitsLessThan50(i)) {
+          this.drugs[i].benefit = this.addOneToBenefits(this.drugs[i].benefit);
+          if (drugName == "Fervex") {
+            this.getBenefitsForFervex(i);
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
+      if (drugName != "Magic Pill") {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
+        if (drugName != "Herbal Tea") {
+          if (drugName != "Fervex") {
+            if (this.isBenefitsAtLeastMoreThan0(i)) {
+              if (drugName != "Magic Pill") {
+                this.substractOneFromBenefits(i);
               }
             }
           } else {
@@ -54,13 +63,44 @@ export class Pharmacy {
               this.drugs[i].benefit - this.drugs[i].benefit;
           }
         } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          if (this.isBenefitsLessThan50(i)) {
+            this.drugs[i].benefit = this.addOneToBenefits(
+              this.drugs[i].benefit
+            );
           }
         }
       }
     }
 
     return this.drugs;
+  }
+
+  isBenefitsAtLeastMoreThan0(i) {
+    return this.drugs[i].benefit > 0;
+  }
+
+  isBenefitsLessThan50(i) {
+    return this.drugs[i].benefit < 50;
+  }
+
+  getBenefitsForFervex(i) {
+    if (this.drugs[i].expiresIn < 11) {
+      if (this.drugs[i].benefit < 50) {
+        this.drugs[i].benefit = this.addOneToBenefits(this.drugs[i].benefit);
+      }
+    }
+    if (this.drugs[i].expiresIn < 6) {
+      if (this.drugs[i].benefit < 50) {
+        this.drugs[i].benefit = this.addOneToBenefits(this.drugs[i].benefit);
+      }
+    }
+  }
+
+  addOneToBenefits(actualBenefit) {
+    return actualBenefit + 1;
+  }
+
+  substractOneFromBenefits(i) {
+    this.drugs[i].benefit = this.drugs[i].benefit - 1;
   }
 }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -119,6 +119,100 @@ describe("Black box testing", () => {
   });
 });
 
+describe("doliprane testing", () => {
+  it("should descrease Doliprane for 1 days ", () => {
+    const drugs = [new Drug("Doliprane", 20, 30)];
+    const trial = new Pharmacy(drugs);
+
+    const log = computeLogs(trial, 1);
+
+    expect(log).toEqual(['[{"name":"Doliprane","expiresIn":19,"benefit":29}]']);
+  });
+
+  it("should descrease Doliprane for 2 days ", () => {
+    const drugs = [new Drug("Doliprane", 20, 30)];
+    const trial = new Pharmacy(drugs);
+
+    const log = computeLogs(trial, 2);
+
+    expect(log).toEqual([
+      '[{"name":"Doliprane","expiresIn":19,"benefit":29}]',
+      '[{"name":"Doliprane","expiresIn":18,"benefit":28}]',
+    ]);
+  });
+
+  it("should descrease Doliprane for 21 days ", () => {
+    const drugs = [new Drug("Doliprane", 20, 30)];
+    const trial = new Pharmacy(drugs);
+
+    const log = computeLogs(trial, 21);
+
+    expect(log).toEqual([
+      '[{"name":"Doliprane","expiresIn":19,"benefit":29}]',
+      '[{"name":"Doliprane","expiresIn":18,"benefit":28}]',
+      '[{"name":"Doliprane","expiresIn":17,"benefit":27}]',
+      '[{"name":"Doliprane","expiresIn":16,"benefit":26}]',
+      '[{"name":"Doliprane","expiresIn":15,"benefit":25}]',
+      '[{"name":"Doliprane","expiresIn":14,"benefit":24}]',
+      '[{"name":"Doliprane","expiresIn":13,"benefit":23}]',
+      '[{"name":"Doliprane","expiresIn":12,"benefit":22}]',
+      '[{"name":"Doliprane","expiresIn":11,"benefit":21}]',
+      '[{"name":"Doliprane","expiresIn":10,"benefit":20}]',
+      '[{"name":"Doliprane","expiresIn":9,"benefit":19}]',
+      '[{"name":"Doliprane","expiresIn":8,"benefit":18}]',
+      '[{"name":"Doliprane","expiresIn":7,"benefit":17}]',
+      '[{"name":"Doliprane","expiresIn":6,"benefit":16}]',
+      '[{"name":"Doliprane","expiresIn":5,"benefit":15}]',
+      '[{"name":"Doliprane","expiresIn":4,"benefit":14}]',
+      '[{"name":"Doliprane","expiresIn":3,"benefit":13}]',
+      '[{"name":"Doliprane","expiresIn":2,"benefit":12}]',
+      '[{"name":"Doliprane","expiresIn":1,"benefit":11}]',
+      '[{"name":"Doliprane","expiresIn":0,"benefit":10}]',
+      '[{"name":"Doliprane","expiresIn":-1,"benefit":8}]',
+    ]);
+  });
+
+  it("should descrease Doliprane for 30 days ", () => {
+    const drugs = [new Drug("Doliprane", 20, 30)];
+    const trial = new Pharmacy(drugs);
+
+    const log = computeLogs(trial, 30);
+
+    expect(log).toEqual([
+      '[{"name":"Doliprane","expiresIn":19,"benefit":29}]',
+      '[{"name":"Doliprane","expiresIn":18,"benefit":28}]',
+      '[{"name":"Doliprane","expiresIn":17,"benefit":27}]',
+      '[{"name":"Doliprane","expiresIn":16,"benefit":26}]',
+      '[{"name":"Doliprane","expiresIn":15,"benefit":25}]',
+      '[{"name":"Doliprane","expiresIn":14,"benefit":24}]',
+      '[{"name":"Doliprane","expiresIn":13,"benefit":23}]',
+      '[{"name":"Doliprane","expiresIn":12,"benefit":22}]',
+      '[{"name":"Doliprane","expiresIn":11,"benefit":21}]',
+      '[{"name":"Doliprane","expiresIn":10,"benefit":20}]',
+      '[{"name":"Doliprane","expiresIn":9,"benefit":19}]',
+      '[{"name":"Doliprane","expiresIn":8,"benefit":18}]',
+      '[{"name":"Doliprane","expiresIn":7,"benefit":17}]',
+      '[{"name":"Doliprane","expiresIn":6,"benefit":16}]',
+      '[{"name":"Doliprane","expiresIn":5,"benefit":15}]',
+      '[{"name":"Doliprane","expiresIn":4,"benefit":14}]',
+      '[{"name":"Doliprane","expiresIn":3,"benefit":13}]',
+      '[{"name":"Doliprane","expiresIn":2,"benefit":12}]',
+      '[{"name":"Doliprane","expiresIn":1,"benefit":11}]',
+      '[{"name":"Doliprane","expiresIn":0,"benefit":10}]',
+      '[{"name":"Doliprane","expiresIn":-1,"benefit":8}]',
+      '[{"name":"Doliprane","expiresIn":-2,"benefit":6}]',
+      '[{"name":"Doliprane","expiresIn":-3,"benefit":4}]',
+      '[{"name":"Doliprane","expiresIn":-4,"benefit":2}]',
+      '[{"name":"Doliprane","expiresIn":-5,"benefit":0}]',
+      '[{"name":"Doliprane","expiresIn":-6,"benefit":0}]',
+      '[{"name":"Doliprane","expiresIn":-7,"benefit":0}]',
+      '[{"name":"Doliprane","expiresIn":-8,"benefit":0}]',
+      '[{"name":"Doliprane","expiresIn":-9,"benefit":0}]',
+      '[{"name":"Doliprane","expiresIn":-10,"benefit":0}]',
+    ]);
+  });
+});
+
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
