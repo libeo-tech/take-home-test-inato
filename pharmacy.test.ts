8c985c6cd5ef36eea3fded871fde60851b877769
Miel Nicolas <miel.nicolas@gmail.com>
2021-05-25 23:13:22 +0200

    switch test in typescript

--- pharmacy.test.ts
+++ pharmacy.test.ts
@@ -1,10 +1,10 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  test("should throw an error", () => {
+    expect(
+      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+    ).toThrowError(Error("Unknown drug test"));
   });
 });
 
