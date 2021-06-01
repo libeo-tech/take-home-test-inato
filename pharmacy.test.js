de8352cc8b257fb10f57d7195997619c73e618a4
victor-michaud <michaudvictor@gmail.com>
2021-06-01 13:08:18 +0200

    :white_check_mark: implements test cases

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,95 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+
+  it("default drug - should decrease the benefit and expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("default-drug", 10, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("default-drug", 9, 9)]
     );
   });
+
+  it("default drug - should decrease the benefit by 2 and expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("default-drug", 0, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("default-drug", -1, 8)]
+    );
+  });
+
+  it("default drug - should decrease the benefit by 2 and expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("default-drug", 0, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("default-drug", -1, 8)]
+    );
+  });
+
+  it("default drug - should only decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("default-drug", 100, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("default-drug", 99, 0)]
+    );
+  });
+
+  it("default drug - should never be above 50", () => {
+    expect(new Pharmacy([new Drug("default-drug", 100, 52)]).updateBenefitValue()).toEqual(
+      [new Drug("default-drug", 99, 51)]
+    );
+  });
+
+  it("Herbal Tead - should increase the benefit by 1 and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 10, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 9, 11)]
+    );
+  });
+
+  it("Herbal Tead - should increase the benefit by 2 and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 0, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -1, 12)]
+    );
+  });
+
+  it("Herbal Tead - should increase the benefit by 2 and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 0, 50)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -1, 50)]
+    );
+  });
+
+  it("Magic Pill - should not decrease the benefit or expiresIn", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 10, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 10, 10)]
+    );
+  });
+
+  it("Fervex - should increase the benefit by one and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Fervex", 20, 20)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 19, 21)]
+    );
+  });
+
+  it("Fervex - should not increase benefit by one and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Fervex", 20, 50)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 19, 50)]
+    );
+  });
+
+  it("Fervex - should increase benefit by two and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Fervex", 10, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 9, 12)]
+    );
+  });
+
+  it("Fervex - should increase benefit by three and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Fervex", 5, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 4, 13)]
+    );
+  });
+
+  it("Fervex - should drop benefit to zero and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Fervex", 0, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", -1, 0)]
+    );
+  });
+
+  it("Dafalgan - should decrease benefit by two and decrease expiresIn by one", () => {
+    expect(new Pharmacy([new Drug("Fervex", 10, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 9, 8)]
+    );
+  });
+
 });
