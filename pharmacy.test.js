230bd688902e9642084bb6e9d04d131b120aa6db
arymaina <55096563+arymaina@users.noreply.github.com>
2021-06-11 03:51:21 +0200

    adde unitest for the new methods
    added some unitest to test each of the new methods of the drug class

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,26 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
-describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+describe("Drug", () => {
+  it("should decrease the benefit ", () => {
+    expect(new Drug("test", 2, 3).decreaseBenefitValue(1)).toEqual(2);
   });
+  it("should decrease the benefit ", () => {
+    expect(new Drug("test", 2, 0).decreaseBenefitValue(1)).toEqual(0);
+  });
+
+  it("should increase the benefit ", () => {
+    expect(new Drug("test", 2, 7).increaseBenefitValue(1)).toEqual(8);
+  });
+  it("should increase the benefit ", () => {
+    expect(new Drug("test", 2, 50).increaseBenefitValue(5)).toEqual(50);
+  });
+
+
+
+  it("should drop the benefit to ZERO ", () => {
+    expect(new Drug("test", 2, 50).dropBenefitToZero(5)).toEqual(0);
+  });
+  
 });
+
+
