e8f7b4a4158da99c7a249a1bd14fa2d0f63fc1ea
anna.platonova <anna.platonova@arondor.com>
2021-06-19 18:52:16 +0200

    Rafactoring update Fervex

--- pharmacy.js
+++ pharmacy.js
@@ -27,7 +27,10 @@ export class Pharmacy {
           break;
         case this.MAGIC_PILL:
           //do nothing
-            break;
+          break;
+        case this.FERVEX:
+          this.updateFervex(this.drugs[i]);
+          break;
         default:
           this.updateNormalDrug(this.drugs[i]);
       }
@@ -35,6 +38,19 @@ export class Pharmacy {
     }
 
     return this.drugs;
+  }
+
+  updateFervex(drug) { 
+    drug.expiresIn = drug.expiresIn - 1;
+    if (drug.expiresIn > 10) {
+      drug.benefit = this.increaseBenefit(drug.benefit, 1);
+    } else if (drug.expiresIn > 5) {
+      drug.benefit = this.increaseBenefit(drug.benefit, 2);
+    } else if (drug.expiresIn >= 0) { 
+      drug.benefit = this.increaseBenefit(drug.benefit, 3);
+    } else {
+      drug.benefit = 0;
+    }
   }
 
   updateHerbalTea(drug) {
