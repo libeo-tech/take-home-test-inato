50e2dff37ce0a1a3393e1050f9f02d376bde4d38
r.bideau <7304827+rbideau@users.noreply.github.com>
2021-06-19 21:34:45 +0200

    Refactors pharmacy
    To simplify updateBenefitValue logic

--- pharmacy.js
+++ pharmacy.js
@@ -16,56 +16,57 @@ export class Pharmacy {
     this.drugs = drugs;
   }
   updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
+    for (let i = 0; i < this.drugs.length; i++) {
+      const drug = this.drugs[i];
+      const initialBenefit = drug.benefit;
+      const initialExpiresIn = drug.expiresIn;
+
+      // base case: lower benefit by one, two if expiry has pass
+      this.setBenefitToDrug(
+        drug,
+        initialBenefit - (initialExpiresIn > 0 ? 1 : 2)
+      );
+      // then lower expiresIn by one
+      drug.expiresIn = initialExpiresIn - 1;
+
+      // special case: "Herbal Tea" increases in benefit by one, two if expiry has pass
+      if (drug.name === "Herbal Tea") {
+        this.setBenefitToDrug(
+          drug,
+          initialBenefit + (initialExpiresIn > 0 ? 1 : 2)
+        );
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+
+      // special case: "Magic Pill" never expires nor decreases in Benefit.
+      if (drug.name === "Magic Pill") {
+        this.setBenefitToDrug(drug, initialBenefit);
+        drug.expiresIn = initialExpiresIn;
       }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
+
+      // special case: "Fervex"
+      if (drug.name === "Fervex") {
+        if (initialExpiresIn <= 0) {
+          // benefit drops to 0 after the expiration date
+          this.setBenefitToDrug(drug, 0);
         } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          if (initialExpiresIn <= 5) {
+            // benefit increase by 3 when there are 5 days or less before expiration date
+            this.setBenefitToDrug(drug, initialBenefit + 3);
+          } else if (initialExpiresIn <= 10) {
+            // benefit increase by 2 when there are 10 days and more than 5 before expiration date
+            this.setBenefitToDrug(drug, initialBenefit + 2);
+          } else {
+            // benefit increase by one otherwise
+            this.setBenefitToDrug(drug, initialBenefit + 1);
           }
         }
       }
     }
 
     return this.drugs;
+  }
+
+  setBenefitToDrug(drug, benefit) {
+    drug.benefit = Math.min(50, Math.max(benefit, 0));
   }
 }
