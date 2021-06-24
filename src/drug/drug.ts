f63835a6beee131ec95f3d6a2ab4212774d2735c
Yann Donazzolo <ydz0l038@gmail.com>
2021-06-24 09:00:42 +0200

    [Refactor] Use properties classes to update drugs

--- src/drug/drug.ts
+++ src/drug/drug.ts
@@ -1,60 +1,23 @@
+import { DefaultDrugProperties } from "./defaultDrugsProperties";
+import { SpecialDrugs } from "./specialDrugsList";
+
 export class Drug {
     name: string;
     expiresIn: number;
     benefit: number;
+    #properties: DefaultDrugProperties;
     constructor(name: string, expiresIn: number, benefit: number) {
         this.name = name;
         this.expiresIn = expiresIn;
         this.benefit = benefit;
+        this.#properties = SpecialDrugs[this.name]
+            ? new SpecialDrugs[this.name]()
+            : new DefaultDrugProperties();
     }
 
     update(): void {
-        if (
-            this.name != "Herbal Tea" &&
-            this.name != "Fervex"
-        ) {
-            if (this.benefit > 0) {
-                if (this.name != "Magic Pill") {
-                    this.benefit = this.benefit - 1;
-                }
-            }
-        } else {
-            if (this.benefit < 50) {
-                this.benefit = this.benefit + 1;
-                if (this.name == "Fervex") {
-                    if (this.expiresIn < 11) {
-                        if (this.benefit < 50) {
-                            this.benefit = this.benefit + 1;
-                        }
-                    }
-                    if (this.expiresIn < 6) {
-                        if (this.benefit < 50) {
-                            this.benefit = this.benefit + 1;
-                        }
-                    }
-                }
-            }
-        }
-        if (this.name != "Magic Pill") {
-            this.expiresIn = this.expiresIn - 1;
-        }
-        if (this.expiresIn < 0) {
-            if (this.name != "Herbal Tea") {
-                if (this.name != "Fervex") {
-                    if (this.benefit > 0) {
-                        if (this.name != "Magic Pill") {
-                            this.benefit = this.benefit - 1;
-                        }
-                    }
-                } else {
-                    this.benefit =
-                        this.benefit - this.benefit;
-                }
-            } else {
-                if (this.benefit < 50) {
-                    this.benefit = this.benefit + 1;
-                }
-            }
-        }
+        this.expiresIn = this.#properties.updateExpiration(this.expiresIn);
+        this.benefit = this.#properties.updateBenefit(this.benefit, this.expiresIn);
+        this.benefit = this.#properties.sanitizeBenefit(this.benefit);
     }
 }
