440c16d520ea3140315618f19f669cdcdfe267f2
Yann Donazzolo <ydz0l038@gmail.com>
2021-06-24 08:30:50 +0200

    [Refactor] Move drug logic to druc class

--- src/drug/drug.ts
+++ src/drug/drug.ts
@@ -3,9 +3,58 @@ export class Drug {
     expiresIn: number;
     benefit: number;
     constructor(name: string, expiresIn: number, benefit: number) {
-      this.name = name;
-      this.expiresIn = expiresIn;
-      this.benefit = benefit;
+        this.name = name;
+        this.expiresIn = expiresIn;
+        this.benefit = benefit;
     }
-  }
-  
+
+    update(): void {
+        if (
+            this.name != "Herbal Tea" &&
+            this.name != "Fervex"
+        ) {
+            if (this.benefit > 0) {
+                if (this.name != "Magic Pill") {
+                    this.benefit = this.benefit - 1;
+                }
+            }
+        } else {
+            if (this.benefit < 50) {
+                this.benefit = this.benefit + 1;
+                if (this.name == "Fervex") {
+                    if (this.expiresIn < 11) {
+                        if (this.benefit < 50) {
+                            this.benefit = this.benefit + 1;
+                        }
+                    }
+                    if (this.expiresIn < 6) {
+                        if (this.benefit < 50) {
+                            this.benefit = this.benefit + 1;
+                        }
+                    }
+                }
+            }
+        }
+        if (this.name != "Magic Pill") {
+            this.expiresIn = this.expiresIn - 1;
+        }
+        if (this.expiresIn < 0) {
+            if (this.name != "Herbal Tea") {
+                if (this.name != "Fervex") {
+                    if (this.benefit > 0) {
+                        if (this.name != "Magic Pill") {
+                            this.benefit = this.benefit - 1;
+                        }
+                    }
+                } else {
+                    this.benefit =
+                        this.benefit - this.benefit;
+                }
+            } else {
+                if (this.benefit < 50) {
+                    this.benefit = this.benefit + 1;
+                }
+            }
+        }
+    }
+}
