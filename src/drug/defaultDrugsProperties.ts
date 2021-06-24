a96377f9a04dd1d266e8414925c87bc8f74ab101
Yann Donazzolo <ydz0l038@gmail.com>
2021-06-24 09:18:37 +0200

    [Fix] Fix lint warnings and minor clean

--- src/drug/defaultDrugsProperties.ts
+++ src/drug/defaultDrugsProperties.ts
@@ -1,18 +1,17 @@
 export class DefaultDrugProperties {
-    updateBenefit(benefit: number, expiration: number): number {
-      if (expiration >= 0) return benefit - 1;
-      return benefit - 2;
-    }
-  
-    sanitizeBenefit(benefit: number): number {
-      let sanitizedBenefit = benefit;
-      sanitizedBenefit = Math.max(sanitizedBenefit, 0);
-      sanitizedBenefit = Math.min(sanitizedBenefit, 50);
-      return sanitizedBenefit;
-    }
-  
-    updateExpiration(expiration: number): number {
-      return expiration - 1;
-    }
+  updateBenefit(benefit: number, expiration: number): number {
+    if (expiration >= 0) return benefit - 1;
+    return benefit - 2;
   }
-  
+
+  sanitizeBenefit(benefit: number): number {
+    let sanitizedBenefit = benefit;
+    sanitizedBenefit = Math.max(sanitizedBenefit, 0);
+    sanitizedBenefit = Math.min(sanitizedBenefit, 50);
+    return sanitizedBenefit;
+  }
+
+  updateExpiration(expiration: number): number {
+    return expiration - 1;
+  }
+}
--- src/drug/specialDrugs/dafalgan.ts
+++ src/drug/specialDrugs/dafalgan.ts
@@ -1,8 +1,8 @@
 import { DefaultDrugProperties } from "../defaultDrugsProperties";
 
 export class Dafalgan extends DefaultDrugProperties {
-  updateBenefit(benefit: number, expiration: number) {
-    if (expiration > 0) return benefit - 2;
+  updateBenefit(benefit: number, expiration: number): number {
+    if (expiration >= 0) return benefit - 2;
     return benefit - 4;
   }
 }
