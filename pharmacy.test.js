ab9b9955714e194c9489162f6f89815f8c4d488c
Ki3ie <43181176+Ki3ie@users.noreply.github.com>
2021-06-27 18:51:12 +0200

    test(pharmacy): add multiple drugs and empty drugs tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,22 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
   });
+  it("should update the benefit and expiresIn", () => {
+    expect(
+      new Pharmacy([
+        new Drug("Doliprane", 20, 30),
+        new Drug("Herbal Tea", 10, 5),
+        new Drug("Fervex", 5, 40),
+        new Drug("Magic Pill", 15, 40)
+      ]).updateBenefitValue()
+    ).toEqual([
+      new Drug("Doliprane", 19, 29),
+      new Drug("Herbal Tea", 9, 6),
+      new Drug("Fervex", 4, 43),
+      new Drug("Magic Pill", 15, 40)
+    ]);
+  });
+  it("should not change", () => {
+    expect(new Pharmacy([]).updateBenefitValue()).toEqual([]);
+  });
 });
