b7a958ed640e44b94200d29907a885bbef5583c5
Elie Bismuth <eliebismuth91@gmail.com>
2021-07-19 17:53:43 +0200

    refactor updateBenefitValue and add tests

--- src/modules/pharmacy/pharmacy.js
+++ src/modules/pharmacy/pharmacy.js
@@ -2,21 +2,69 @@ export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+
   updateBenefitValue() {
+    this.drugs = this.drugs.map(drug => {
+      if (drug.name === "Magic Pill") return drug;
+
+      drug.expiresIn = drug.expiresIn - 1;
+
+      if (drug.name === "Fervex") {
+        if (drug.expiresIn < 0) {
+          drug.benefit = 0;
+        } else if (drug.expiresIn <= 5) {
+          drug.benefit = this.incrementBenefit(drug, 3);
+        } else if (drug.expiresIn <= 10) {
+          drug.benefit = this.incrementBenefit(drug, 2);
+        } else {
+          drug.benefit = this.incrementBenefit(drug, 1);
+        }
+        return drug;
+      }
+
+      if (drug.name === "Herbal Tea") {
+        if (drug.expiresIn < 0) {
+          drug.benefit = this.incrementBenefit(drug, 2);
+        } else {
+          drug.benefit = this.incrementBenefit(drug, 1);
+        }
+        return drug;
+      }
+
+      if (drug.expiresIn < 0) {
+        drug.benefit = this.decrementBenefit(drug, 2);
+      } else {
+        drug.benefit = this.decrementBenefit(drug, 1);
+      }
+
+      return drug;
+    });
+    return this.drugs;
+  }
+
+  incrementBenefit(drug, amount) {
+    return drug.benefit + amount < 50 ? drug.benefit + amount : 50;
+  }
+
+  decrementBenefit(drug, amount) {
+    return drug.benefit - amount > 0 ? drug.benefit - amount : 0;
+  }
+
+  OldupdateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
+        this.drugs[i].name !== "Herbal Tea" &&
+        this.drugs[i].name !== "Fervex"
       ) {
         if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
+          if (this.drugs[i].name !== "Magic Pill") {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
         }
       } else {
         if (this.drugs[i].benefit < 50) {
           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
+          if (this.drugs[i].name === "Fervex") {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
                 this.drugs[i].benefit = this.drugs[i].benefit + 1;
@@ -30,14 +78,14 @@ export class Pharmacy {
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
+      if (this.drugs[i].name !== "Magic Pill") {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
+        if (this.drugs[i].name !== "Herbal Tea") {
+          if (this.drugs[i].name !== "Fervex") {
             if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
+              if (this.drugs[i].name !== "Magic Pill") {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
             }
--- src/modules/pharmacy/pharmacy.test.js
+++ src/modules/pharmacy/pharmacy.test.js
@@ -7,4 +7,58 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)]
     );
   });
+
+  it("should not decrease under 0", () => {
+    expect(new Pharmacy([new Drug("test", 2, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("test", 1, 0)]
+    );
+  });
+
+  it("should not decrease for Magic Pill", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 2, 3)]);
+  });
+
+  it("should increase 1 for Herbal Tea", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+  });
+
+  it("should increase 2 for Herbal Tea", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -1, 5)]);
+  });
+
+  it("should not increase 2 for Herbal Tea in more than 50", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -1, 50)]);
+  });
+
+  it("should increase 1 for Fervex", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 12, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 11, 4)]);
+  });
+
+  it("should increase 2 for Fervex", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 11, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 10, 5)]);
+  });
+
+  it("should increase 3 for Fervex", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 6, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 5, 6)]);
+  });
+
+  it("should set benefit to 0 for Fervex", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", -1, 0)]);
+  });
 });
