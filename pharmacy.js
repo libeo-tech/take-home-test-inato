3fc3824120490ac9d1a96a4a125c46f0b9f86c86
Eric Chheu <eric.chheu@epitech.eu>
2021-07-27 17:08:33 +0200

    Dafalgan

--- pharmacy.js
+++ pharmacy.js
@@ -11,51 +11,49 @@ export class Pharmacy {
     this.drugs = drugs;
   }
   updateBenefitValue() {
+    const MAX = 50;
+    const magicPill = "Magic Pill";
+    const fervex = "Fervex";
+    const herbalTea = "Herbal Tea";
+    const dafalgan = "Dafalgan";
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
+      if (this.drugs[i].name !== magicPill) {
+        if (this.drugs[i].name === fervex) {
+          if (this.drugs[i].benefit < MAX) {
+            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+            if (this.drugs[i].benefit < MAX) {
+              if (this.drugs[i].expiresIn < 11) {
+                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+              }
+              if (this.drugs[i].expiresIn < 6) {
+                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+              }
+            }
+          }
+        } else if (this.drugs[i].name === herbalTea) {
+          if (this.drugs[i].benefit < MAX) {
+            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+          }
+        } else {
+          if (this.drugs[i].benefit > 0) {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
         }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
         this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
+        if (this.drugs[i].name === dafalgan) {
+          this.drugs[i].benefit = this.drugs[i].benefit - 1;
+        }
+        if (this.drugs[i].expiresIn < 0) {
+          if (this.drugs[i].name === herbalTea) {
+            if (this.drugs[i].benefit < MAX) {
+              this.drugs[i].benefit = this.drugs[i].benefit + 1;
             }
+          } else if (this.drugs[i].name === fervex) {
+            this.drugs[i].benefit = 0;
           } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+            if (this.drugs[i].benefit > 0) {
+              this.drugs[i].benefit = this.drugs[i].benefit - 1;
+            }
           }
         }
       }
