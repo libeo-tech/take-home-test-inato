a6de237bcf4f9e40177a73b7c336f6259ece4de7
frederic grati <grati.frederic@gmail.com>
2021-07-30 10:18:19 +0200

    refacto updateBenefitValue

--- pharmacy.js
+++ pharmacy.js
@@ -6,61 +6,33 @@ export class Drug {
   }
 }
 
+function updateDefaultDrug(drug) {
+  const delta = drug.expiresIn > 0 ? 1 : 2;
+  const expiresIn = drug.expiresIn - 1;
+  let benefit = drug.benefit - delta;
+  if (benefit > 50) {
+    benefit = 50;
+  }
+  if (benefit < 0) {
+    benefit = 0;
+  }
+  return {
+    ...drug,
+    expiresIn,
+    benefit,
+  };
+}
+const updateDrugsHandlers = {};
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
+      const handler =
+        updateDrugsHandlers[this.drugs[i].name] || updateDefaultDrug;
+      this.drugs[i] = handler(this.drugs[i]);
     }
-
     return this.drugs;
   }
 }
