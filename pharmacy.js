29793a0b0e20b8c39bbff31f2fcb5841e31e2353
FlorentJakubowski <florent.jakubowski@outmind.fr>
2021-08-23 09:11:50 +0200

    feat: ✨ Add Dafalgan class with method

--- pharmacy.js
+++ pharmacy.js
@@ -57,6 +57,27 @@ export class Fervex extends Drug {
     } else this.benefit = this.benefit + 1;
   }
 }
+export class Dafalgan extends Drug {
+  constructor(expiresIn, benefit) {
+    super("Dafalgan", expiresIn, benefit);
+  }
+  updateBenefit() {
+    if (this.expiresIn < 0) {
+      if (this.benefit - 4 >= 0) {
+        this.benefit = this.benefit - 4;
+      } else if (this.benefit - 3 === 0) {
+        this.benefit = this.benefit - 3;
+      } else if (this.benefit - 2 === 0) {
+        this.benefit = this.benefit - 2;
+      } else if (this.benefit - 1 === 0) {
+        this.benefit = this.benefit - 1;
+      }
+    } else if (this.expiresIn >= 0) {
+      if (this.benefit - 2 >= 0) this.benefit = this.benefit - 2;
+      else if (this.benefit - 1 === 0) this.benefit = this.benefit - 1;
+    }
+  }
+}
 
 export class Pharmacy {
   constructor(drugs = []) {
