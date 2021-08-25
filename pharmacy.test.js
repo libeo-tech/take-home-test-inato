8ce6ba761d56f821a3a123edbe5b74576e84f23a
lola.d <lola.d@sublissi.me>
2021-08-25 15:37:34 +0200

    added unit tests to Fervex drugs

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -20,4 +20,30 @@ describe("Pharmacy", () => {
       ).toEqual([new Drug("test", -2, 0)]);
     });
   });
+
+  describe("Fervex", () => {
+    it("should increase the benefit twice when there are 10 days or less and decrease expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 15, 12)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 14, 13)]);
+    });
+
+    it("should increase the benefit by 3 when there are 5 days or less and decrease expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 1, 6)]);
+    });
+
+    it("should drop the benefit to 0 and decrease expiresIn after the expiration date. ", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 0, 9)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", -1, 0)]);
+    });
+
+    it("should not increase benefit more than 50", () => {
+      expect(
+        new Pharmacy([new Drug("Fervex", 15, 49)]).updateBenefitValue()
+      ).toEqual([new Drug("Fervex", 14, 50)]);
+    });
+  });
 });
