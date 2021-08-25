58846826571725310bf8fd8a32f58d6d0b0813cc
lola.d <lola.d@sublissi.me>
2021-08-25 15:36:46 +0200

    added unit tests for common drugs

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,23 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("Common drugs", () => {
+    it("should decrease the benefit and expiresIn", () => {
+      expect(
+        new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+      ).toEqual([new Drug("test", 1, 2)]);
+    });
+
+    it("should decrease the benefit twice after the expiration date", () => {
+      expect(
+        new Pharmacy([new Drug("test", -2, 6)]).updateBenefitValue()
+      ).toEqual([new Drug("test", -3, 4)]);
+    });
+
+    it("should only decrease expiresIn if benefit is 0", () => {
+      expect(
+        new Pharmacy([new Drug("test", -1, 0)]).updateBenefitValue()
+      ).toEqual([new Drug("test", -2, 0)]);
+    });
   });
 });
