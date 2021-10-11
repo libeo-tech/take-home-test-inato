3f6ea0279e4bc69a11dc8376898250975d610bf3
Désir <emmanuel.desir@enablon.com>
2021-10-11 23:18:10 +0200

    removed unnecessary comments

--- pharmacy.js
+++ pharmacy.js
@@ -71,40 +71,6 @@ export class Pharmacy {
       drug.expiresIn = this.getNewExpiresIn(drug);
       drug.benefit = this.getNewBenefit(drug);
 
-      // const specialBenefitIncrements = {
-      //   "Herbal Tea": 1,
-      //   "Magic Pill": 0,
-      //   Fervex: {
-      //     standard: 1,
-      //     10: 2,
-      //     5: 3
-      //   }
-      // };
-      // const specialExpiresInIncrements = {
-      //   "Magic Pill": 0
-      // };
-      // const maxBenefitValue = 50;
-      // this.drugs.map(drug => {
-      //   // decrement days
-      //   let expiresInIncrement =
-      //     specialExpiresInIncrements[drug.name] !== undefined
-      //       ? specialExpiresInIncrements[drug.name]
-      //       : -1;
-      //   drug.expiresIn += expiresInIncrement;
-
-      //   // normal case
-      //   let benefitIncrement =
-      //     specialBenefitIncrements[drug.name] !== undefined
-      //       ? specialBenefitIncrements[drug.name]
-      //       : -1;
-      //   if (drug.expiresIn < 0) {
-      //     benefitIncrement = 2 * benefitIncrement;
-      //   }
-      //   drug.benefit += benefitIncrement;
-      //   drug.benefit = drug.benefit >= 0 ? drug.benefit : 0;
-      //   drug.benefit = drug.benefit >= maxBenefitValue ? maxBenefitValue : drug.benefit;
-
-      //   return drug;
       return drug;
     });
 
