69892372d29abec85f0d326e79dc9fc88e709cd7
Sébastien Lampazona <lampazona.sebastien@gmail.com>
2021-10-28 12:02:13 +0200

    Préparation de la classe Drugs pour le TDD

--- src/models/Drug.js
+++ src/models/Drug.js
@@ -1,7 +1,44 @@
 export default class Drug {
+  name = 'BasicDrug';
+  expiresIn = 10;
+  #benefit = 50;
+
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
+
+    this.toJSON = function() {
+      return {
+        name: this.name,
+        expiresIn: this.expiresIn,
+        benefit: this.benefit,
+      }
+    };
+  }
+
+  get benefit() {
+    return this.#benefit;
+  }
+
+  set benefit(benefit) {
+    if (benefit < 0) {
+      throw new Error('benefit cannot be lower than 0');
+    }
+    else if (benefit > 50){
+      throw new Error('benefit cannot be higher than 50');
+    }
+    this.#benefit = benefit;
+  }
+
+  /**
+   * Return the new benefit, after day change
+   * @returns {Number} - The new calculated benefit
+   */
+  calculateNextBenefit() {
+    if (this.benefit > 0){
+      this.benefit--;
+    }
+    return this.benefit;
   }
 }
