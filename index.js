251b27a7fc11744d7ebc71d46213474b1ad10a75
Nathan Meunier <nmeunier@carlipa.com>
2021-12-13 21:11:28 +0100

    feat(): first commit, refactor, case default drugs

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { Drug, Pharmacy } from "./pharmacy";
 import fs from "fs";
 
 const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug("Doliprane", 20, 30)
+  // new Drug("Herbal Tea", 10, 5),
+  // new Drug("Fervex", 5, 40),
+  // new Drug("Magic Pill", 15, 40)
 ];
 const trial = new Pharmacy(drugs);
 
