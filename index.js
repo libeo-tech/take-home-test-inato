f49e5627a08b218621e0f2bb8c0ca7e407659f81
Nathan Meunier <nmeunier@carlipa.com>
2021-12-13 21:29:47 +0100

    feat(Herbal): herbal case

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { Drug, Pharmacy } from "./pharmacy";
 import fs from "fs";
 
 const drugs = [
-  new Drug("Doliprane", 20, 30)
-  // new Drug("Herbal Tea", 10, 5),
+  new Drug("Doliprane", 20, 30),
+  new Drug("Herbal Tea", 10, 5)
   // new Drug("Fervex", 5, 40),
   // new Drug("Magic Pill", 15, 40)
 ];
@@ -13,7 +13,8 @@ const trial = new Pharmacy(drugs);
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(trial.updateBenefitValue()));
+  // log.push(JSON.stringify(trial.updateBenefitValue()));
+  console.log(trial.updateBenefitValue());
 }
 
 /* eslint-disable no-console */
