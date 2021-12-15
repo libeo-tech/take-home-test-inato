1714ff155455656d3206437691584418c58096ab
Yurii Kurylenko <92849071+yurii-ku@users.noreply.github.com>
2021-12-15 19:00:18 +0200

    Modify pharmacy logic and test

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Pharmacy } from "./pharmacy";
+import { Doliparne } from "./drugs/doliparne";
+import { HerbalTea } from "./drugs/herbalTea";
+import { Fervex } from "./drugs/fervex";
+import { MagicPill } from "./drugs/magicPill";
+import { Dafalgan } from "./drugs/dafalgan";
 
 import fs from "fs";
 
 const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Doliparne(20, 30),
+  new HerbalTea(10, 5),
+  new Fervex(5, 40),
+  new MagicPill(15, 40),
+  new Dafalgan(15, 10)
 ];
 const trial = new Pharmacy(drugs);
 
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(trial.updateBenefitValue()));
+  log.push(
+    JSON.stringify(
+      trial.updateBenefitValue().map(drug => drug.toJSON())
+    )
+  );
 }
 
 /* eslint-disable no-console */
