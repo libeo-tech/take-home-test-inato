557f04f9697aa4b953a3076d647eb609b40fe906
mordenius <mordenius@gmail.com>
2021-12-23 04:48:48 +0200

    test(pharmacy) fix format

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,10 @@
 import { Drug, Pharmacy } from "./pharmacy";
+import { Linear } from "./revaluations";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
+    expect(new Pharmacy([new Drug("test", 2, 3, [new Linear()])]).updateBenefitValue().toString()).toEqual(
+      [new Drug("test", 1, 2, [new Linear()])].toString()
     );
   });
 });
