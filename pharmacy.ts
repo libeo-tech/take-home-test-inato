ea1e5e68e8071ca9093b9d57801cf70a68f4ea32
mordenius <mordenius@gmail.com>
2021-12-23 04:44:38 +0200

    feature(pharmacy) strategy for drug revaluations

--- pharmacy.ts
+++ pharmacy.ts
@@ -1,14 +1,49 @@
+export interface RevaluationData {
+  expiresIn: number;
+  currentBenefit: number;
+  beforeRevaluationBenefit: number;
+}
+
+export interface Revaluation {
+  calculate(_: RevaluationData): number;
+}
+
 interface DrugData {
   readonly name: string;
   expiresIn: number;
   benefit: number;
 }
+
+// The Benefit of an item is never negative.
+// The Benefit of an item is never more than 50.
+export class Limits implements Revaluation {
+  calculate({ currentBenefit }: RevaluationData): number {
+    if (currentBenefit <= 0) {
+      return 0;
+    }
+
+    if (currentBenefit >= 50) {
+      return 50;
+    }
+
+    return currentBenefit;
+  }
+}
+
 export class Drug implements DrugData {
+  private revaluations: Revaluation[] = [];
+
   constructor(
     public readonly name: string,
     public expiresIn: number,
     public benefit: number,
+    revaluations: Revaluation[] = []
   ) {
+    for (const revaluation of revaluations) {
+      this.revaluations.push(revaluation);
+    }
+
+    this.revaluations.push(new Limits());
   }
 
   public toJSON = (): DrugData => ({
@@ -29,7 +64,16 @@ export class Drug implements DrugData {
   }
 
   protected _benefitRevaluate(): void {
-    // TODO: Missing logic
+    const beforeRevaluationBenefit = this.benefit;
+    this.benefit = this.revaluations.reduce<number>(
+      (currentBenefit, revaluation): number =>
+        revaluation.calculate({
+          beforeRevaluationBenefit,
+          currentBenefit,
+          expiresIn: this.expiresIn
+        }),
+      this.benefit
+    );
   }
 
   protected updateExpires(): void {
