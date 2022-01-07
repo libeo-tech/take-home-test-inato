a361a5caafcde5698dbec4e292ad47148a0bb777
benjamin villin <benjaminvillin91@gmail.com>
2022-01-07 09:59:41 +0100

    clean old code + add unit test

--- index.js
+++ index.js
@@ -4,39 +4,22 @@ import {
 } from "./pharmacy";
 import fs from "fs";
 
-const drugs1 = [
+const drugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 5, 40),
   new Drug("Magic Pill", 15, 40)
 ];
-const drugs2 = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
-];
-const trial = new Pharmacy(drugs1);
-const trial2 = new Pharmacy(drugs2);
+
+const trial = new Pharmacy(drugs);
 const log = [];
-const log2 = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
   log.push(JSON.stringify(trial.updateBenefitValue()));
-  log2.push(JSON.stringify(trial2.newUpdateBenefitValue()));
 }
 
 /* eslint-disable no-console */
 fs.writeFile("output.txt", log.toString(), err => {
-  if (err) {
-    console.log("error");
-  }
-  else {
-    console.log("success");
-  }
-});
-
-fs.writeFile("output2.txt", log2.toString(), err => {
   if (err) {
     console.log("error");
   }
--- pharmacy.js
+++ pharmacy.js
@@ -64,67 +64,13 @@ export class Pharmacy {
     
     return success;
   }
-  
-  updateBenefitValue() {
-    for (var i = 0; i < this.drugs.length; i++) {
-      if (
-          this.drugs[i].name != "Herbal Tea" &&
-          this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-                this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
-      }
-    }
-    
-    return this.drugs;
-  }
-  
+
   /*
   * Iterate on drugs array to update benefit and expiresIn values of each one
   * params:
   * return: drogs (Array of Object {name (String), expiresIn: (Int), benefit (Int)})
   * */
-  newUpdateBenefitValue() {
+  updateBenefitValue() {
     for (const drug of this.drugs){
       const currentRule = drogsRules && drogsRules.rules.length > 0 ? drogsRules.rules.find(el => el.name === drug.name) || defaultDrogRule.rules[0] : defaultDrogRule.rules[0];
       const maxBenefit = drogsRules && drogsRules.maxBenefit ? drogsRules.maxBenefit : defaultDrogRule.maxBenefit
