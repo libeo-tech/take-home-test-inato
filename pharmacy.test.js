a8d6ab101e427c164b121ca00feb2d0ae8bff4d9
“Ahmad <ahmad.moussa.lb@gmail.com>
2022-01-09 12:41:50 +0100

    [update] ADD unit tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,10 +1,67 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
-const test = new Drug("test", 2, 3);
-const pharmacy = new Pharmacy([test]);
-
 describe("Pharmacy", () => {
-  it("[unit] should decrease benefit & expiresIn", () => {
-    expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+  describe("General drug", () => {
+    let test = new Drug("test", 2, 3);
+    let pharmacy = new Pharmacy([test]);
+
+    it("[unit] should decrease benefit & expiresIn", () => {
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+
+    it("[unit] should degrades benefit twice after expiration", () => {
+      test.expiresIn = 0;
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+
+    it("[unit] should not have negative benefit", () => {
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+
+    it("[unit] should not have benefit > 50", () => {
+      test.name = "Herbal Tea";
+      test.expiresIn = -18;
+      test.benefit = 50;
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+  });
+
+  describe("Herbal Tea", () => {
+    let herbalTea = new Drug("Herbal Tea", 0, 3);
+    let pharmacy = new Pharmacy([herbalTea]);
+    it("[unit] should increases in benefit twice after the expiration date", () => {
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+
+    it("[unit] should increases in benefit as its expiration date approaches", () => {
+      herbalTea.expiresIn = 5;
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+  });
+
+  describe("Magic Pill", () => {
+    let magicPill = new Drug("Magic Pill", 1, 10);
+    let pharmacy = new Pharmacy([magicPill]);
+    it("[unit] should never expires nor decreases in benefit", () => {
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+  });
+
+  describe("Fervex", () => {
+    let fervex = new Drug("Fervex", 20, 10);
+    let pharmacy = new Pharmacy([fervex]);
+    it("[unit] should increases in benefit as its expiration date approaches", () => {
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+
+    it("[unit] should increases in benefit by 2 when <= 10 days", () => {
+      fervex.expiresIn = 9;
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
+
+    it("[unit] should increases in benefit by 3 when <= 5 days", () => {
+      fervex.expiresIn = 4;
+      expect(pharmacy.updateBenefitValue()).toMatchSnapshot();
+    });
   });
 });
