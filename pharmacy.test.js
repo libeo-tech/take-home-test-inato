f31eb4f76fd0ba9a8741ae60d62763e78f8e2442
BLI <bohao.li.ext@eulerhermes.com>
2022-01-13 23:07:00 +0100

    Added unit tests for Herbal Tea

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -8,7 +8,7 @@ function testPhar(drugName, expiresIn, benifit, newExpiresIn, newBenifit) {
 
 describe('Pharmacy', () => {
   describe('Test a normal drug', () => {
-    describe('when drug not expired', () => {
+    describe('When drug not expired', () => {
       it('If benefit >= 1 ==> expiresIn -= 1, benefit -= 1 ', () => {
         testPhar('Test', 2, 3, 1, 2);
       });
@@ -26,6 +26,29 @@ describe('Pharmacy', () => {
       it('If beneft ∈ [0, 1] ==> expiresIn -= 1, benefit == 0', () => {
         testPhar('Test', 0, 1, -1, 0);
         testPhar('Test', 0, 0, -1, 0);
+      });
+    });
+  });
+
+  describe('Test Herbal Tea', () => {
+    describe('When drug not expired', () => {
+      it('If benefit <= 49 ==> expiresIn -= 1, benefit += 1', () => {
+        testPhar('Herbal Tea', 15, 30, 14, 31);
+      });
+
+      it('If benefit == 50 ==> expiresIn -= 1, benefit == 50', () => {
+        testPhar('Herbal Tea', 15, 50, 14, 50);
+      });
+    });
+
+    describe('When drug expired', () => {
+      it('If benefit <= 48 ==> expiresIn -= 1, benefit += 2', () => {
+        testPhar('Herbal Tea', 0, 12, -1, 14);
+      });
+
+      it('If benefit ∈ [49, 50] ==> expiresIn -= 1, benefit == 50', () => {
+        testPhar('Herbal Tea', 0, 49, -1, 50);
+        testPhar('Herbal Tea', 0, 50, -1, 50);
       });
     });
   });
