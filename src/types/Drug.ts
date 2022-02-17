86b8dad87b32ea8c02289ea00075e8e9da651a2a
louismarie <12646487+louismarie@users.noreply.github.com>
2022-02-17 07:53:22 +0100

    refacto: add encapsulation

--- src/types/Drug.ts
+++ src/types/Drug.ts
@@ -1,11 +1,75 @@
+const DRUG_MAX_BENEFIT = 50;
+
 export class Drug {
-  name: string;
-  expiresIn: number;
-  benefit: number;
+  private name: string;
+  private expiresIn: number;
+  private benefit: number;
 
   constructor(name: string, expiresIn: number, benefit: number) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
+  }
+
+  private handleMaxBenefit() {
+    return this.benefit > DRUG_MAX_BENEFIT ? DRUG_MAX_BENEFIT : this.benefit;
+  }
+
+  private handleDegradationDafalgan() {
+    return this.benefit - 2;
+  }
+
+  private handleDegradationFervex() {
+    if (this.expiresIn <= 0) {
+      return 0;
+    } else if (this.expiresIn <= 5) {
+      return this.benefit + 3;
+    } else if (this.expiresIn <= 10) {
+      return this.benefit + 2;
+    }
+    return this.benefit + 1;
+  }
+
+  private handleDegradationMagicPill() {
+    return this.benefit;
+  }
+
+  private handleDegradationHerbalTea() {
+    if (this.expiresIn > 0) {
+      return this.benefit + 1;
+    }
+    return this.benefit + 2;
+  }
+
+  private handleDegradationCommonDrugs() {
+    let benefit;
+
+    if (this.expiresIn > 0) {
+      benefit = this.benefit - 1;
+    } else {
+      benefit = this.benefit - 2;
+    }
+
+    return benefit > 0 ? benefit : 0;
+  }
+
+  updateBenefitAndExpiration() {
+    if (this.name === "Herbal Tea") {
+      this.benefit = this.handleDegradationHerbalTea();
+      this.expiresIn--;
+    } else if (this.name === "Magic Pill") {
+      this.benefit = this.handleDegradationMagicPill();
+    } else if (this.name === "Fervex") {
+      this.benefit = this.handleDegradationFervex();
+      this.expiresIn--;
+    } else if (this.name === "Dafalgan") {
+      this.benefit = this.handleDegradationDafalgan();
+      this.expiresIn--;
+    } else {
+      this.benefit = this.handleDegradationCommonDrugs();
+      this.expiresIn--;
+    }
+
+    this.benefit = this.handleMaxBenefit();
   }
 }
--- src/types/Pharmacy.ts
+++ src/types/Pharmacy.ts
@@ -1,76 +1,17 @@
 import { Drug } from "./Drug";
 
 export class Pharmacy {
-  drugs: Drug[];
-  MAX_BENEFIT = 50;
+  private drugs: Drug[];
 
   constructor(drugs: Drug[]) {
     this.drugs = drugs;
-  }
-
-  private handleMaxBenefit(d: Drug) {
-    return d.benefit > this.MAX_BENEFIT ? this.MAX_BENEFIT : d.benefit;
-  }
-
-  private handleDegradationDafalgan(d: Drug) {
-    return d.benefit - 2;
-  }
-
-  private handleDegradationFervex(d: Drug) {
-    if (d.expiresIn <= 0) {
-      return 0;
-    } else if (d.expiresIn <= 5) {
-      return d.benefit + 3;
-    } else if (d.expiresIn <= 10) {
-      return d.benefit + 2;
-    }
-    return d.benefit + 1;
-  }
-
-  private handleDegradationMagicPill(d: Drug) {
-    return d.benefit;
-  }
-
-  private handleDegradationHerbalTea(d: Drug) {
-    if (d.expiresIn > 0) {
-      return d.benefit + 1;
-    }
-    return d.benefit + 2;
-  }
-
-  private handleDegradationCommonDrugs(d: Drug) {
-    let benefit;
-
-    if (d.expiresIn > 0) {
-      benefit = d.benefit - 1;
-    } else {
-      benefit = d.benefit - 2;
-    }
-
-    return benefit > 0 ? benefit : 0;
   }
 
   updateBenefitValue() {
     const drugs = this.drugs;
 
     drugs.map((d) => {
-      if (d.name === "Herbal Tea") {
-        d.benefit = this.handleDegradationHerbalTea(d);
-        d.expiresIn--;
-      } else if (d.name === "Magic Pill") {
-        d.benefit = this.handleDegradationMagicPill(d);
-      } else if (d.name === "Fervex") {
-        d.benefit = this.handleDegradationFervex(d);
-        d.expiresIn--;
-      } else if (d.name === "Dafalgan") {
-        d.benefit = this.handleDegradationDafalgan(d);
-        d.expiresIn--;
-      } else {
-        d.benefit = this.handleDegradationCommonDrugs(d);
-        d.expiresIn--;
-      }
-
-      d.benefit = this.handleMaxBenefit(d);
+      d.updateBenefitAndExpiration();
     });
 
     return drugs;
