65d253f6ad3f4c0c2fb649931dc1cb2d4792e7b9
wrq77 <74533971+wrq77@users.noreply.github.com>
2022-02-17 23:45:21 +0100

    first commit

--- pharmacy.js
+++ pharmacy.js
@@ -5,62 +5,120 @@ export class Drug {
     this.benefit = benefit;
   }
 }
+const maxBenefit = 50;
+const minBenefit = 0;
 
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
   }
+
+  BenefitplusOne(drug){
+    if(drug.benefit <= 49){
+      drug.benefit = drug.benefit+1;
+    }else{
+      drug.benefit = maxBenefit;
+    }
+  }
+
+  BenefitplusTwo(drug){
+    if(drug.benefit <= 48){
+      drug.benefit = drug.benefit+2;
+    }else{
+      drug.benefit = maxBenefit;
+    }
+  }
+
+  BenefitplusThree(drug){
+    if(drug.benefit <= 47){
+      drug.benefit = drug.benefit+3;
+    }else{
+      drug.benefit = maxBenefit;
+    }
+  }
+
+  BenefitminusOne(drug){
+    if(drug.benefit >= 1){
+      drug.benefit = drug.benefit - 1;
+    }else{
+      drug.benefit = minBenefit;
+    }
+  }
+
+  BenefitminusTwo(drug){
+    if(drug.benefit >= 2){
+      drug.benefit = drug.benefit - 2;
+    }else{
+      drug.benefit = minBenefit;
+    }
+  }
+
+  BenefitminusFour(drug){
+    if(drug.benefit >= 4){
+      drug.benefit = drug.benefit - 4;
+    }else{
+      drug.benefit = minBenefit;
+    }
+  }
+
+  Default(drug) {
+    if (drug.expiresIn > 0) {
+      this.BenefitplusOne(drug);
+    } else {
+      this.BenefitminusTwo(drug);
+    }
+    drug.expiresIn--;
+  }
+
+  HerbalTea(drug){
+    if (drug.expiresIn > 0) {
+      this.BenefitplusOne(drug);
+    } else {
+      this.BenefitplusTwo(drug);
+    }
+    drug.expiresIn =  drug.expiresIn -1 ;
+  }
+
+  MagicPill(drug){
+
+  }
+
+  Fervex(drug){
+    if (drug.expiresIn >= 11) {
+      this.BenefitplusOne(drug);
+    } else if (drug.expiresIn >= 6 && drug.expiresIn <= 10) {
+      this.BenefitplusTwo(drug);
+    } else if (drug.expiresIn >= 1 && drug.expiresIn <= 5) {
+      this.BenefitplusThree(drug);
+    } else {
+      drug.benefit = minBenefit;
+    }
+    drug.expiresIn--;
+  }
+
+  Dafalgan(drug){
+    if (drug.expiresIn > 0) {
+      this.BenefitminusTwo(drug);
+    } else {
+      this.BenefitminusFour(drug);
+    }
+    drug.expiresIn--;
+  }
+  
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
-      ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
-      } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          if (this.drugs[i].name == "Fervex") {
-            if (this.drugs[i].expiresIn < 11) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-            if (this.drugs[i].expiresIn < 6) {
-              if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      }
-      if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
-              }
-            }
-          } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
-          }
-        } else {
-          if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
-          }
-        }
+      if(this.drugs[i].name == "Herbal Tea"){
+        this.HerbalTea(this.drugs[i]);
+      }else if(this.drugs[i].name == "Magic Pill"){
+        this.MagicPill(this.drugs[i]);
+      }else if(this.drugs[i].name == "Fervex"){
+        this.Fervex(this.drugs[i]);
+      }else if(this.drugs[i].name == "Dafalgan"){
+        this.Dafalgan(this.drugs[i]);
+      }else{
+        this.Default(this.drugs[i]);
       }
     }
-
     return this.drugs;
   }
 }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,5 +1,6 @@
 import { Drug, Pharmacy } from "./pharmacy";
 
+//default test
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
@@ -7,3 +8,91 @@ describe("Pharmacy", () => {
     );
   });
 });
+
+// Herbal Tea test
+describe("Herbal Tea test", () => {
+  it("should increase by 1 with expiresIn > 0", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 15, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 14, 31)]
+    );
+  });
+
+  it("should increase by 2 with expiresIn <= 0", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", -1, 10)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -2, 12)]
+    );
+  });
+
+  it("benefit is never more than 50", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 15, 50)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 14, 50)]
+    );
+  });
+
+});
+
+// Magic Pill test
+describe("Magic Pill test", () => {
+  it("should never expire nor decrease in benefit", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 15, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 15, 40)]
+    );
+  });
+})
+
+// Fervex test
+describe("Fervex test", () => {
+  it("should increase by 1 with expiresIn > 10", () => {
+    expect(new Pharmacy([new Drug("Fervex", 15, 25)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 14, 26)]
+    );
+  });
+
+  it("should increase by 2 with expiresIn <= 10 and expiresIn > 5", () => {
+    expect(new Pharmacy([new Drug("Fervex", 10, 15)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 9, 17)]
+    );
+  });
+
+  it("should increase by 3 with expiresIn <= 5 and expiresIn > 0", () => {
+    expect(new Pharmacy([new Drug("Fervex", 5, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 4, 33)]
+    );
+  });
+
+  it("should drop to 0 with expiresIn <= 0", () => {
+    expect(new Pharmacy([new Drug("Fervex", 0, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", -1, 0)]
+    );
+  });
+
+  it("benefit is never more than 50", () => {
+    expect(new Pharmacy([new Drug("Fervex", 15, 50)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 14, 50)]
+    );
+  });
+
+});
+
+// Defalgan test
+describe("Defalgan test", () => {
+  it("should decrease by 2 with expiresIn > 0 ", () => {
+    expect(new Pharmacy([new Drug("Defalgan", 15, 40)]).updateBenefitValue()).toEqual(
+      [new Drug("Defalgan", 14, 38)]
+    );
+  });
+
+  it("should decrease by 4 with expiresIn <= 0 ", () => {
+    expect(new Pharmacy([new Drug("Defalgan", 0, 30)]).updateBenefitValue()).toEqual(
+      [new Drug("Defalgan", 0, 26)]
+    );
+  });
+
+  it("benefit is never less than 0", () => {
+    expect(new Pharmacy([new Drug("Defalgan", -1, 2)]).updateBenefitValue()).toEqual(
+      [new Drug("Defalgan", -2, 0)]
+    );
+  });
+
+})
+
