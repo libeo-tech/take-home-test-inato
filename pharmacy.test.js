528880e8bed537b30a4597d45c704a2ce042b3a2
lucasgameiroborges <lucasgb@lucasgb.localdomain>
2022-02-21 17:36:29 -0300

    added drug-specifitc tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,6 +6,10 @@ describe("Pharmacy", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
+
+    expect(new Pharmacy([new Drug("test", -2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -3, 1)]
+    );
   });
 
   it('should return the same output file', () => {
@@ -17,4 +21,47 @@ describe("Pharmacy", () => {
     
   });
 
+  it("should increase herbal tea´s benefit with time", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 1, 4)]
+    );
+
+    expect(new Pharmacy([new Drug("Herbal Tea", -1, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -2, 5)]
+    );
+  });
+
+  it("should increase fervex´s benefit with time", () => {
+    expect(new Pharmacy([new Drug("Fervex", 12, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 11, 4)]
+    );
+
+    expect(new Pharmacy([new Drug("Fervex", 7, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 6, 5)]
+    );
+
+    expect(new Pharmacy([new Drug("Fervex", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", 1, 6)]
+    );
+
+    expect(new Pharmacy([new Drug("Fervex", 0, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Fervex", -1, 0)]
+    );
+  });
+
+  it("should keep magic pill´s stats constant", () => {
+    expect(new Pharmacy([new Drug("Magic Pill", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Magic Pill", 2, 3)]
+    );
+  });
+  
+  it("should decrease dafalgan´s benefit twice as fast", () => {
+    expect(new Pharmacy([new Drug("Dafalgan", 2, 3)]).updateBenefitValue()).toEqual(
+      [new Drug("Dafalgan", 1, 1)]
+    );
+
+    expect(new Pharmacy([new Drug("Dafalgan", -2, 5)]).updateBenefitValue()).toEqual(
+      [new Drug("test", -3, 1)]
+    );
+  });
 });
