7921b01471aaf7a510935a0f929ba8fdfef707e3
Romain Laneuville <romain.laneuville@naval-group.com>
2022-02-26 13:56:51 +0100

    [test] + add a test to verify that the generated output is correct

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,23 @@
 import { Drug, Pharmacy } from "./pharmacy";
+import fs from "fs";
+import { Buffer } from "buffer";
+const { execSync } = require("child_process");
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
+  });
+});
+
+describe("Pharmacy", () => {
+  it("should output.txt be equals to original output-original.txt", () => {
+    const original = fs.readFileSync("./output-original.txt");
+
+    // Make sure the command is successful
+    expect(execSync("docker-compose up").equals(Buffer.from("success")));
+
+    expect(fs.readFileSync("./output.txt").equals(original));
   });
 });
