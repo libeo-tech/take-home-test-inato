56ecbb894f6f3689c581f73fb44d4440c47e4223
gael-henonin <100447438+gael-henonin@users.noreply.github.com>
2022-03-01 18:42:45 +0100

    refactor: use increment/decrement operators

--- pharmacy.js
+++ pharmacy.js
@@ -18,43 +18,43 @@ export class Pharmacy {
       ) {
         if (drug.benefit > 0) {
           if (drug.name != "Magic Pill") {
-            drug.benefit = drug.benefit - 1;
+            drug.benefit--;
           }
         }
       } else {
         if (drug.benefit < 50) {
-          drug.benefit = drug.benefit + 1;
+          drug.benefit++;
           if (drug.name == "Fervex") {
             if (drug.expiresIn < 11) {
               if (drug.benefit < 50) {
-                drug.benefit = drug.benefit + 1;
+                drug.benefit++;
               }
             }
             if (drug.expiresIn < 6) {
               if (drug.benefit < 50) {
-                drug.benefit = drug.benefit + 1;
+                drug.benefit++;
               }
             }
           }
         }
       }
       if (drug.name != "Magic Pill") {
-        drug.expiresIn = drug.expiresIn - 1;
+        drug.expiresIn--;
       }
       if (drug.expiresIn < 0) {
         if (drug.name != "Herbal Tea") {
           if (drug.name != "Fervex") {
             if (drug.benefit > 0) {
               if (drug.name != "Magic Pill") {
-                drug.benefit = drug.benefit - 1;
+                drug.benefit--;
               }
             }
           } else {
-            drug.benefit = drug.benefit - drug.benefit;
+            drug.benefit -= drug.benefit;
           }
         } else {
           if (drug.benefit < 50) {
-            drug.benefit = drug.benefit + 1;
+            drug.benefit++;
           }
         }
       }
