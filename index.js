9b8913a1570b2fba84e1d354f1e7348717a6949f
ArthurGuerquin <58431001+ArthurGuerquin@users.noreply.github.com>
2022-03-03 11:52:40 +0100

    [ADD] Dafalgan
    New Drug Dafalgan and add parameters in for the other drugs

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ import { Drug, Pharmacy } from "./pharmacy";
 import fs from "fs";
 
 const drugs = [
-  new Drug("Doliprane", 20, 30),
-  new Drug("Herbal Tea", 10, 5),
-  new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  // Normal Drugs decrease benefit by 1 each day, do not expire after expir date and have a linear evolution.
+  new Drug("Doliprane", 20, 30, 1, 1, false, true),
+  // Herbal tea increases in benefit over time, normal evolution (twice after expiration date).
+  new Drug("Herbal Tea", 10, 5, 1, -1, false, true),
+  // Fervex increases in benefit over time, not a linear evolution.
+  new Drug("Fervex", 5, 40, 1, -1, true, false, [10, 5]),
+  // magic pill will not decrease benefit nor expiration date.
+  new Drug("Magic Pill", 15, 40, 0, 0, false, true),
+  // Dafalgan is a normal drug but decreases benefit twice as fast.
+  new Drug("Dafalgan", 20, 30, 1, 2, false, true)
 ];
 const trial = new Pharmacy(drugs);
 
@@ -23,4 +29,5 @@ fs.writeFile("output.txt", log.toString(), err => {
     console.log("success");
   }
 });
+
 /* eslint-enable no-console */
