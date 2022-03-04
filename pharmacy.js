97c3e3c28cdf82591f13d0fbf9081c854c5381ab
agailloty <mr.clerisaxel@hotmail.fr>
2022-03-04 23:36:17 +0100

    Refactoring update logic

--- pharmacy.js
+++ pharmacy.js
@@ -10,6 +10,38 @@ export class Drug {
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
+  degrade(decreaseFactor = 1, direction = -1) {
+    this.expiresIn -= 1;
+    this.benefit += decreaseFactor * direction;
+
+    if (this.benefit <= 0) {
+      this.benefit = 0 // benefit cannot be negative
+    } else if (this.benefit > 50) {
+      this.benefit = 50 // cannot be > 50
+    }
+  }
+
+  update() {
+  
+    if (this.name == MagicPill) {
+      this.expiresIn = this.expiresIn;
+      this.benefit = this.benefit;
+    } else if (this.name == Fervex || this.name == HerbalTea) {
+      if (this.expiresIn < 11 && this.expiresIn < 6) {
+        this.degrade(2, 1)
+      } else if (this.expiresIn <= 5 && this.expiresIn >= 0) {
+        degrade(3, 1)
+      } else {
+        this.degrade(0, 1);
+      }
+    } else {
+      // Catch every other drugs
+      let factor = this.expiresIn < 0 ? 2 : 1;
+      this.degrade(factor)
+    }
+    
+    return this;
+  }
 }
 
 export class Pharmacy {
@@ -17,7 +49,7 @@ export class Pharmacy {
     this.drugs = drugs;
   }
   updateBenefitValue() {
-
+    return this.drugs.map(drug => drug.update());
   }
     
 
