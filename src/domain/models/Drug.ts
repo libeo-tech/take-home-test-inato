adb55e6eabdd169a44261ba3a14df87d3c8ce4c0
Loic VU-VAN <loic.vuvan@gmail.com>
2022-03-12 12:57:27 +0100

    added Drug model definition and its tests accordingly to readme.md (exercise definition)

--- src/domain/models/Drug.ts
+++ src/domain/models/Drug.ts
@@ -1,27 +1,189 @@
-export class Drug {
-  private readonly expiresIn: number; // number of days
+import { differenceInDays } from "date-fns";
 
-  private constructor(params: { expiresIn: number }) {
-    this.expiresIn = params.expiresIn;
+export class Drug {
+  protected readonly name: string;
+  protected readonly originalExpiresIn: number; // number of days
+  protected readonly originalBenefit: number; // how powerful the drug is
+  protected readonly timeBenefitFactor: number;
+  protected readonly timeBenefitFactorAfterExpiration: number;
+  protected readonly createdAt: Date;
+
+  constructor(params: {
+    name: string;
+    originalExpiresIn: number;
+    originalBenefit: number;
+    createdAt: Date;
+  }, config?: {
+    timeBenefitFactor?: number;
+    timeBenefitFactorAfterExpiration?: number;
+  }) {
+    // throw to avoid invalid model. Guarantee Drug integrity.
+    Drug.validate(params);
+
+    this.name = params.name;
+    this.originalExpiresIn = params.originalExpiresIn;
+    this.originalBenefit = params.originalBenefit;
+    this.createdAt = new Date(params.createdAt); // avoid mutability
+    this.timeBenefitFactor = config?.timeBenefitFactor ?? -1;
+    this.timeBenefitFactorAfterExpiration = config?.timeBenefitFactorAfterExpiration ?? -2;
   }
 
   getExpiresIn(): number {
-    return this.expiresIn;
+    return this.originalExpiresIn;
   }
 
-  private static validate(params: { expiresIn: number }): void {
-    if (Number.isNaN(params.expiresIn)) {
-      throw new Error(`expiresIn "${params.expiresIn}" is not a valid number`);
+  getOriginalBenefit(): number {
+    return this.originalBenefit;
+  }
+
+  getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getExpiresInAt(targetDate: Date): number {
+    const expiresIn = this.originalExpiresIn - differenceInDays(targetDate, this.getCreatedAt());
+    if (expiresIn < 0) {
+      return 0;
     }
 
-    if (Number(params.expiresIn) <= 0) {
+    return expiresIn;
+  }
+
+  getBenefitAt(targetDate: Date): number {
+    if (targetDate < this.getCreatedAt()) {
+      return this.originalBenefit; // not alterated in the past. May be discussable.
+    }
+
+    const {
+      numberOfDaysAfterExpiration,
+      numberOfDaysBeforeExpiration
+    } = this.getBenefitAtCalculationParams(targetDate);
+
+    const benefitAt = this.originalBenefit +
+      (numberOfDaysBeforeExpiration * this.timeBenefitFactor) +
+      numberOfDaysAfterExpiration * this.timeBenefitFactorAfterExpiration
+    ;
+
+    return this.benefitValueGuard(benefitAt);
+  }
+
+  protected getBenefitAtCalculationParams(targetDate: Date): {
+    diffInDays: number;
+    numberOfDaysAfterExpiration: number;
+    numberOfDaysBeforeExpiration: number;
+  } {
+    const diffInDays = differenceInDays(targetDate, this.getCreatedAt());
+
+    let numberOfDaysAfterExpiration = diffInDays - this.originalExpiresIn || 0;
+    if (numberOfDaysAfterExpiration < 0) {
+      numberOfDaysAfterExpiration = 0
+    }
+
+    const numberOfDaysBeforeExpiration = diffInDays - numberOfDaysAfterExpiration;
+
+    return {
+      diffInDays,
+      numberOfDaysAfterExpiration,
+      numberOfDaysBeforeExpiration
+    }
+  }
+
+  protected benefitValueGuard (benefit: number): number {
+    if (benefit < 0) {
+      return 0;
+    }
+
+    return benefit > 50 ? 50 : benefit;
+  }
+
+  // todo ensure the rest of Model is fully compliant to its business definition
+  private static validate(params: {
+    name: string;
+    originalExpiresIn: number;
+    originalBenefit: number;
+    createdAt: Date;
+  }): void {
+    if (Number.isNaN(params.originalExpiresIn)) {
+      throw new Error(`originalExpiresIn "${params.originalExpiresIn}" is not a valid number`);
+    }
+
+    if (Number(params.originalExpiresIn) <= 0) {
       throw new Error('Expiration cannot be equal / under 0.');
     }
+
+    if (Number.isNaN(params.originalBenefit)) {
+      throw new Error(`originalBenefit "${params.originalBenefit}" is not a valid number`);
+    }
+
+    if (!(params.createdAt instanceof Date)) {
+      throw new Error(`createdAt "${params.createdAt}" must be a Date`);
+    }
+  }
+}
+
+export class Fervex extends Drug {
+  constructor(params: {
+    originalExpiresIn: number;
+    originalBenefit: number;
+    createdAt: Date;
+  }) {
+    super({
+      name: 'Fervex',
+      originalExpiresIn: params.originalExpiresIn,
+      originalBenefit: params.originalBenefit,
+      createdAt: params.createdAt,
+    }, { timeBenefitFactor: 1 });
   }
 
-  static create(params: { expiresIn: number }): Drug {
-    this.validate(params);
+  /**
+   * override Drug for specific behavior
+   * @param targetDate
+   */
+  getBenefitAt(targetDate: Date): number {
+    if (targetDate < this.getCreatedAt()) {
+      return this.originalBenefit; // not alterated in the past. May be discussable.
+    }
 
-    return new Drug(params);
+    const { diffInDays } = this.getBenefitAtCalculationParams(targetDate);
+
+    const diffBeforeExpiration = this.getExpiresIn() - diffInDays;
+
+    if (diffInDays > this.getExpiresIn()) {
+      return 0;
+    }
+
+    if (diffBeforeExpiration > 10) {
+      return super.getBenefitAt(targetDate);
+    }
+
+    if (this.originalExpiresIn <= 5) {
+      return this.benefitValueGuard(this.originalBenefit + diffInDays * 3);
+    }
+
+    const lastThreshold = this.originalExpiresIn - 5;
+    let numberOfDaysLastThreshold = diffInDays - lastThreshold;
+    if (numberOfDaysLastThreshold <= 0) {
+      numberOfDaysLastThreshold = 0;
+    }
+
+    const secondThreshold = this.originalExpiresIn - 10;
+    let numberOfDaysSecondThreshold = diffInDays - secondThreshold - numberOfDaysLastThreshold;
+    if (numberOfDaysSecondThreshold <= 0) {
+      numberOfDaysSecondThreshold = 0;
+    }
+
+    const numberOfDaysFirstThreshold = diffInDays - numberOfDaysSecondThreshold - numberOfDaysLastThreshold;
+
+    const benefit = this.originalBenefit +
+      (numberOfDaysLastThreshold * 3) +
+      numberOfDaysSecondThreshold * 2 +
+      numberOfDaysFirstThreshold
+    ;
+
+    return this.benefitValueGuard(benefit);
   }
 }
--- tests/domain/models/Drug.test.ts
+++ tests/domain/models/Drug.test.ts
@@ -1,10 +1,270 @@
 import { expect } from 'chai';
-import { Drug } from '../../../src/domain/models/Drug';
+import { addDays } from 'date-fns';
+import {
+  Drug,
+  Fervex
+} from '../../../src/domain/models/Drug';
 
 describe('Drug', () => {
-  it('Should have an "expiresIn" property', () => {
-    const drug = Drug.create({ expiresIn: 12 });
+  let defaultOriginalBenefit: number;
+  let defaultExpiresIn: number;
+  let defaultCreatedAt: Date;
+  let dummyName: string;
+
+  before(() => {
+  });
+
+  beforeEach(() => {
+    dummyName = 'drug test';
+    defaultOriginalBenefit = 10;
+    defaultExpiresIn = 7;
+    defaultCreatedAt = new Date('2022-03-12T10:00:00+01:00');
+  });
+
+  it('Should have an "originalExpiresIn" property', () => {
+    const drug = new Drug({
+      name: dummyName,
+      originalExpiresIn: 12,
+      originalBenefit: defaultOriginalBenefit,
+      createdAt: defaultCreatedAt,
+    });
 
     expect(drug.getExpiresIn()).to.eql(12);
+  });
+
+  it('Should have a "originalBenefit" property', () => {
+    const drug = new Drug({
+      name: dummyName,
+      originalExpiresIn: defaultExpiresIn,
+      originalBenefit: 16,
+      createdAt: defaultCreatedAt,
+    });
+
+    expect(drug.getOriginalBenefit()).to.eql(16);
+  });
+
+  it ('Should have a date of "creation"', () => {
+    const createdAt = new Date('2022-03-02T10:00:00+01:00');
+    const drug = new Drug({
+      name: dummyName,
+      originalExpiresIn: defaultExpiresIn,
+      originalBenefit: 16,
+      createdAt,
+    });
+
+    expect(drug.getCreatedAt()).to.eql(createdAt);
+  });
+
+  describe('Handle expiration', () => {
+    it('Should not decrement expirationIn', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-01T10:00:00+01:00'),
+      });
+
+      expect(drug.getExpiresInAt(new Date('2022-03-01T12:00:00+01:00'))).to.eql(7);
+    });
+
+    it('Should decrement expirationIn by 1', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-01T10:00:00+01:00'),
+      });
+
+      expect(drug.getExpiresInAt(new Date('2022-03-02T12:00:00+01:00'))).to.eql(6);
+    });
+
+    it('Should decrement expirationIn by 2', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-01T10:00:00+01:00'),
+      });
+
+      expect(drug.getExpiresInAt(new Date('2022-03-03T12:00:00+01:00'))).to.eql(5);
+    });
+    it('Should be expired', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-01T10:00:00+01:00'),
+      });
+
+      expect(drug.getExpiresInAt(new Date('2022-03-09T12:00:00+01:00'))).to.eql(0);
+    });
+  });
+
+  describe('Should handle benefit calculation logic', () => {
+    it('Should have a benefit === default benefit because no elapsed day', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-02T10:00:00+01:00'),
+      });
+
+      expect(drug.getBenefitAt(new Date('2022-03-02T12:00:00+01:00'))).to.eql(25);
+    });
+
+    it('Should have a benefit decrease by one day', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-02T10:00:00+01:00'),
+      });
+
+      expect(drug.getBenefitAt(new Date('2022-03-03T12:00:00+01:00'))).to.eql(24);
+    });
+
+    it('Should have a benefit of 0 (and not a negative number)', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-02T10:00:00+01:00'),
+      });
+
+      expect(drug.getBenefitAt(new Date('2022-03-20T12:00:00+01:00'))).to.eql(0);
+    });
+
+    it('Should decrease twice as fast once the expiration is passed', () => {
+      const createdAt = new Date('2022-03-02T10:00:00+01:00');
+      const originalBenefit = 25;
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit,
+        createdAt,
+      });
+
+      const createdAtPlusTenDays = addDays(createdAt, 10);
+
+      expect(drug.getBenefitAt(createdAtPlusTenDays)).to.eql(originalBenefit - 13);
+    });
+
+    it('Should have a benefit increase by one day', () => {
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit: 25,
+        createdAt: new Date('2022-03-02T10:00:00+01:00'),
+      }, { timeBenefitFactor: 1 });
+
+      expect(drug.getBenefitAt(new Date('2022-03-03T12:00:00+01:00'))).to.eql(26);
+    });
+
+    it('Should increase twice as fast once the expiration is passed', () => {
+      const createdAt = new Date('2022-03-02T10:00:00+01:00');
+      const originalBenefit = 25;
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit,
+        createdAt,
+      }, { timeBenefitFactor: 1, timeBenefitFactorAfterExpiration: 2 });
+
+      const someDaysLater = addDays(createdAt, 10);
+
+      expect(drug.getBenefitAt(someDaysLater)).to.eql(originalBenefit + 13);
+    });
+
+    it('Should not have a benefit greater than 50', () => {
+      const createdAt = new Date('2022-03-02T10:00:00+01:00');
+      const originalBenefit = 25;
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit,
+        createdAt,
+      }, { timeBenefitFactor: 1, timeBenefitFactorAfterExpiration: 2 });
+
+      const someDaysLater = addDays(createdAt, 100);
+
+      expect(drug.getBenefitAt(someDaysLater)).to.eql(50);
+    });
+
+    it('Should not decrease following factors', () => {
+      const createdAt = new Date('2022-03-02T10:00:00+01:00');
+      const originalBenefit = 25;
+      const drug = new Drug({
+        name: dummyName,
+        originalExpiresIn: 7,
+        originalBenefit,
+        createdAt,
+      }, { timeBenefitFactor: 0, timeBenefitFactorAfterExpiration: 0 });
+
+      const someDaysLater = addDays(createdAt, 100);
+
+      expect(drug.getBenefitAt(someDaysLater)).to.eql(originalBenefit);
+    });
+
+    describe('Fervex specific use case', () => {
+      it('Should increase benefit like herbal tea when not under specific threshold', () => {
+        const drug = new Fervex({
+          originalExpiresIn: 12,
+          originalBenefit: 25,
+          createdAt: new Date('2022-03-02T10:00:00+01:00'),
+        });
+
+        expect(drug.getBenefitAt(new Date('2022-03-03T12:00:00+01:00'))).to.eql(26);
+      });
+
+      it('Should increase benefit with a specific threshold when 9 days before expiration', () => {
+        const drug = new Fervex({
+          originalExpiresIn: 31,
+          originalBenefit: 25,
+          createdAt: new Date('2022-03-01T10:00:00+01:00'),
+        });
+
+        expect(drug.getBenefitAt(new Date('2022-03-23T12:00:00+01:00'))).to.eql(48);
+      });
+
+      it('Should increase benefit with two specific thresholds when 3 days before expiration', () => {
+        const drug = new Fervex({
+          originalExpiresIn: 15,
+          originalBenefit: 1,
+          createdAt: new Date('2022-03-01T10:00:00+01:00'),
+        });
+
+        expect(drug.getBenefitAt(new Date('2022-03-13T12:00:00+01:00'))).to.eql(22);
+      });
+
+      it('Should has 0 benefit when expired', () => {
+        const drug = new Fervex({
+          originalExpiresIn: 12,
+          originalBenefit: 25,
+          createdAt: new Date('2022-03-02T10:00:00+01:00'),
+        });
+
+        expect(drug.getBenefitAt(new Date('2022-03-16T12:00:00+01:00'))).to.eql(0);
+      });
+
+      it('Should decrease by last factor only if medicine specified in specific state', () => {
+        const drug = new Fervex({
+          originalExpiresIn: 5,
+          originalBenefit: 40,
+          createdAt: new Date('2022-03-02T10:00:00+01:00'),
+        });
+
+        expect(drug.getBenefitAt(new Date('2022-03-03T12:00:00+01:00'))).to.eql(43);
+      });
+
+      it('Should decrease by last factor and second if medicine specified in specific state', () => {
+        const drug = new Fervex({
+          originalExpiresIn: 10,
+          originalBenefit: 30,
+          createdAt: new Date('2022-03-01T10:00:00+01:00'),
+        });
+
+        expect(drug.getBenefitAt(new Date('2022-03-07T12:00:00+01:00'))).to.eql(30 + 10 + 3);
+      });
+    });
   });
 });
