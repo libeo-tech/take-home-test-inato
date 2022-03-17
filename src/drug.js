06aa175335907b13658ae0d8c9f716d3c82f9f59
Valentin <valeporti@hotmail.com>
2022-03-17 22:26:55 +0100

    test: did changes according to failing tests and added required feature

--- src/drug.js
+++ src/drug.js
@@ -5,44 +5,58 @@ export class Drug {
     this.benefit = benefit;
   }
 
-  getDegradation = () => {
+  getDegradation() {
     const degradationUnit = 1;
     // case "Magic Pill"
     if (this.name === "Magic Pill") return 0;
     // case "Herbal Tea"
-    else if (this.name === "Herbal Tea" && this.expiresIn >= 0)
+    else if (this.name === "Herbal Tea" && this.expiresIn > 0)
       return degradationUnit * 1;
-    else if (this.name === "Herbal Tea" && this.expiresIn < 0)
+    else if (this.name === "Herbal Tea" && this.expiresIn <= 0)
       return degradationUnit * 2;
     // case "Fervex"
-    else if (this.name === "Fervex" && this.expiresIn < 0)
+    else if (this.name === "Fervex" && this.expiresIn <= 0)
       // not important, it will be nullified by factor, just explicited for readiness
       return degradationUnit * 0;
-    else if (this.name === "Fervex" && this.expiresIn < 5)
+    else if (this.name === "Fervex" && this.expiresIn <= 5)
       return degradationUnit * 3;
-    else if (this.name === "Fervex" && this.expiresIn < 10)
+    else if (this.name === "Fervex" && this.expiresIn <= 10)
       return degradationUnit * 2;
     // case "Dafalgan"
-    else if (this.name === "Dafalgan") return degradationUnit * 2;
+    else if (this.name === "Dafalgan" && this.expiresIn > 0)
+      return degradationUnit * 2;
 
+    // base case
+    if (this.expiresIn <= 0) return degradationUnit * 2;
     return degradationUnit;
-  };
+  }
 
-  getDegradationFactor = () => {
-    // cases that nullify the benefit
-    if (this.name === "Fervex" && this.expiresIn < 0) return 0;
-
+  getDegradationFactor() {
     // increasing cases
     if (["Herbal Tea", "Fervex"].includes(this.name)) return 1;
 
+    // static cases
+    if (this.benefit <= 0) return 0;
+
     // decreasing cases (main)
     return -1;
-  };
+  }
 
-  updateBenefit = () => {
+  getBenefitNullifyingFactor() {
+    // cases that nullify the benefit
+    if (this.name === "Fervex" && this.expiresIn <= 0) return 0;
+    return 1;
+  }
+
+  updateBenefitFromDayDegradation() {
     const factor = this.getDegradationFactor();
     const degradation = this.getDegradation();
+    const benefitNullifier = this.getBenefitNullifyingFactor();
 
-    this.benefit = (this.benefit + degradation) * factor;
-  };
+    this.benefit = benefitNullifier * (this.benefit + degradation * factor);
+  }
+
+  decreaseExpiresIn() {
+    if (this.name !== "Magic Pill") this.expiresIn--;
+  }
 }
